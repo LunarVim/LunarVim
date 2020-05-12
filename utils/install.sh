8670c2710ef518e6a9d149c072233e588239f2a3
Chris <chrisatmachine@gmail.com>
2020-05-11 22:05:20 -0400

    update install script

--- utils/install.sh
+++ utils/install.sh
@@ -5,36 +5,40 @@ set -o errexit    # exit when command fails
 
 
 installnode() { \
-    echo "installing node..."
+    echo "Installing node..."
     curl -sL install-node.now.sh/lts | bash
     npm i -g neovim
 }
 
 installpip() { \
-    echo "installing pip..."
+    echo "Installing pip..."
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
     python3 get-pip.py
     rm get-pip.py
 }
 
 installpynvim() { \
-    echo "installing pynvim..."
+    echo "Installing pynvim..."
     pip install pynvim
 }
 
 cloneconfig() { \
-    echo "cloning Nvim Mach 2 configuration"
+    echo "Cloning Nvim Mach 2 configuration"
     git clone https://github.com/ChristianChiarulli/nvim.git ~/.config/nvim
 }
 
 moveoldnvim() { \
-    echo "moving your config to nvim.old"
+    echo "Moving your config to nvim.old"
     mv $HOME/.config/nvim $HOME/.config/nvim.old
 }
 
 installplugins() { \
+    mv $HOME/.config/nvim/init.vim $HOME/.config/nvim/init.vim.tmp
+    mv $HOME/.config/nvim/utils/init.vim $HOME/.config/nvim/init.vim
     "Installing plugins..."
     nvim --headless +PlugInstall +qall > /dev/null 2>&1
+    mv $HOME/.config/nvim/init.vim $HOME/.config/nvim/utils/init.vim
+    mv $HOME/.config/nvim/init.vim.tmp $HOME/.config/nvim/init.vim
 }
 
 # Welcome
@@ -44,7 +48,7 @@ echo 'Installing Nvim Mach 2'
 which node > /dev/null && echo "node installed, moving on..." || installnode
 
 # install pip
-which pip > /dev/null && echo "pip installed, moving on..." || installpynvim
+which pip > /dev/null && echo "pip installed, moving on..." || installpip
 
 # install pynvim
 pip list | grep pynvim > /dev/null && echo "pynvim installed, moving on..." || installpynvim
@@ -55,7 +59,7 @@ pip list | grep pynvim > /dev/null && echo "pynvim installed, moving on..." || i
 cloneconfig
 
 # install plugins
-which neovim > /dev/null && installplugins
+which nvim > /dev/null && installplugins
 
 # Install extensions
 mkdir -p ~/.config/coc/extensions
