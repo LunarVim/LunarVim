5030b0bf89360bfcbf15fb65965036daad58ff11
Chris <chrisatmachine@gmail.com>
2020-10-31 16:41:14 -0400

    update nvcodeline

--- lua/nvcodeline.lua
+++ lua/nvcodeline.lua
@@ -9,13 +9,13 @@ local colors = {
   yellow = '#fabd2f',
   cyan = '#008080',
   darkblue = '#081633',
-  green = '#afd700',
+  green = '#608B4E',
   orange = '#FF8800',
   purple = '#5d4d7a',
   magenta = '#d16d9e',
   grey = '#c0c0c0',
-  blue = '#0087d7',
-  red = '#ec5f67'
+  blue = '#569CD6',
+  red = '#D16969'
 }
 
 local buffer_not_empty = function()
@@ -25,44 +25,66 @@ local buffer_not_empty = function()
   return false
 end
 
-gls.left[1] = {
-  FirstElement = {
-    -- provider = function() return '▋' end,
-    provider = function() return ' ' end,
-    highlight = {colors.bg,colors.purple}
-  },
-}
+-- gls.left[1] = {
+--   FirstElement = {
+--     -- provider = function() return '▋' end,
+--     provider = function() return ' ' end,
+--     highlight = {colors.bg,colors.bg}
+--   },
+-- }
+-- gls.left[2] = {
+--   ViMode = {
+--     provider = function()
+--       local alias = {n = 'NORMAL',i = 'INSERT',c= 'COMMAND',V= 'VISUAL', [''] = 'VISUAL'}
+--       return alias[vim.fn.mode()]
+--     end,
+--     separator = ' ',
+--     separator_highlight = {colors.yellow,function()
+--       if not buffer_not_empty() then
+--         return colors.purple
+--       end
+--       return colors.purple
+--     end},
+--     highlight = {colors.grey,colors.purple,'bold'},
+--   },
+-- }
 gls.left[2] = {
   ViMode = {
     provider = function()
-      local alias = {n = 'NORMAL',i = 'INSERT',c= 'COMMAND',V= 'VISUAL', [''] = 'VISUAL'}
-      return alias[vim.fn.mode()]
+      -- auto change color according the vim mode
+      local mode_color = {n = colors.purple, 
+                          i = colors.green,
+                          v=colors.blue,
+                          [''] = colors.blue,
+                          V=colors.blue,
+                          c = colors.purple,
+                          no = colors.magenta,
+                          s = colors.orange,
+                          S=colors.orange,
+                          [''] = colors.orange,
+                          ic = colors.yellow,
+                          R = colors.red,
+                          Rv = colors.red,
+                          cv = colors.red,
+                          ce=colors.red, 
+                          r = colors.cyan,
+                          rm = colors.cyan, 
+                          ['r?'] = colors.cyan,
+                          ['!']  = colors.red,
+                          t = colors.red}
+      vim.api.nvim_command('hi GalaxyViMode guibg='..mode_color[vim.fn.mode()])
+      return '  NV Code '
     end,
     separator = ' ',
     separator_highlight = {colors.yellow,function()
       if not buffer_not_empty() then
-        return colors.purple
+        return colors.bg
       end
-      return colors.purple
+      return colors.bg
     end},
-    highlight = {colors.grey,colors.purple,'bold'},
+    highlight = {colors.grey,colors.bg,'bold'},
   },
 }
--- gls.left[2] = {
---   ViMode = {
---     provider = function()
---       -- auto change color according the vim mode
---       local mode_color = {n = colors.magenta, i = colors.green,v=colors.blue,[''] = colors.blue,V=colors.blue,
---                           c = colors.red,no = colors.magenta,s = colors.orange,S=colors.orange,
---                           [''] = colors.orange,ic = colors.yellow,R = colors.purple,Rv = colors.purple,
---                           cv = colors.red,ce=colors.red, r = colors.cyan,rm = colors.cyan, ['r?'] = colors.cyan,
---                           ['!']  = colors.red,t = colors.red}
---       vim.api.nvim_command('hi GalaxyViMode guifg='..mode_color[vim.fn.mode()])
---       return '  '
---     end,
---     highlight = {colors.red,colors.line_bg,'bold'},
---   },
--- }
 -- gls.left[3] ={
 --   FileIcon = {
 --     separator = ' ',
@@ -83,7 +105,7 @@ gls.left[2] = {
 
 gls.left[3] = {
   GitIcon = {
-    provider = function() return '  ' end,
+    provider = function() return ' ' end,
     condition = buffer_not_empty,
     highlight = {colors.orange,colors.bg},
   }
@@ -166,24 +188,24 @@ gls.right[1]= {
   FileFormat = {
     provider = 'FileFormat',
     separator = ' ',
-    separator_highlight = {colors.bg,colors.purple},
-    highlight = {colors.grey,colors.purple},
+    separator_highlight = {colors.bg,colors.bg},
+    highlight = {colors.grey,colors.bg},
   }
 }
 gls.right[2] = {
   LineInfo = {
     provider = 'LineColumn',
     separator = ' | ',
-    separator_highlight = {colors.darkblue,colors.purple},
-    highlight = {colors.grey,colors.purple},
+    separator_highlight = {colors.darkblue,colors.bg},
+    highlight = {colors.grey,colors.bg},
   },
 }
 gls.right[3] = {
   PerCent = {
     provider = 'LinePercent',
-    separator = ' ',
-    separator_highlight = {colors.darkblue,colors.purple},
-    highlight = {colors.grey,colors.darkblue},
+    separator = ' |',
+    separator_highlight = {colors.darkblue,colors.bg},
+    highlight = {colors.grey,colors.bg},
   }
 }
 gls.right[4] = {
