9785c28407bd8e71f913ba2cb6fd61d0ab849c70
Chris <chrisatmachine@gmail.com>
2021-03-08 03:57:55 -0500

    refactor thing

--- lua/config-telescope.lua
+++ lua/config-telescope.lua
@@ -1,35 +1,6 @@
 local actions = require('telescope.actions')
 -- Global remapping
 ------------------------------
-require('telescope').setup{
-  defaults = {
-    mappings = {
-      i = {
-      ["<C-j>"] = actions.move_selection_next,
-      ["<C-k>"] = actions.move_selection_previous,
-        -- To disable a keymap, put [map] = false
-        -- So, to not map "<C-n>", just put
-        -- ["<c-x>"] = false,
-
-        -- Otherwise, just set the mapping to the function that you want it to be.
-        -- ["<C-i>"] = actions.select_horizontal,
-
-        -- Add up multiple actions
-        ["<CR>"] = actions.select_default + actions.center,
-
-        -- You can perform as many actions in a row as you like
-        -- ["<CR>"] = actions.select_default + actions.center + my_cool_custom_action,
-      },
-      n = {
-      ["<C-j>"] = actions.move_selection_next,
-      ["<C-k>"] = actions.move_selection_previous,
-        -- ["<esc>"] = actions.close,
-        -- ["<C-i>"] = my_cool_custom_action,
-      },
-    },
-  }
-}
-
       -- '--color=never',
 require('telescope').setup{
   defaults = {
@@ -76,6 +47,30 @@ require('telescope').setup{
     qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
 
     -- Developer configurations: Not meant for general override
-    buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
+    buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker,
+    mappings = {
+      i = {
+      ["<C-j>"] = actions.move_selection_next,
+      ["<C-k>"] = actions.move_selection_previous,
+        -- To disable a keymap, put [map] = false
+        -- So, to not map "<C-n>", just put
+        -- ["<c-x>"] = false,
+
+        -- Otherwise, just set the mapping to the function that you want it to be.
+        -- ["<C-i>"] = actions.select_horizontal,
+
+        -- Add up multiple actions
+        ["<CR>"] = actions.select_default + actions.center,
+
+        -- You can perform as many actions in a row as you like
+        -- ["<CR>"] = actions.select_default + actions.center + my_cool_custom_action,
+      },
+      n = {
+      ["<C-j>"] = actions.move_selection_next,
+      ["<C-k>"] = actions.move_selection_previous,
+        -- ["<esc>"] = actions.close,
+        -- ["<C-i>"] = my_cool_custom_action,
+      },
+    },
   }
 }
