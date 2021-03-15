ce21cc9c636950b8cb9827f2a23d8169a9d700fb
Chris <chrisatmachine@gmail.com>
2021-03-14 20:56:56 -0400

    udpate startify

--- lua/nv-startify/init.lua
+++ lua/nv-startify/init.lua
@@ -15,3 +15,8 @@ vim.g.startify_change_to_vcs_root = 1
 vim.g.startify_fortune_use_unicode = 1
 vim.g.startify_session_persistence = 1
 vim.g.startify_session_dir = '~/.config/nvim/session'
+
+
+vim.api.nvim_exec('let startify_lists = [ { \'type\': \'files\',     \'header\': [\'   Files\'] }, { \'type\': \'sessions\',  \'header\': [\'   Sessions\'] },    { \'type\': \'bookmarks\', \'header\': [\'   Bookmarks\'] },                                                                   ]',true)
+
+vim.api.nvim_exec('let startify_bookmarks = [ { \'b\': \'~/Blog\' }, { \'i\': \'~/.config/nvim/init.vim\' }, { \'z\': \'~/.zshrc\' }]', true)
