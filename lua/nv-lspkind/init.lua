16b711d9b0387b7771b51fb54821dd2824bf86bb
Chris <chrisatmachine@gmail.com>
2021-03-15 03:29:40 -0400

    lspkind

---
+++ lua/nv-lspkind/init.lua
@@ -0,0 +1,26 @@
+-- symbols for autocomplete
+require('lspkind').init({
+    with_text = false,
+    symbol_map = {
+      Text = '  ',
+      Method = '  ',
+      Function = '  ',
+      Constructor = '  ',
+      Variable = '[]',
+      Class = '  ',
+      Interface = ' 蘒',
+      Module = '  ',
+      Property = '  ',
+      Unit = ' 塞 ',
+      Value = '  ',
+      Enum = ' 練',
+      Keyword = '  ',
+      Snippet = '  ',
+      Color = '',
+      File = '',
+      Folder = ' ﱮ ',
+      EnumMember = '  ',
+      Constant = '  ',
+      Struct = '  '
+    },
+})
