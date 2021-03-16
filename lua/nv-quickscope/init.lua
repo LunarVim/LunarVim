dbbe4ee4c9c757109e9feb8c39d00dd68efb5142
Chris <chrisatmachine@gmail.com>
2021-03-16 02:42:41 -0400

    remove extra config

--- lua/nv-quickscope/init.lua
+++ lua/nv-quickscope/init.lua
@@ -1,7 +1,4 @@
 -- Trigger a highlight in the appropriate direction when pressing these keys:
 -- vim.cmd('let g:qs_highlight_on_keys = [\'f\', \'F\', \'t\', \'T\']')
 vim.g.qs_highlight_on_keys = {'f', 'F', 't', 'T'}
-
-vim.api.nvim_exec('highlight QuickScopePrimary guifg=\'#00C7DF\' gui=underline ctermfg=155 cterm=underline', false)
-vim.cmd('highlight QuickScopeSecondary guifg=\'#eF5F70\' gui=underline ctermfg=81 cterm=underline')
 vim.g.qs_max_chars=150
