7f8fb0135a8bf9fa2a67118f57110e130e91b4cf
Chris <chrisatmachine@gmail.com>
2021-03-16 23:11:27 -0400

    update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,16 +1,16 @@
 
-vim.cmd [[packadd packer.nvim]]
 
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
 local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
 
-
 if fn.empty(fn.glob(install_path)) > 0 then
   execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
   execute 'packadd packer.nvim'
 end
+
+vim.cmd [[packadd packer.nvim]]
 
 -- vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile' -- Auto compile when there are changes in plugins.lua
 
