ab661fb3b801c1ad41559d3f039cff53557ce9f6
michaelslec <57121017+michaelslec@users.noreply.github.com>
2021-03-18 22:01:39 -0700

    fix: packpath error on first run (#139)
    packadd command not working because it was not installed on first run.
    
    Co-authored-by: Christian Chiarulli <chrisatmachine@gmail.com>

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,3 +1,5 @@
+vim.cmd [[packadd packer.nvim]]
+
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
@@ -7,8 +9,6 @@ if fn.empty(fn.glob(install_path)) > 0 then
     execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
     execute 'packadd packer.nvim'
 end
-
-vim.cmd [[packadd packer.nvim]]
 
 vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile' -- Auto compile when there are changes in plugins.lua
 
