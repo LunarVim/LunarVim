63147455d074d7b6b89f973976783ec72de10bce
Chris <chrisatmachine@gmail.com>
2021-03-22 15:43:35 -0400

    remove number for now

--- lua/nv-utils/init.lua
+++ lua/nv-utils/init.lua
@@ -38,7 +38,13 @@ nv_utils.define_augroups({
 
         { 'FileType', 'dashboard', 'set showtabline=0 | autocmd WinLeave <buffer> set showtabline=2'},
         { 'FileType', '*', 'setlocal number'},
+        { 'FileType', 'NeogitStatus', 'setlocal nonumber'},
         { 'FileType', 'dashboard', 'setlocal nonumber'},
+        { 'FileType', 'qf', 'setlocal nonumber'},
+        { 'FileType', 'floaterm', 'setlocal nonumber'},
+        { 'FileType', 'NvimTree', 'setlocal nonumber'},
+        { 'FileType', 'rnvimr', 'setlocal nonumber'},
+        { 'FileType', 'TelescopePrompt', 'setlocal nonumber'},
         { 'FileType', 'dashboard', 'setlocal nocursorline'},
         -- { 'FileType', 'dashboard', 'set nonumber | autocmd WinLeave <buffer> set number'},
         {'BufRead', '*.sol', 'setlocal filetype=solidity'},
