8d41fced1f3617c0df7f2ffbd8fae5210c37299b
Chris <chrisatmachine@gmail.com>
2021-03-21 20:02:57 -0400

    spelling is acting up

--- lua/nv-compe/init.lua
+++ lua/nv-compe/init.lua
@@ -21,7 +21,7 @@ require'compe'.setup {
         vsnip = {kind = "  "},
         nvim_lsp = {kind = "  "},
         nvim_lua = {kind = "  "},
-        spell = {kind = "  "},
+        -- spell = {kind = "  ", filetypes = "markdown"},
         tags = false,
         snippets_nvim = {kind = "  "},
         treesitter = {kind = "  "},
