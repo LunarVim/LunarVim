24e7248577d39d00de7dcd68bd382591735ccf86
Henry Oliver <henry_oliver@me.com>
2021-03-24 15:57:01 -0400

    Update `nv-rnvimr` > `init.lua` simplifying it

--- lua/nv-rnvimr/init.lua
+++ lua/nv-rnvimr/init.lua
@@ -8,17 +8,17 @@ vim.g.rnvimr_pick_enable = 1
 -- Make Neovim to wipe the buffers corresponding to the files deleted by Ranger
 vim.g.rnvimr_bw_enable = 1
 
+-- Set up only two columns
+vim.g.rnvimr_ranger_cmd = 'ranger --cmd="set column_ratios 1,1"'
+
+-- Customize the initial layout
+vim.g.rnvimr_layout = { 
+  style = 'minimal',
+  relative = 'editor',
+  row = math.floor(0.05 * vim.g.lines),
+  col = math.floor(0.05 * vim.g.columns),
+  height = math.floor(0.9 * vim.g.lines),
+  width = math.floor(0.9 * vim.g.columns)
+}
+
 -- vim.cmd('nmap <leader>r :RnvimrToggle<CR>')
-
---[[ let g:rnvimr_ranger_cmd = 'ranger --cmd="set column_ratios 1,1"'
-            " \ --cmd="set draw_borders separators"'
-
-" let g:rnvimr_layout = { 'relative': 'editor',
-"             \ 'width': float2nr(round(0.6 * &columns)),
-"             \ 'height': float2nr(round(0.6 * &lines)),
-"             \ 'col': float2nr(round(0.2 * &columns)),
-"             \ 'row': float2nr(round(0.2 * &lines)),
-"             \ 'style': 'minimal' }
-
-let g:rnvimr_presets = [
-            \ {'width': 0.800, 'height': 0.800}] ]]
