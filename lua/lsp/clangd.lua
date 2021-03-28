7565d9397fea4c59eecd0ccc0b49d6c1727e6aea
James Walmsley <james@fullfat-fs.co.uk>
2021-03-28 02:11:24 +0100

    [LSP] clangd - Support LspInstall upstream path. (#174)
    cool 👾

--- lua/lsp/clangd.lua
+++ lua/lsp/clangd.lua
@@ -1,4 +1,4 @@
 require'lspconfig'.clangd.setup{
-    cmd = {DATA_PATH .. "/lspinstall/clangd/clangd_11.0.0/bin/clangd"},
+    cmd = {DATA_PATH .. "/lspinstall/cpp/clangd/bin/clangd"},
     on_attach = require'lsp'.common_on_attach
 }
