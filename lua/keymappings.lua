c94e5a922c74f4df3789dab9c77796650982d744
Alan Jenkins <alan.james.jenkins@gmail.com>
2021-03-29 15:53:20 +0100

    Remove visual mode mapping for moving a selected block
    Remove the visual mode mappings for moving a selected block of text up
    and down lines as these override the default Vim line joining mapping
    and the functionality can easily be replicated by just deleting and
    pasting.

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -32,10 +32,6 @@ vim.cmd([[
 vim.api.nvim_set_keymap('v', '<', '<gv', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('v', '>', '>gv', {noremap = true, silent = true})
 
--- Move selected line / block of text in visual mode
-vim.api.nvim_set_keymap('x', 'K', ':move \'<-2<CR>gv-gv', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('x', 'J', ':move \'>+1<CR>gv-gv', {noremap = true, silent = true})
-
 -- Better nav for omnicomplete
 vim.cmd('inoremap <expr> <c-j> (\"\\<C-n>\")')
 vim.cmd('inoremap <expr> <c-k> (\"\\<C-p>\")')
