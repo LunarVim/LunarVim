029c7e5219705e40e82db9c78e50b7633e5ac3ac
chaeing <smchae9@gmail.com>
2021-03-31 07:50:00 -0700

    Add svelte lsp config for LspInstall (#202)

--- init.lua
+++ init.lua
@@ -65,6 +65,7 @@ require('lsp.html-ls')
 require('lsp.efm-general-ls')
 require('lsp.virtual_text')
 require('lsp.latex-ls')
+require('lsp.svelte-ls')
 
 -- vim.lsp.callbacks["textDocument/publishDiagnostics"] = function() end
 -- vim.lsp.handlers["textDocument/publishDiagnostics"] = nil
