f1c33c375a363222958405f5a4aee4c6c77e9455
mjcc30 <cordeiromaxime@gmail.com>
2021-04-02 00:42:21 +0200

    add compe-tabnine
    from example : https://github.com/JoseConseco/nvim_config/search?q=tabnine

--- lua/nv-compe/init.lua
+++ lua/nv-compe/init.lua
@@ -19,6 +19,7 @@ require'compe'.setup {
         buffer = {kind = "  "},
         calc = {kind = "  "},
         vsnip = {kind = "  "},
+	tabnine = {kind = " ", priority=200, max_reslts=6},
         nvim_lsp = {kind = "  "},
         -- nvim_lua = {kind = "  "},
 		nvim_lua = false,
