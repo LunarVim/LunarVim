6d637010b707a9f6cba51253c16bcf0da950e032
Biser Stoilov <biserstoilov@gmail.com>
2021-04-02 19:55:44 +0300

    Update init.lua

--- lua/nv-dashboard/init.lua
+++ lua/nv-dashboard/init.lua
@@ -13,6 +13,7 @@ vim.g.dashboard_custom_section = {
     a = {description = {'  Find File          '}, command = 'Telescope find_files'},
     b = {description = {'  Recently Used Files'}, command = 'Telescope oldfiles'},
     c = {description = {'  Load Last Session  '}, command = 'SessionLoad'},
+    p = {description = {'  Projects           '}, command = 'Telescope project'},
     d = {description = {'  Find Word          '}, command = 'Telescope live_grep'},
     e = {description = {'  Settings           '}, command = ':e ~/.config/nvim/nv-settings.lua'}
     -- e = {description = {' Marks              '}, command = 'Telescope marks'}
