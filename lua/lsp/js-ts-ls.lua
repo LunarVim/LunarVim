f40925f94f73f05b88a867e26ff583c1c44829d3
Dominik Marcinowski <30533306+DominikMarcinowski@users.noreply.github.com>
2021-04-02 09:25:50 +0200

    Make tss server work on standalone js/ts/etc. files (#216)

--- lua/lsp/js-ts-ls.lua
+++ lua/lsp/js-ts-ls.lua
@@ -14,7 +14,7 @@ require'lspconfig'.tsserver.setup {
     on_attach = require'lsp'.tsserver_on_attach,
     -- This makes sure tsserver is not used for formatting (I prefer prettier)
     -- on_attach = require'lsp'.common_on_attach,
-    root_dir = require('lspconfig/util').root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git"),
+    root_dir = require('lspconfig/util').root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git", "."),
     settings = {documentFormatting = false},
     handlers = {
         ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
