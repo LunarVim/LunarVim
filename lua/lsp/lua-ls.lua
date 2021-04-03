bce3db44034ceca5e2dcb54cf87d4db0531dc3c6
DominikMarcinowski <dmarcinowski@zoho.com>
2021-04-03 20:01:29 +0200

    Add awesome to lua-ls globals

--- lua/lsp/lua-ls.lua
+++ lua/lsp/lua-ls.lua
@@ -15,7 +15,7 @@ require'lspconfig'.sumneko_lua.setup {
             },
             diagnostics = {
                 -- Get the language server to recognize the `vim` global
-                globals = {'vim'}
+                globals = {'vim', 'awesome'}
             },
             workspace = {
                 -- Make the server aware of Neovim runtime files
