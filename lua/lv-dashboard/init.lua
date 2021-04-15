3677277a0959899793d18e979ac60d803352c553
Christian Chiarulli <chris.machine@pm.me>
2021-04-15 12:49:48 -0400

    Update init.lua

--- lua/lv-dashboard/init.lua
+++ lua/lv-dashboard/init.lua
@@ -36,7 +36,7 @@ vim.g.dashboard_custom_section = {
     b = {description = {'  Recently Used Files'}, command = 'Telescope oldfiles'},
     c = {description = {'  Load Last Session  '}, command = 'SessionLoad'},
     d = {description = {'  Find Word          '}, command = 'Telescope live_grep'},
-    e = {description = {'  Settings           '}, command = ':e ~/.config/nvim/nv-settings.lua'}
+    e = {description = {'  Settings           '}, command = ':e ~/.config/nvim/lv-settings.lua'}
     -- e = {description = {'  Marks              '}, command = 'Telescope marks'}
 }
 
