be94ac9e1f8bc547875aeb979ad24103b37a4a17
shinu_donney <shinudonney@tuta.io>
2021-04-22 02:03:41 +0530

    Update a faulty comment

--- lua/settings.lua
+++ lua/settings.lua
@@ -8,7 +8,7 @@ vim.cmd('let &titleold="'..TERMINAL..'"')
 vim.o.titlestring="%<%F%=%l/%L - nvim"
 vim.wo.wrap = O.wrap_lines -- Display long lines as just one line
 vim.cmd('set whichwrap+=<,>,[,],h,l') -- move to next line with theses keys
-vim.cmd('syntax on') -- move to next line with theses keys
+vim.cmd('syntax on') -- syntax highlighting
 vim.o.pumheight = 10 -- Makes popup menu smaller
 vim.o.fileencoding = "utf-8" -- The encoding written to file
 vim.o.cmdheight = 2 -- More space for displaying messages
