d48c797ec959ab179146f1a94bb00f9eed2c8d0f
Francisco Suárez <franksg88@gmail.com>
2021-05-04 19:26:35 -0500

    comment to keep original one by default.

--- lv-settings.lua
+++ lv-settings.lua
@@ -14,8 +14,8 @@ O.auto_close_tree = 0
 O.wrap_lines = false
 
 -- dashboard 
-O.dashboard.custom_header = {""}
-O.dashboard.footer = {""}
+-- O.dashboard.custom_header = {""}
+-- O.dashboard.footer = {""}
 
 -- if you don't want all the parsers change this to a table of the ones you want
 O.treesitter.ensure_installed = "all"
