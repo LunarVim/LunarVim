db2b17be723483c6406234e4017ade9b772a9bbf
Francisco Suárez <tarkin88@users.noreply.github.com>
2021-05-04 21:02:55 -0500

    ✨  Change dashboard header and footer by user (#363)
    * :sparkles: ability to change dashboard header and footer.
    
    * :sparkles: lv-settings adapted.
    
    * comment to keep original one by default.

--- lua/lv-globals.lua
+++ lua/lv-globals.lua
@@ -61,9 +61,21 @@ O = {
 	ruby = {
 		diagnostics = {virtualtext = {spacing = 0, prefix = ""}, signs = true, underline = true},
 		filetypes = {'rb', 'erb', 'rakefile'}
-	}
+	},
     -- css = {formatter = '', autoformat = false, virtual_text = true},
     -- json = {formatter = '', autoformat = false, virtual_text = true}
+
+	dashboard = {
+		custom_header = {
+'                 _..._                                                                           ',
+'               .\'   (_`.    _                         __     ___           ',
+'              :  .      :  | |   _   _ _ __   __ _ _ _\\ \\   / (_)_ __ ___  ',
+'              :)    ()  :  | |  | | | | \'_ \\ / _` | \'__\\ \\ / /| | \'_ ` _ \\ ',
+'              `.   .   .\'  | |__| |_| | | | | (_| | |   \\ V / | | | | | | |',
+'                `-...-\'    |_____\\__,_|_| |_|\\__,_|_|    \\_/  |_|_| |_| |_|',
+		},
+		footer= {'chrisatmachine.com'}
+	}
 }
 
 DATA_PATH = vim.fn.stdpath('data')
