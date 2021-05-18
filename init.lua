946dc0c1dd65746195bafc2a61ea9dfe7fd2505e
Kyle Stubblefield <iamtuxmeister@gmail.com>
2021-05-18 08:36:33 -0700

    moved the including of lv-settings to above the settings so local settings configured in lv-settings can override the globals that get loaded in settings, 0.relative_number for example (#396)

--- init.lua
+++ init.lua
@@ -1,12 +1,12 @@
 require('lv-globals')
+vim.cmd('luafile ~/.config/nvim/lv-settings.lua')
 require('settings')
 require('plugins')
 require('lv-utils')
 require('lv-autocommands')
-vim.cmd('luafile ~/.config/nvim/lv-settings.lua')
 require('keymappings')
 require('lv-nvimtree') -- This plugin must be required somewhere before colorscheme.  Placing it after will break navigation keymappings
-require('colorscheme') -- This plugin must be required somewhere after nvimtree. Placing it before will break navigation keymappings 
+require('colorscheme') -- This plugin must be required somewhere after nvimtree. Placing it before will break navigation keymappings
 require('lv-galaxyline')
 require('lv-comment')
 require('lv-compe')
