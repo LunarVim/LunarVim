4924ad1a14dc24bf5ff563e7175e078daecd94fe
rebuilt <memoryman51@hotmail.com>
2021-05-21 13:57:03 +0200

    improves startuptime

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,26 +8,6 @@ if fn.empty(fn.glob(install_path)) > 0 then
     execute "packadd packer.nvim"
 end
 
---- Check if a file or directory exists in this path
-local function require_plugin(plugin)
-    local plugin_prefix = fn.stdpath("data") .. "/site/pack/packer/opt/"
-
-    local plugin_path = plugin_prefix .. plugin .. "/"
-    --	print('test '..plugin_path)
-    local ok, err, code = os.rename(plugin_path, plugin_path)
-    if not ok then
-        if code == 13 then
-            -- Permission denied, but it exists
-            return true
-        end
-    end
-    --	print(ok, err, code)
-    if ok then
-        vim.cmd("packadd " .. plugin)
-    end
-    return ok, err, code
-end
-
 vim.cmd "autocmd BufWritePost plugins.lua PackerCompile" -- Auto compile when there are changes in plugins.lua
 
 return require("packer").startup(
@@ -35,75 +15,61 @@ return require("packer").startup(
         -- Packer can manage itself as an optional plugin
         use "wbthomason/packer.nvim"
 
-        -- TODO refactor all of this (for now it works, but yes I know it could be wrapped in a simpler function)
-        use {"neovim/nvim-lspconfig", opt = true}
-        use {"glepnir/lspsaga.nvim", opt = true}
-        use {"kabouzeid/nvim-lspinstall", opt = true}
+        -- LSP
+        use {"neovim/nvim-lspconfig" }
+        use {"glepnir/lspsaga.nvim", cmd = "Lspsaga" }
+        use {"kabouzeid/nvim-lspinstall", 
+        cmd = "LspInstall",
+        after = "nvim-lspconfig", 
+        config = function ()
+          require('lv-lspinstall')
+        end }
 
         -- Telescope
-        use {"nvim-lua/popup.nvim", opt = true}
-        use {"nvim-lua/plenary.nvim", opt = true}
-        use {"nvim-telescope/telescope.nvim", opt = true}
-        use {"nvim-telescope/telescope-fzy-native.nvim", opt = true}
+        use {"nvim-telescope/telescope.nvim",
+        cmd = "Telescope",
+        event = "BufReadPre",
+        module = {"plenary.nvim", "popup.nvim"},
+        config = function() require("lv-telescope") end,
+        requires = {
+            {"nvim-lua/popup.nvim"},
+            {"nvim-lua/plenary.nvim"},
+            {"nvim-telescope/telescope-fzy-native.nvim"}
+        } }
 
         -- Debugging
         use {"mfussenegger/nvim-dap", opt = true}
 
         -- Autocomplete
-        use {"hrsh7th/nvim-compe", opt = true}
-        use {"hrsh7th/vim-vsnip", opt = true}
-        use {"rafamadriz/friendly-snippets", opt = true}
+        use {"hrsh7th/nvim-compe", event = "InsertEnter", config = function() require("lv-compe") end}
+        use {"hrsh7th/vim-vsnip", after = "nvim-compe"}
+        use {"rafamadriz/friendly-snippets", after = "nvim-compe"}
 
         -- Treesitter
-        use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
-        use {"windwp/nvim-ts-autotag", opt = true}
+        use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate", event = "BufRead"}
+        use {"windwp/nvim-ts-autotag", ft = {  'html', 'javascript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue'},
+config = function() require('nvim-ts-autotag').setup() end,
+    }
 
         -- Explorer
-        use {"kyazdani42/nvim-tree.lua", opt = true}
+        use {"kyazdani42/nvim-tree.lua", cmd = "NvimTreeToggle", event = {"BufReadPre","ColorScheme"}, config = function() require('lv-nvimtree') end}
         -- TODO remove when open on dir is supported by nvimtree
-        use "kevinhwang91/rnvimr"
+        use {"kevinhwang91/rnvimr", cmd = "RnvimrToggle", config = function() require("lv-rnvimr") end }
 
         -- use {'lukas-reineke/indent-blankline.nvim', opt=true, branch = 'lua'}
-        use {"lewis6991/gitsigns.nvim", opt = true}
-        -- use {"liuchengxu/vim-which-key", opt = true}
-        use {"folke/which-key.nvim", opt = true}
-        use {"ChristianChiarulli/dashboard-nvim", opt = true}
-        use {"windwp/nvim-autopairs", opt = true}
-        use {"terrortylor/nvim-comment", opt = true}
-        use {"kevinhwang91/nvim-bqf", opt = true}
+        use {"lewis6991/gitsigns.nvim", event = "BufReadPre", config = function() require("lv-gitsigns") end}
+        use {"folke/which-key.nvim", event = {"VimEnter", "BufReadPre"}, config = function() require('lv-which-key') end}
+        use {"ChristianChiarulli/dashboard-nvim", event = "VimEnter", config = function() require('lv-dashboard') end }
+        use {"windwp/nvim-autopairs", after = "nvim-treesitter", config = function() require("lv-autopairs") end}
+        use {"terrortylor/nvim-comment", cmd = "CommentToggle", keys ="gcc", config = function() require("lv-comment") end}
+        use {"kevinhwang91/nvim-bqf", event = "BufReadPre"}
 
         -- Color
-        use {"christianchiarulli/nvcode-color-schemes.vim", opt = true}
-
-        -- Icons
-        use {"kyazdani42/nvim-web-devicons", opt = true}
+        use {"christianchiarulli/nvcode-color-schemes.vim", event = "VimEnter"}
 
         -- Status Line and Bufferline
-        use {"glepnir/galaxyline.nvim", opt = true}
-        use {"romgrk/barbar.nvim", opt = true}
+        use {"glepnir/galaxyline.nvim", event = "VimEnter", config = function() require("lv-galaxyline") end}
+        use {"romgrk/barbar.nvim", event = "VimEnter",config = function() require("lv-barbar") end, requires = {"kyazdani42/nvim-web-devicons",event = "ColorScheme"}}
 
-        require_plugin("nvim-lspconfig")
-        require_plugin("lspsaga.nvim")
-        require_plugin("nvim-lspinstall")
-        require_plugin("friendly-snippets")
-        require_plugin("popup.nvim")
-        require_plugin("plenary.nvim")
-        require_plugin("telescope.nvim")
-        require_plugin("nvim-dap")
-        require_plugin("nvim-compe")
-        require_plugin("vim-vsnip")
-        require_plugin("nvim-treesitter")
-        require_plugin("nvim-ts-autotag")
-        require_plugin("nvim-tree.lua")
-        require_plugin("gitsigns.nvim")
-        require_plugin("which-key.nvim")
-        require_plugin("dashboard-nvim")
-        require_plugin("nvim-autopairs")
-        require_plugin("nvim-comment")
-        require_plugin("nvim-bqf")
-        require_plugin("nvcode-color-schemes.vim")
-        require_plugin("nvim-web-devicons")
-        require_plugin("galaxyline.nvim")
-        require_plugin("barbar.nvim")
     end
 )
