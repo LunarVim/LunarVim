cfd35c4b49233f8a97e3d4371e1bbaeb37991d14
tafryn <tafryn@gmail.com>
2021-05-25 11:16:40 -0700

    Enable the fzy file sorter for telescope

--- lua/lv-telescope/init.lua
+++ lua/lv-telescope/init.lua
@@ -15,7 +15,7 @@ require('telescope').setup {
         sorting_strategy = "descending",
         layout_strategy = "horizontal",
         layout_defaults = {horizontal = {mirror = false}, vertical = {mirror = false}},
-        file_sorter = require'telescope.sorters'.get_fuzzy_file,
+        file_sorter = require'telescope.sorters'.get_fzy_sorter,
         file_ignore_patterns = {},
         generic_sorter = require'telescope.sorters'.get_generic_fuzzy_sorter,
         shorten_path = true,
