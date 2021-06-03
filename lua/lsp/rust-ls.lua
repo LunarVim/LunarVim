fc7a1cad71200196996ab47d5b9d61a41181a04d
Robin Kautz <bob3000@users.noreply.github.com>
2021-06-03 14:22:03 +0200

    put back obsolete lsp config

---
+++ lua/lsp/rust-ls.lua
@@ -0,0 +1,6 @@
+-- the rust-tools plugin will configure this automatically
+-- require'lspconfig'.rust_analyzer.setup{
+--     cmd = {DATA_PATH .. "/lspinstall/rust/rust-analyzer"},
+--     on_attach = require'lsp'.common_on_attach
+-- }
+
