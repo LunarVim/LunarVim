3eee6dc3fd5e90a7f88f5fbc1a09cdce1691ea1d
Fabrizio Sestito <fabrizio.sestito@suse.com>
2021-06-18 22:11:30 +0200

    Change Workspace Symbol binding to Telescope DynamicFinder (#462)

--- lua/lv-which-key/init.lua
+++ lua/lv-which-key/init.lua
@@ -126,7 +126,7 @@ local mappings = {
         t = {"<cmd>LspTypeDefinition<cr>", "Type Definition"},
         x = {"<cmd>cclose<cr>", "Close Quickfix"},
         s = {"<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols"},
-        S = {"<cmd>Telescope lsp_workspace_symbols<cr>", "Workspace Symbols"}
+        S = {"<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", "Workspace Symbols"}
     },
     s = {
         name = "+Search",
