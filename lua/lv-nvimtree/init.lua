fea795ea342e01038ea37930a80274f7ac7b58fe
d0zzerr <sergensabahoglu@gmail.com>
2021-07-01 01:49:47 +0300

    nvimtree new keymap configuration

--- lua/lv-nvimtree/init.lua
+++ lua/lv-nvimtree/init.lua
@@ -28,64 +28,73 @@ M.config = function()
         folder_arrows = 1
     }
 
-    vim.g.nvim_tree_icons = {
-        default = '',
-        symlink = '',
-        git = {
-            unstaged = "",
-            staged = "S",
-            unmerged = "",
-            renamed = "➜",
-            deleted = "",
-            untracked = "U",
-            ignored = "◌"
-        },
-        folder = {
-            default = "",
-            open = "",
-            empty = "",
-            empty_open = "",
-            symlink = ""
-        }
-    }
+    g.nvim_tree_icons = {
+     default= '',
+     symlink= '',
+     git= {
+       unstaged= "",
+       staged= "✓",
+       unmerged= "",
+       renamed= "➜",
+       untracked= "★",
+       deleted= "",
+       ignored= "◌"
+       },
+     folder= {
+       arrow_open= "",
+       arrow_closed= "",
+       default= "",
+       open= "",
+       empty= "",
+       empty_open= "",
+       symlink= "",
+       symlink_open= "",
+       },
+       lsp= {
+         hint= "",
+         info= "",
+         warning= "",
+         error= "",
+       }
+     }
 
     local tree_cb = require"nvim-tree.config".nvim_tree_callback
 
     g.nvim_tree_bindings = {
-        ["u"] = ":lua require'some_module'.some_function()<cr>",
-        ["<CR>"] = tree_cb("edit"),
-        ["l"] = tree_cb("edit"),
-        ["o"] = tree_cb("edit"),
-        ["h"] = tree_cb("close_node"),
-        ["<2-LeftMouse>"] = tree_cb("edit"),
-        ["<2-RightMouse>"] = tree_cb("cd"),
-        ["<C-]>"] = tree_cb("cd"),
-        ["<C-v>"] = tree_cb("vsplit"),
-        ["v"] = tree_cb("vsplit"),
-        ["<C-x>"] = tree_cb("split"),
-        ["<C-t>"] = tree_cb("tabnew"),
-        ["<"] = tree_cb("prev_sibling"),
-        [">"] = tree_cb("next_sibling"),
-        ["<BS>"] = tree_cb("close_node"),
-        ["<S-CR>"] = tree_cb("close_node"),
-        ["<Tab>"] = tree_cb("preview"),
-        ["I"] = tree_cb("toggle_ignored"),
-        ["H"] = tree_cb("toggle_dotfiles"),
-        ["R"] = tree_cb("refresh"),
-        ["a"] = tree_cb("create"),
-        ["d"] = tree_cb("remove"),
-        ["r"] = tree_cb("rename"),
-        ["<C-r>"] = tree_cb("full_rename"),
-        ["x"] = tree_cb("cut"),
-        ["c"] = tree_cb("copy"),
-        ["p"] = tree_cb("paste"),
-        ["y"] = tree_cb("copy_name"),
-        ["Y"] = tree_cb("copy_path"),
-        ["gy"] = tree_cb("copy_absolute_path"),
-        ["[c"] = tree_cb("prev_git_item"),
-        ["]c"] = tree_cb("next_git_item"),
-        ["-"] = tree_cb("dir_up"),
-        ["q"] = tree_cb("close")
+        { key = {"u"}, cb = tree_cb(":lua require'some_module'.some_function()<cr>") },
+        { key= {"<CR>"}, cb = tree_cb("edit") },
+        { key= {"l"}, cb = tree_cb("edit") },
+        { key= {"o"}, cb = tree_cb("edit") },
+        { key= {"h"}, cb = tree_cb("close_node") },
+        { key= {"<2-LeftMouse>"}, cb = tree_cb("edit") },
+        { key= {"<2-RightMouse>"}, cb = tree_cb("cd") },
+        { key= {"<C-]>"}, cb = tree_cb("cd") },
+        { key= {"<C-v>"}, cb = tree_cb("vsplit") },
+        { key= {"v"}, cb = tree_cb("vsplit") },
+        { key= {"<C-x>"}, cb = tree_cb("split") },
+        { key= {"<C-t>"}, cb = tree_cb("tabnew") },
+        { key= {"<"}, cb = tree_cb("prev_sibling") },
+        { key= {">"}, cb = tree_cb("next_sibling") },
+        { key= {"<BS>"}, cb = tree_cb("close_node") },
+        { key= {"<S-CR>"}, cb = tree_cb("close_node") },
+        { key= {"<Tab>"}, cb = tree_cb("preview") },
+        { key= {"I"}, cb = tree_cb("toggle_ignored") },
+        { key= {"H"}, cb = tree_cb("toggle_dotfiles") },
+        { key= {"R"}, cb = tree_cb("refresh") },
+        { key= {"a"}, cb = tree_cb("create") },
+        { key= {"d"}, cb = tree_cb("remove") },
+        { key= {"r"}, cb = tree_cb("rename") },
+        { key= {"<C-r>"}, cb = tree_cb("full_rename") },
+        { key= {"x"}, cb = tree_cb("cut") },
+        { key= {"c"}, cb = tree_cb("copy") },
+        { key= {"p"}, cb = tree_cb("paste") },
+        { key= {"y"}, cb = tree_cb("copy_name") },
+        { key= {"Y"}, cb = tree_cb("copy_path") },
+        { key= {"gy"}, cb = tree_cb("copy_absolute_path") },
+        { key= {"[c"}, cb = tree_cb("prev_git_item") },
+        { key= {"]c"}, cb = tree_cb("next_git_item") },
+        { key= {"-"}, cb = tree_cb("dir_up") },
+        { key= {"q"}, cb = tree_cb("close") }
     }
 end
 
