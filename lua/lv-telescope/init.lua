905622e3d6333e868b8ec3d2d8b448fa4dc52421
rebuilt <memoryman51@hotmail.com>
2021-07-01 14:16:39 +0200

    Update telescope config in response to breaking changes in the latest telescope update

--- lua/lv-telescope/init.lua
+++ lua/lv-telescope/init.lua
@@ -11,7 +11,6 @@ require('telescope').setup {
             'rg', '--no-heading', '--with-filename', '--line-number',
             '--column', '--smart-case'
         },
-        prompt_position = "bottom",
         prompt_prefix = " ",
         selection_caret = " ",
         entry_prefix = "  ",
@@ -19,7 +18,10 @@ require('telescope').setup {
         selection_strategy = "reset",
         sorting_strategy = "descending",
         layout_strategy = "horizontal",
-        layout_defaults = {
+        layout_config = {
+            width = 0.75,
+            prompt_position = "bottom",
+            preview_cutoff = 120,
             horizontal = {mirror = false},
             vertical = {mirror = false}
         },
@@ -28,10 +30,6 @@ require('telescope').setup {
         generic_sorter = require'telescope.sorters'.get_generic_fuzzy_sorter,
         shorten_path = true,
         winblend = 0,
-        width = 0.75,
-        preview_cutoff = 120,
-        results_height = 1,
-        results_width = 0.8,
         border = {},
         borderchars = {'─', '│', '─', '│', '╭', '╮', '╯', '╰'},
         color_devicons = true,
