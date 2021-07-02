e0b16543773a95e2b9996858b7e88af8ce919a45
Chris <chrisatmachine@gmail.com>
2021-07-02 11:30:19 -0400

    fix lspsaga

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -34,7 +34,7 @@ return require("packer").startup(function(use)
 
     -- TODO refactor all of this (for now it works, but yes I know it could be wrapped in a simpler function)
     use {"neovim/nvim-lspconfig"}
-    use {"glepnir/lspsaga.nvim", event = "BufRead"}
+    use {"glepnir/lspsaga.nvim"}
     use {"kabouzeid/nvim-lspinstall"}
     -- Telescope
     use {"nvim-lua/popup.nvim"}
