07ae0097f6d84626c917a2664b29f8ca1f8eadaa
maminou <83711649+pulme@users.noreply.github.com>
2021-07-02 18:01:17 +0100

    shortmess

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,5 +1,5 @@
-vim.cmd('set iskeyword+=-') -- treat dash separated words as a word text object"
-vim.cmd('set shortmess+=c') -- Don't pass messages to |ins-completion-menu|.
+vim.cmd('set iskeyword+=-') -- treat dash separated words as a word text object" 
+vim.opt.shortmess:append("c") -- Don't pass messages to |ins-completion-menu|.
 vim.cmd('set inccommand=split') -- Make substitution work in realtime
 vim.o.hidden = O.hidden_files -- Required to keep multiple buffers open multiple buffers
 vim.o.title = true
