ac13c8a07abe784623e26a34bbda0fd67c7bdd0e
James Walmsley <james@fullfat-fs.co.uk>
2021-07-02 23:33:44 +0100

    [LSP] clang - Better defaults for clangd.

--- ftplugin/c.lua
+++ ftplugin/c.lua
@@ -1,5 +1,13 @@
+local clangd_flags = {"--background-index"};
+
+if O.lang.clang.cross_file_rename then
+   table.insert(clangd_flags, "--cross-file-rename")
+end
+
+table.insert(clangd_flags, "--header-insertion=" .. O.lang.clang.header_insertion)
+
 require'lspconfig'.clangd.setup {
-    cmd = {DATA_PATH .. "/lspinstall/cpp/clangd/bin/clangd"},
+    cmd = {DATA_PATH .. "/lspinstall/cpp/clangd/bin/clangd",  unpack(clangd_flags)},
     on_attach = require'lsp'.common_on_attach,
     handlers = {
         ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
