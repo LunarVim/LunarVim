c7d2ccabfb33172e7524e68a0a25bb759a022c88
Rob Wilson <roobert@gmail.com>
2021-07-04 09:50:59 +0100

    Minor fixes

--- lua/compile.lua
+++ lua/compile.lua
@@ -1,7 +1,12 @@
-local first_run = CACHE_PATH .. "first_run"
+function file_exists(name)
+  local f = io.open(name, "r")
+  return f ~= nil and io.close(f)
+end
+
+local first_run = CACHE_PATH .. "/first_run"
 
 -- Run PackerCompile after PackerInstall has completed during install phase
-if fn.isfile(first_run) != 0 then
+if file_exists(first_run) then
   vim.cmd "autocmd User PackerComplete ++once PackerCompile"
-  fn.system { "rm", first_run }
+  vim.fn.system { "rm", first_run }
 end
