a6df638cfaa1bd1db9534554eacc33a516ebf89d
Rob Wilson <roobert@gmail.com>
2021-07-04 11:48:03 +0100

    Switch to use `vim.fn.filereadable()`

--- lua/compile.lua
+++ lua/compile.lua
@@ -1,12 +1,7 @@
-function file_exists(name)
-  local f = io.open(name, "r")
-  return f ~= nil and io.close(f)
-end
-
 local first_run = CACHE_PATH .. "/first_run"
 
 -- Run PackerCompile after PackerInstall has completed during install phase
-if file_exists(first_run) then
+if vim.fn.filereadable(first_run) then
   vim.cmd "autocmd User PackerComplete ++once PackerCompile"
   vim.fn.system { "rm", first_run }
 end
