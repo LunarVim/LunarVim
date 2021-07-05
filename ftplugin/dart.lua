6ee73013c3f9c3907c42ab085050ec3dc7a42db6
Sanghyo Park <sanghyo@spareblock.com>
2021-07-05 15:49:53 +0900

    conflicts

--- ftplugin/dart.lua
+++ ftplugin/dart.lua
@@ -1,11 +1,23 @@
-require("lspconfig").dartls.setup {
-  cmd = { "dart", O.lang.dart.sdk_path, "--lsp" },
-  on_attach = require("lsp").common_on_attach,
-  init_options = {
-    closingLabels = false,
-    flutterOutline = false,
-    onlyAnalyzeProjectsWithOpenFiles = false,
-    outline = false,
-    suggestFromUnimportedLibraries = true,
-  },
-}
+if O.lang.dart.flutter_tools.active then
+  require("flutter-tools").setup{}
+else
+  require("lspconfig").dartls.setup {
+    cmd = { "dart", O.lang.dart.sdk_path, "--lsp" },
+    on_attach = require("lsp").common_on_attach,
+    init_options = {
+      closingLabels = false,
+      flutterOutline = false,
+      onlyAnalyzeProjectsWithOpenFiles = false,
+      outline = false,
+      suggestFromUnimportedLibraries = true,
+    },
+  }
+end
+
+if O.lang.dart.autoformat then
+  require('lv-utils').define_augroups({
+    _dart_autoformat = {
+      {'BufWritePre', '*.dart', 'lua vim.lsp.buf.formatting_sync(nil, 1000)'}
+    }
+  })
+end
