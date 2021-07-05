9f511bcb594b7e2461c97cb8182603928c773c2f
Rafael <rafaelcontacto@pm.me>
2021-07-04 22:14:01 -0300

    start formatting rules

--- ftplugin/css.lua
+++ ftplugin/css.lua
@@ -1,9 +1,10 @@
 -- npm install -g vscode-css-languageserver-bin
-require'lspconfig'.cssls.setup {
-    cmd = {
-        "node", DATA_PATH .. "/lspinstall/css/vscode-css/css-language-features/server/dist/node/cssServerMain.js",
-        "--stdio"
-    },
-    on_attach = require'lsp'.common_on_attach
+require("lspconfig").cssls.setup {
+  cmd = {
+    "node",
+    DATA_PATH .. "/lspinstall/css/vscode-css/css-language-features/server/dist/node/cssServerMain.js",
+    "--stdio",
+  },
+  on_attach = require("lsp").common_on_attach,
 }
-vim.cmd("setl ts=2 sw=2")
+vim.cmd "setl ts=2 sw=2"
