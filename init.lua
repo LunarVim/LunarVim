3dc0ae934735a0e445c75dc13e66679c1dc187c4
Rafael <rafaelcontacto@pm.me>
2021-07-05 15:40:01 -0300

    (fix) define_augroup add curly brackets

--- init.lua
+++ init.lua
@@ -16,9 +16,11 @@ end
 if O.format_on_save then
   require("lv-utils").define_augroups {
     autoformat = {
-      "BufWritePre",
-      "*",
-      [[try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry]],
+      {
+        "BufWritePre",
+        "*",
+        [[try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry]],
+      },
     },
   }
 end
