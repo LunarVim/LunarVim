a7fa8ded54c0ede72b577c2bc37c352966065a22
kjssad <kimsadomia@protonmail.com>
2021-07-05 16:58:22 +0800

    feat(lsp): add single type border to hover popup

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -41,6 +41,12 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   }
 )
 
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover, {
+    border = "single"
+  }
+)
+
 -- symbols for autocomplete
 vim.lsp.protocol.CompletionItemKind = {
     "   (Text) ",
