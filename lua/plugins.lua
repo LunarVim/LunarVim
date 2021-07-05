d4822c2920336287579c74a5b37f0aa08402b871
christianchiarulli <chrisatmachine@gmail.com>
2021-07-05 16:20:14 -0400

    no lazyloading barbar

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -124,7 +124,7 @@ return require("packer").startup(function(use)
       vim.api.nvim_set_keymap("n", "<S-TAB>", ":BufferPrevious<CR>", { noremap = true, silent = true })
       vim.api.nvim_set_keymap("n", "<S-x>", ":BufferClose<CR>", { noremap = true, silent = true })
     end,
-    event = "BufRead",
+    -- event = "BufRead",
   }
 
   -- use {
