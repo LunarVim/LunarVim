2a16537216ffbf30759d01cd8ca70f606aed3a47
Chris <chrisatmachine@gmail.com>
2021-07-07 11:44:10 -0400

    check if group exists before removing

--- lua/lv-neoformat/init.lua
+++ lua/lv-neoformat/init.lua
@@ -1,12 +1,18 @@
 -- autoformat
 if O.format_on_save then
-  require("lv-utils").define_augroups {
-    autoformat = {
-      {
-        "BufWritePre",
-        "*",
-        [[try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry]],
-      },
-    },
-  }
+	require("lv-utils").define_augroups({
+		autoformat = {
+			{
+				"BufWritePre",
+				"*",
+				[[try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry]],
+			},
+		},
+	})
+end
+
+if not O.format_on_save then
+	vim.cmd([[if exists('#autoformat#BufWritePre')
+	:autocmd! autoformat
+	endif]])
 end
