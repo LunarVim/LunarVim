b25cc0194ec4ef78f7242eae3eb49301d3264a1d
christianchiarulli <chrisatmachine@gmail.com>
2021-07-06 23:44:22 -0400

    remove that

--- utils/installer/install.sh
+++ utils/installer/install.sh
@@ -104,7 +104,6 @@ cloneconfig() {
     mv $HOME/.config/nvim/utils/installer/lv-config.example.lua $HOME/.config/nvim/lv-config.lua
     # mv $HOME/.config/nvim/utils/init.lua $HOME/.config/nvim/init.lua
     nvim -u $HOME/.config/nvim/init.lua +PackerInstall
-    nvim --headless +PackerSync
     # rm $HOME/.config/nvim/init.lua
     # mv $HOME/.config/nvim/init.lua.tmp $HOME/.config/nvim/init.lua
 }
