0c6771a385e3f2e4a9f1e5ae80eb7615e4fa99e0
christianchiarulli <chrisatmachine@gmail.com>
2021-07-06 22:55:11 -0400

    you can now toggle format on and off on the fly!

---
+++ lua/lv-neoformat/init.lua
@@ -0,0 +1,16 @@
+-- autoformat
+if O.format_on_save then
+  require("lv-utils").define_augroups {
+    autoformat = {
+      {
+        "BufWritePre",
+        "*",
+        [[try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry]],
+      },
+    },
+  }
+end
+
+if not O.format_on_save then
+  vim.cmd ":autocmd! autoformat"
+end
