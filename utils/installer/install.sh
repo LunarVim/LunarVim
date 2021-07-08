95808150c567d41dae959eef2725334e4ceaabdd
Chris <chrisatmachine@gmail.com>
2021-07-08 00:17:36 -0400

    testing blocking install

--- utils/installer/install.sh
+++ utils/installer/install.sh
@@ -100,11 +100,15 @@ installpacker() {
 
 cloneconfig() {
     echo "Cloning LunarVim configuration"
-    git clone --branch stable https://github.com/ChristianChiarulli/lunarvim.git ~/.config/nvim
+    git clone --branch rolling https://github.com/ChristianChiarulli/lunarvim.git ~/.config/nvim
     mv $HOME/.config/nvim/utils/installer/lv-config.example.lua $HOME/.config/nvim/lv-config.lua
     # mv $HOME/.config/nvim/utils/init.lua $HOME/.config/nvim/init.lua
     # nvim -u $HOME/.config/nvim/init.lua +PackerCompile +PackerInstall
-    nvim +PackerCompile +PackerInstall
+    nvim \
+        +'autocmd User PackerComplete sleep 100m | write ~/.config/nvim/packer.result | qall' \
+        +PackerInstall
+        cat ~/.config/nvim/packer.result | rg -v 'Press'
+    # nvim +PackerCompile +PackerInstall
     # rm $HOME/.config/nvim/init.lua
     # mv $HOME/.config/nvim/init.lua.tmp $HOME/.config/nvim/init.lua
 }
