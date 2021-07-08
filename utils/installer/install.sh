45d51e65d41cc35c6095f8d162969470fa15f6ac
christianchiarulli <chrisatmachine@gmail.com>
2021-07-08 18:14:12 -0400

    try with diff config

--- utils/installer/install.sh
+++ utils/installer/install.sh
@@ -101,7 +101,7 @@ installpacker() {
 cloneconfig() {
     echo "Cloning LunarVim configuration"
     git clone --branch master https://github.com/ChristianChiarulli/lunarvim.git ~/.config/nvim
-    cp $HOME/.config/nvim/utils/installer/lv-config.example.lua $HOME/.config/nvim/lv-config.lua
+    cp $HOME/.config/nvim/utils/installer/lv-config.example-no-ts.lua $HOME/.config/nvim/lv-config.lua
     nvim --headless \
         +'autocmd User PackerComplete sleep 100m | qall' \
         +PackerInstall
@@ -109,6 +109,8 @@ cloneconfig() {
         +'autocmd User PackerComplete sleep 10m | qall' \
         +PackerSync
     echo -e "\nCompile Complete"
+    rm $HOME/.config/nvim/lv-config.lua
+    cp $HOME/.config/nvim/utils/installer/lv-config.example.lua $HOME/.config/nvim/lv-config.lua
 }
 
 asktoinstallnode() {
