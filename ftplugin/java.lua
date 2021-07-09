2279cb3e65a56d4ddc584c42ba8d8525f7c53c19
Chris <chrisatmachine@gmail.com>
2021-07-08 23:53:54 -0400

    fix java on attach

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -23,13 +23,8 @@ local util = require "lspconfig/util"
 --         CONFIG_PATH.."/.debuggers/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar")
 -- };
 
-local on_attach = function(client, bufr)
-  -- require('jdtls').setup_dap()
-  require("lsp").common_on_attach(client, bufr)
-end
-
 require("lspconfig").jdtls.setup {
-  on_attach = on_attach,
+  on_attach = require("lsp").common_on_attach,
   cmd = { DATA_PATH .. "/lspinstall/java/jdtls.sh" },
   filetypes = { "java" },
   root_dir = util.root_pattern { ".git", "build.gradle", "pom.xml" },
