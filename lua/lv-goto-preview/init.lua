6f7b45e54315c1130d867bfa1b099e65c2914fac
MatthiasX86 <28681206+MatthiasX86@users.noreply.github.com>
2021-07-10 11:53:09 -0400

    explicitly setting key mapping

--- lua/lv-goto-preview/init.lua
+++ lua/lv-goto-preview/init.lua
@@ -4,13 +4,15 @@ M.config = function ()
   require('goto-preview').setup {
     width = 120; -- Width of the floating window
     height = 25; -- Height of the floating window
-    default_mappings = true; -- Bind default mappings
+    default_mappings = false; -- Bind default mappings
     debug = false; -- Print debug information
     opacity = nil; -- 0-100 opacity level of the floating window where 100 is fully transparent.
     post_open_hook = nil -- A function taking two arguments, a buffer and a window to be ran as a hook.
   }
 end
 
+-- could have just used "default_mappings = true" setup option
+-- but this way we can explicitly see mappings
 vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>")
 vim.cmd("nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>")
 vim.cmd("nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>")
