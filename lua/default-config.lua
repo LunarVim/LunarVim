ed973fdb9d1df908449f99e67037820825fd4dce
Uzaaft <48220549+Uzaaft@users.noreply.github.com>
2021-07-11 20:15:03 +0200

    Bugfix: Fixed edition issue with rustfmt. (#881)

--- lua/default-config.lua
+++ lua/default-config.lua
@@ -226,7 +226,7 @@ O = {
       -- @usage can be clippy
       formatter = {
         exe = "rustfmt",
-        args = { "--emit=stdout" },
+        args = { "--emit=stdout", "--edition=2018" },
       },
       linter = "",
       diagnostics = {
