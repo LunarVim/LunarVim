7536d223c7e8544720834fd895b8e76425c84d10
Resat Kubilay Kandemir <kubilaykandemir@outlook.com>
2021-07-12 02:21:06 +1000

    Now, cssls and autocomplete now works with css scss sass files

--- ftplugin/sass.lua
+++ ftplugin/sass.lua
@@ -1,16 +1,1 @@
-if not require("lv-utils").check_lsp_client_active "cssls" then
-  local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities.textDocument.completion.completionItem.snippetSupport = true
-  -- npm install -g vscode-css-languageserver-bin
-  require("lspconfig").cssls.setup {
-    cmd = {
-      "node",
-      DATA_PATH .. "/lspinstall/css/vscode-css/css-language-features/server/dist/node/cssServerMain.js",
-      "--stdio",
-    },
-    on_attach = require("lsp").common_on_attach,
-    capabilities = capabilities,
-  }
-end
-
-vim.cmd "setl ts=2 sw=2"
+css.lua
