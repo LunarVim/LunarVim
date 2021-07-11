50bb8acce4fd605bb0687f96143d326366df2701
christianchiarulli <chrisatmachine@gmail.com>
2021-07-11 15:08:31 -0400

    support colorschemes from lunarvim colorschemes

--- lua/lv-galaxyline/init.lua
+++ lua/lv-galaxyline/init.lua
@@ -45,8 +45,8 @@ table.insert(gls.left, {
       vim.api.nvim_command("hi GalaxyViMode guifg=" .. mode_color[vim.fn.mode()])
       return "▊"
     end,
-    separator_highlight = { "NONE", colors.bg },
-    highlight = { "NONE", colors.bg },
+    separator_highlight = { "NONE", colors.alt_bg },
+    highlight = { "NONE", colors.alt_bg },
   },
 })
 -- print(vim.fn.getbufvar(0, 'ts'))
@@ -59,8 +59,8 @@ table.insert(gls.left, {
     end,
     condition = condition.check_git_workspace,
     separator = " ",
-    separator_highlight = { "NONE", colors.bg },
-    highlight = { colors.orange, colors.bg },
+    separator_highlight = { "NONE", colors.alt_bg },
+    highlight = { colors.orange, colors.alt_bg },
   },
 })
 
@@ -69,8 +69,8 @@ table.insert(gls.left, {
     provider = "GitBranch",
     condition = condition.check_git_workspace,
     separator = " ",
-    separator_highlight = { "NONE", colors.bg },
-    highlight = { colors.grey, colors.bg },
+    separator_highlight = { "NONE", colors.alt_bg },
+    highlight = { colors.grey, colors.alt_bg },
   },
 })
 
@@ -79,7 +79,7 @@ table.insert(gls.left, {
     provider = "DiffAdd",
     condition = condition.hide_in_width,
     icon = "  ",
-    highlight = { colors.green, colors.bg },
+    highlight = { colors.green, colors.alt_bg },
   },
 })
 
@@ -88,7 +88,7 @@ table.insert(gls.left, {
     provider = "DiffModified",
     condition = condition.hide_in_width,
     icon = " 柳",
-    highlight = { colors.blue, colors.bg },
+    highlight = { colors.blue, colors.alt_bg },
   },
 })
 
@@ -97,7 +97,7 @@ table.insert(gls.left, {
     provider = "DiffRemove",
     condition = condition.hide_in_width,
     icon = "  ",
-    highlight = { colors.red, colors.bg },
+    highlight = { colors.red, colors.alt_bg },
   },
 })
 
@@ -106,7 +106,7 @@ table.insert(gls.left, {
     provider = function()
       return " "
     end,
-    highlight = { colors.grey, colors.bg },
+    highlight = { colors.grey, colors.alt_bg },
   },
 })
 -- get output from shell command
@@ -151,7 +151,7 @@ table.insert(gls.left, {
   VirtualEnv = {
     provider = PythonEnv,
     event = "BufEnter",
-    highlight = { colors.green, colors.bg },
+    highlight = { colors.green, colors.alt_bg },
   },
 })
 
@@ -159,14 +159,14 @@ table.insert(gls.right, {
   DiagnosticError = {
     provider = "DiagnosticError",
     icon = "  ",
-    highlight = { colors.red, colors.bg },
+    highlight = { colors.red, colors.alt_bg },
   },
 })
 table.insert(gls.right, {
   DiagnosticWarn = {
     provider = "DiagnosticWarn",
     icon = "  ",
-    highlight = { colors.orange, colors.bg },
+    highlight = { colors.orange, colors.alt_bg },
   },
 })
 
@@ -174,7 +174,7 @@ table.insert(gls.right, {
   DiagnosticInfo = {
     provider = "DiagnosticInfo",
     icon = "  ",
-    highlight = { colors.yellow, colors.bg },
+    highlight = { colors.yellow, colors.alt_bg },
   },
 })
 
@@ -182,7 +182,7 @@ table.insert(gls.right, {
   DiagnosticHint = {
     provider = "DiagnosticHint",
     icon = "  ",
-    highlight = { colors.blue, colors.bg },
+    highlight = { colors.blue, colors.alt_bg },
   },
 })
 
@@ -195,8 +195,8 @@ table.insert(gls.right, {
       return ""
     end,
     separator = " ",
-    separator_highlight = { "NONE", colors.bg },
-    highlight = { colors.green, colors.bg },
+    separator_highlight = { "NONE", colors.alt_bg },
+    highlight = { colors.green, colors.alt_bg },
   },
 })
 
@@ -239,7 +239,7 @@ table.insert(gls.right, {
       return true
     end,
     icon = " ",
-    highlight = { colors.grey, colors.bg },
+    highlight = { colors.grey, colors.alt_bg },
   },
 })
 
@@ -247,8 +247,8 @@ table.insert(gls.right, {
   LineInfo = {
     provider = "LineColumn",
     separator = "  ",
-    separator_highlight = { "NONE", colors.bg },
-    highlight = { colors.grey, colors.bg },
+    separator_highlight = { "NONE", colors.alt_bg },
+    highlight = { colors.grey, colors.alt_bg },
   },
 })
 
@@ -256,8 +256,8 @@ table.insert(gls.right, {
   PerCent = {
     provider = "LinePercent",
     separator = " ",
-    separator_highlight = { "NONE", colors.bg },
-    highlight = { colors.grey, colors.bg },
+    separator_highlight = { "NONE", colors.alt_bg },
+    highlight = { colors.grey, colors.alt_bg },
   },
 })
 
@@ -268,8 +268,8 @@ table.insert(gls.right, {
     end,
     condition = condition.hide_in_width,
     separator = " ",
-    separator_highlight = { "NONE", colors.bg },
-    highlight = { colors.grey, colors.bg },
+    separator_highlight = { "NONE", colors.alt_bg },
+    highlight = { colors.grey, colors.alt_bg },
   },
 })
 
@@ -278,8 +278,8 @@ table.insert(gls.right, {
     provider = "FileTypeName",
     condition = condition.hide_in_width,
     separator = " ",
-    separator_highlight = { "NONE", colors.bg },
-    highlight = { colors.grey, colors.bg },
+    separator_highlight = { "NONE", colors.alt_bg },
+    highlight = { colors.grey, colors.alt_bg },
   },
 })
 
@@ -288,8 +288,8 @@ table.insert(gls.right, {
     provider = "FileEncode",
     condition = condition.hide_in_width,
     separator = " ",
-    separator_highlight = { "NONE", colors.bg },
-    highlight = { colors.grey, colors.bg },
+    separator_highlight = { "NONE", colors.alt_bg },
+    highlight = { colors.grey, colors.alt_bg },
   },
 })
 
@@ -299,8 +299,8 @@ table.insert(gls.right, {
       return " "
     end,
     separator = " ",
-    separator_highlight = { "NONE", colors.bg },
-    highlight = { colors.grey, colors.bg },
+    separator_highlight = { "NONE", colors.alt_bg },
+    highlight = { colors.grey, colors.alt_bg },
   },
 })
 
@@ -308,8 +308,8 @@ table.insert(gls.short_line_left, {
   BufferType = {
     provider = "FileTypeName",
     separator = " ",
-    separator_highlight = { "NONE", colors.bg },
-    highlight = { colors.grey, colors.bg },
+    separator_highlight = { "NONE", colors.alt_bg },
+    highlight = { colors.grey, colors.alt_bg },
   },
 })
 
@@ -317,8 +317,8 @@ table.insert(gls.short_line_left, {
   SFileName = {
     provider = "SFileName",
     condition = condition.buffer_not_empty,
-    highlight = { colors.grey, colors.bg },
+    highlight = { colors.grey, colors.alt_bg },
   },
 })
 
---table.insert(gls.short_line_right[1] = {BufferIcon = {provider = 'BufferIcon', highlight = {colors.grey, colors.bg}}})
+--table.insert(gls.short_line_right[1] = {BufferIcon = {provider = 'BufferIcon', highlight = {colors.grey, colors.alt_bg}}})
