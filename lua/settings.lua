afe079d821f39763aa46990234a0699fb3ba5fc3
James Walmsley <james@fullfat-fs.co.uk>
2021-07-11 09:21:21 +0100

    [SETTINGS] Ensure <Leader> is mapped before plugin configs are sourced. (#863)

--- lua/settings.lua
+++ lua/settings.lua
@@ -43,6 +43,13 @@ local disabled_built_ins = {
   "spellfile_plugin",
   -- 'matchit', 'matchparen', 'shada_plugin',
 }
+
+if O.leader_key == " " or O.leader_key == "space" then
+  vim.g.mapleader = ' '
+else
+  vim.g.mapleader = O.leader_key
+end
+
 for _, plugin in pairs(disabled_built_ins) do
   vim.g["loaded_" .. plugin] = 1
 end
