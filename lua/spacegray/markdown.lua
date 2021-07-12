e780972b3061bb2bdd49639870aa7a1b7727cba4
christianchiarulli <chrisatmachine@gmail.com>
2021-07-12 12:51:49 -0400

    spacegray now in lua

---
+++ lua/spacegray/markdown.lua
@@ -0,0 +1,27 @@
+local markdown = {
+		markdownBlockquote = {fg = C.accent, },
+		markdownBold = {fg = C.yellow, style = "bold", },
+		markdownCode = {fg = C.green, },
+		markdownCodeBlock = {fg = C.green, },
+		markdownCodeDelimiter = {fg = C.green, },
+		markdownH1 = {fg = C.blue, },
+		markdownH2 = {fg = C.blue, },
+		markdownH3 = {fg = C.blue, },
+		markdownH4 = {fg = C.blue, },
+		markdownH5 = {fg = C.blue, },
+		markdownH6 = {fg = C.blue, },
+		markdownHeadingDelimiter = {fg = C.red, },
+		markdownHeadingRule = {fg = C.accent, },
+		markdownId = {fg = C.purple, },
+		markdownIdDeclaration = {fg = C.blue, },
+		markdownIdDelimiter = {fg = C.light_gray, },
+		markdownLinkDelimiter = {fg = C.light_gray, },
+		markdownItalic = {style = "italic", },
+		markdownLinkText = {fg = C.blue, },
+		markdownListMarker = {fg = C.red, },
+		markdownOrderedListMarker = {fg = C.red, },
+		markdownRule = {fg = C.accent, },
+		markdownUrl = {fg = C.cyan, style = "underline", },
+}
+
+return markdown
