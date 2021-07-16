b3748fc093010f61a2ac8bac0778fef93ac35966
Vaverka <jvaverka@protonmail.com>
2021-07-16 10:33:04 -0400

    format ftdetect/julia.lua

--- ftdetect/julia.lua
+++ ftdetect/julia.lua
@@ -1,9 +1,1 @@
--- Since lspinstall doesn't support Julia yet,
--- detect *jl files & trigger LanguageServer.jl
--- and add settings that make writing Julia nice
-vim.cmd([[
-  au BufRead,BufNewFile *.jl set filetype=julia
-  let g:latex_to_unicode_auto = 1
-  let g:latex_to_unicode_tab = 1
-  autocmd Filetype julia setlocal omnifunc=v:lua.vim.lsp.omnifunc
-]])
+vim.cmd [[ au BufRead,BufNewFile *.jl set filetype=julia ]]
