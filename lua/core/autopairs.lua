15b276db3c425e60dc280661387778d4f74cc452
rebuilt <memoryman51@hotmail.com>
2021-07-16 02:54:26 +0200

    set config to nil instead of false and turn conditional logic into early return

--- lua/core/autopairs.lua
+++ lua/core/autopairs.lua
@@ -5,63 +5,63 @@ local M = {}
 
 M.config = function()
   O.plugin.autopairs = {
-    config = false,
+    config = nil,
   }
 end
 
 M.setup = function()
   if O.plugin.autopairs.config then
-    O.plugin.autopairs.config()
-  else
-    local status_ok, autopairs = pcall(require, "nvim-autopairs")
-    if not status_ok then
-      return false
-    end
-    local npairs = require "nvim-autopairs"
-    local Rule = require "nvim-autopairs.rule"
+    return O.plugin.autopairs.config()
+  end
 
-    -- skip it, if you use another global object
-    _G.MUtils = {}
+  local status_ok, autopairs = pcall(require, "nvim-autopairs")
+  if not status_ok then
+    return false
+  end
+  local npairs = require "nvim-autopairs"
+  local Rule = require "nvim-autopairs.rule"
 
-    vim.g.completion_confirm_key = ""
-    MUtils.completion_confirm = function()
-      if vim.fn.pumvisible() ~= 0 then
-        if vim.fn.complete_info()["selected"] ~= -1 then
-          return vim.fn["compe#confirm"](npairs.esc "<cr>")
-        else
-          return npairs.esc "<cr>"
-        end
+  -- skip it, if you use another global object
+  _G.MUtils = {}
+
+  vim.g.completion_confirm_key = ""
+  MUtils.completion_confirm = function()
+    if vim.fn.pumvisible() ~= 0 then
+      if vim.fn.complete_info()["selected"] ~= -1 then
+        return vim.fn["compe#confirm"](npairs.esc "<cr>")
       else
-        return npairs.autopairs_cr()
+        return npairs.esc "<cr>"
       end
+    else
+      return npairs.autopairs_cr()
     end
+  end
 
-    if package.loaded["compe"] then
-      require("nvim-autopairs.completion.compe").setup {
-        map_cr = true, --  map <CR> on insert mode
-        map_complete = true, -- it will auto insert `(` after select function or method item
-      }
-    end
-
-    npairs.setup {
-      check_ts = true,
-      ts_config = {
-        lua = { "string" }, -- it will not add pair on that treesitter node
-        javascript = { "template_string" },
-        java = false, -- don't check treesitter on java
-      },
-    }
-
-    require("nvim-treesitter.configs").setup { autopairs = { enable = true } }
-
-    local ts_conds = require "nvim-autopairs.ts-conds"
-
-    -- press % => %% is only inside comment or string
-    npairs.add_rules {
-      Rule("%", "%", "lua"):with_pair(ts_conds.is_ts_node { "string", "comment" }),
-      Rule("$", "$", "lua"):with_pair(ts_conds.is_not_ts_node { "function" }),
+  if package.loaded["compe"] then
+    require("nvim-autopairs.completion.compe").setup {
+      map_cr = true, --  map <CR> on insert mode
+      map_complete = true, -- it will auto insert `(` after select function or method item
     }
   end
+
+  npairs.setup {
+    check_ts = true,
+    ts_config = {
+      lua = { "string" }, -- it will not add pair on that treesitter node
+      javascript = { "template_string" },
+      java = false, -- don't check treesitter on java
+    },
+  }
+
+  require("nvim-treesitter.configs").setup { autopairs = { enable = true } }
+
+  local ts_conds = require "nvim-autopairs.ts-conds"
+
+  -- press % => %% is only inside comment or string
+  npairs.add_rules {
+    Rule("%", "%", "lua"):with_pair(ts_conds.is_ts_node { "string", "comment" }),
+    Rule("$", "$", "lua"):with_pair(ts_conds.is_not_ts_node { "function" }),
+  }
 end
 
 return M
