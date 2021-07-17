5d21a1dbd8e7c4eb8b82f85df75433dcff86d930
Abouzar Parvan <abzcoding@users.noreply.github.com>
2021-07-18 03:41:57 +0430

    a better path_display default for telescope (#1009)

--- lua/core/telescope.lua
+++ lua/core/telescope.lua
@@ -33,7 +33,7 @@ M.config = function()
       file_sorter = require("telescope.sorters").get_fzy_sorter,
       file_ignore_patterns = {},
       generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
-      path_display = { "shorten" },
+      path_display = { shorten = 5 },
       winblend = 0,
       border = {},
       borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
