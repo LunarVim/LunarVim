8f8f8bf13a24cf260d660253e88fd026b7dc68d4
Pasi Bergman <pasi.bergman@iki.fi>
2021-07-18 18:33:58 +0300

    feature! Add Vue formatter (prettier) setup

--- lua/lang/vue.lua
+++ lua/lang/vue.lua
@@ -1,13 +1,50 @@
 local M = {}
 
 M.config = function()
-  -- TODO: implement config for language
-  return "No config available!"
+  O.lang.vue = {
+    formatter = {
+      exe = "prettier",
+      args = {
+        "--stdin-filepath",
+        "${FILEPATH}",
+      },
+      stdin = true,
+    },
+    auto_import = true,
+  }
 end
 
 M.format = function()
-  -- TODO: implement formatter for language
-  return "No formatter available!"
+  vim.cmd "let proj = FindRootDirectory()"
+  local root_dir = vim.api.nvim_get_var "proj"
+
+  -- use the global formatter if you didn't find the local one
+  local formatter_instance = root_dir .. "/node_modules/.bin/" .. O.lang.vue.formatter.exe
+  if vim.fn.executable(formatter_instance) ~= 1 then
+    formatter_instance = O.lang.vue.formatter.exe
+  end
+
+  local ft = vim.bo.filetype
+  O.formatters.filetype[ft] = {
+    function()
+      local buffer_filepath = vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))
+
+      local formatter_args = O.lang.vue.formatter.args
+      for i = 1, #formatter_args do
+        formatter_args[i], _ = string.gsub(formatter_args[i], "${FILEPATH}", buffer_filepath)
+      end
+
+      return {
+        exe = formatter_instance,
+        args = formatter_args,
+        stdin = O.lang.vue.formatter.stdin,
+      }
+    end,
+  }
+  require("formatter.config").set_defaults {
+    logging = false,
+    filetype = O.formatters.filetype,
+  }
 end
 
 M.lint = function()
@@ -24,8 +61,9 @@ M.lsp = function()
   require("lspconfig").vuels.setup {
     cmd = { DATA_PATH .. "/lspinstall/vue/node_modules/.bin/vls", "--stdio" },
     on_attach = require("lsp").common_on_attach,
-    root_dir = require("lspconfig").util.root_pattern(".git", "vue.config.js", "package.json", "yarn.lock"),
   }
+
+  require("lsp.ts-fmt-lint").setup()
 end
 
 M.dap = function()
