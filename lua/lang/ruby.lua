cafe743d5dcf5bc3a8a7b031b5e064eb89d16b2c
Will Leinweber <will@bitfission.com>
2021-07-19 13:17:29 -0700

    Add sorbet lsp for ruby

--- lua/lang/ruby.lua
+++ lua/lang/ruby.lua
@@ -41,24 +41,26 @@ M.lint = function()
 end
 
 M.lsp = function()
-  if require("lv-utils").check_lsp_client_active "solargraph" then
-    return
+  if not require("lv-utils").check_lsp_client_active "sorbet" then
+    require("lspconfig").sorbet.setup {}
   end
 
-  -- If you are using rvm, make sure to change below configuration
-  require("lspconfig").solargraph.setup {
-    cmd = { DATA_PATH .. "/lspinstall/ruby/solargraph/solargraph", "stdio" },
-    on_attach = require("lsp").common_on_attach,
-    handlers = {
-      ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-        virtual_text = O.lang.ruby.diagnostics.virtual_text,
-        signs = O.lang.ruby.diagnostics.signs,
-        underline = O.lang.ruby.diagnostics.underline,
-        update_in_insert = true,
-      }),
-    },
-    filetypes = O.lang.ruby.filetypes,
-  }
+  if not require("lv-utils").check_lsp_client_active "solargraph" then
+    -- If you are using rvm, make sure to change below configuration
+    require("lspconfig").solargraph.setup {
+      cmd = { DATA_PATH .. "/lspinstall/ruby/solargraph/solargraph", "stdio" },
+      on_attach = require("lsp").common_on_attach,
+      handlers = {
+        ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+          virtual_text = O.lang.ruby.diagnostics.virtual_text,
+          signs = O.lang.ruby.diagnostics.signs,
+          underline = O.lang.ruby.diagnostics.underline,
+          update_in_insert = true,
+        }),
+      },
+      filetypes = O.lang.ruby.filetypes,
+    }
+  end
 end
 
 M.dap = function()
