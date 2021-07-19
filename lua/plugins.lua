e50a586a3a08b29ef9dac3094ee563d989a68190
Abouzar Parvan <abzcoding@users.noreply.github.com>
2021-07-19 11:47:49 +0430

    toggle() already follows the file, no need for find_file() (#1020)

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -74,7 +74,7 @@ return {
     "kyazdani42/nvim-tree.lua",
     -- event = "BufWinOpen",
     -- cmd = "NvimTreeToggle",
-    commit = "fd7f60e242205ea9efc9649101c81a07d5f458bb",
+    -- commit = "fd7f60e242205ea9efc9649101c81a07d5f458bb",
     config = function()
       require("core.nvimtree").setup()
     end,
