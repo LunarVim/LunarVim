30320ae00a1abddee063daf977cdef75e7f8e49d
Chris <chrisatmachine@gmail.com>
2021-07-22 18:36:36 -0400

    cleanup

--- lua/lsp/angular-ls.lua
+++
@@ -1,6 +0,0 @@
--- TODO: find correct root filetype
--- :LspInstall angular
-require("lspconfig").angularls.setup {
-  cmd = { DATA_PATH .. "/lspinstall/angular/node_modules/@angular/language-server/bin/ngserver", "--stdio" },
-  on_attach = require("lsp").common_on_attach,
-}
