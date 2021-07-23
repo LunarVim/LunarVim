c5205918ca668f5a1153b583d6a77e8419f64fd2
Chris <chrisatmachine@gmail.com>
2021-07-23 02:13:41 -0400

    disable scala

--- ftplugin/scala.lua
+++ ftplugin/scala.lua
@@ -1,4 +1,4 @@
-require("lang.scala").format()
-require("lang.scala").lint()
-require("lang.scala").lsp()
-require("lang.scala").dap()
+-- require("lang.scala").format()
+-- require("lang.scala").lint()
+-- require("lang.scala").lsp()
+-- require("lang.scala").dap()
