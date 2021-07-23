0884dcd84670bc097c34253e983d2cde9c209dfa
Abouzar Parvan <abzcoding@users.noreply.github.com>
2021-07-23 19:29:44 +0000

    Fix the colorscheme issue on new installations

--- init.lua
+++ init.lua
@@ -23,6 +23,7 @@ local plugins = require "plugins"
 local plugin_loader = require("plugin-loader").init()
 plugin_loader:load { plugins, O.user_plugins }
 vim.g.colors_name = O.colorscheme -- Colorscheme must get called after plugins are loaded or it will break new installs.
+vim.cmd("colorscheme " .. O.colorscheme)
 
 require "settings"
 require "lv-utils"
