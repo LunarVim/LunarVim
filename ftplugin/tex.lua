62753e02125f071b0c1e751a15f2fb942b5b5fc9
Chris <chrisatmachine@gmail.com>
2021-07-23 02:13:33 -0400

    disable tex

--- ftplugin/tex.lua
+++ ftplugin/tex.lua
@@ -1,4 +1,4 @@
-require("lang.tex").format()
-require("lang.tex").lint()
-require("lang.tex").lsp()
-require("lang.tex").dap()
+-- require("lang.tex").format()
+-- require("lang.tex").lint()
+-- require("lang.tex").lsp()
+-- require("lang.tex").dap()
