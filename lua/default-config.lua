fd88205be5141b23b9fab100f9e2a8d5951aab1e
christianchiarulli <chrisatmachine@gmail.com>
2021-07-24 15:52:28 -0400

    outside of object

--- lua/default-config.lua
+++ lua/default-config.lua
@@ -4,6 +4,15 @@ CACHE_PATH = vim.fn.stdpath "cache"
 TERMINAL = vim.fn.expand "$TERMINAL"
 USER = vim.fn.expand "$USER"
 
+local schemas = nil
+
+local common_on_attach = require("lsp").common_on_attach
+local common_capabilities = require("lsp").common_capabilities()
+local status_ok, jsonls_settings = pcall(require, "nlspsettings.jsonls")
+if status_ok then
+  schemas = jsonls_settings.get_default_schemas(),
+end
+
 lvim = {
   keys = {
     leader_key = "space",
@@ -58,14 +67,6 @@ lvim = {
   },
 }
 
-local schemas = nil
-
-local common_on_attach = require("lsp").common_on_attach
-local common_capabilities = require("lsp").common_capabilities()
-local status_ok, jsonls_settings = pcall(require, "nlspsettings.jsonls")
-if status_ok then
-  schemas = jsonls_settings.get_default_schemas(),
-end
 
 -- TODO move all of this into lang specific files, only require when using
 lvim.lang = {
