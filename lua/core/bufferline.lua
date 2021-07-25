0dec3eee533e11a9b975c0b98e755f9332899b10
Kai <luxuspur@gmail.com>
2021-07-25 06:28:40 +0200

    remove shift - x  from buffer line (#1073)
    fixes issue https://github.com/ChristianChiarulli/LunarVim/issues/1053

--- lua/core/bufferline.lua
+++ lua/core/bufferline.lua
@@ -1,4 +1,3 @@
-vim.api.nvim_set_keymap("n", "<S-x>", ":BufferClose<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<S-l>", ":BufferNext<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<S-h>", ":BufferPrevious<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>c", ":BufferClose<CR>", { noremap = true, silent = true })
