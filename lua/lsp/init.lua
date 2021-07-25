229720a1f6971a7f89f7f09c58a0eedfb13cfb61
Abouzar Parvan <abzcoding@users.noreply.github.com>
2021-07-25 11:36:22 +0000

    removd unused loop variable index

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -281,7 +281,7 @@ local function is_string(t)
 end
 
 local function has_value(tab, val)
-  for index, value in ipairs(tab) do
+  for _, value in ipairs(tab) do
     if value == val then
       return true
     end
