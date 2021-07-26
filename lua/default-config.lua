d4af6cce0ec83da58ae2de2606aef5d89107bfa1
ichigo--gyuunyuu <15829692+ichigo-gyuunyuu@users.noreply.github.com>
2021-07-26 20:23:55 +0530

    lsp fields for tex files

--- lua/default-config.lua
+++ lua/default-config.lua
@@ -749,6 +749,21 @@ lvim.lang = {
       },
     },
   },
+  tex = {
+    formatter = {
+      exe = "latexindent",
+      args = {},
+    },
+    linters = { "chketx" },
+    lsp = {
+      provider = "texlab",
+      setup = {
+        cmd = { DATA_PATH .. "/lspinstall/latex/texlab" },
+        on_attach = common_on_attach,
+        capabilities = common_capabilities,
+      },
+    },
+  },
   typescript = {
     -- @usage can be prettier or eslint
     formatter = {
