4d881bd4c3281fbb3af85ebcecc72bc0fea2ebd8
UltimateOmega <16052180+UltimateOmega@users.noreply.github.com>
2021-07-27 00:43:06 +0430

    Update uninstall.sh

--- utils/installer/uninstall.sh
+++ utils/installer/uninstall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-rm -rf .local/share/lunarvim
+rm -rf ~/.local/share/lunarvim
 sudo rm /usr/local/bin/lvim
-rm -rf .local/share/applications/lvim.desktop
+rm -rf ~/.local/share/applications/lvim.desktop
