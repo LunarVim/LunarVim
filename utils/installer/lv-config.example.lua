44dc05c242f672e263d7f13db9135b691f41fe8f
abzcoding <abzcoding@gmail.com>
2021-07-27 22:21:25 +0430

    add some sample which-key mappings to lv-config.lua and README.md

--- utils/installer/lv-config.example.lua
+++ utils/installer/lv-config.example.lua
@@ -73,3 +73,13 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- }
 
 -- Additional Leader bindings for WhichKey
+-- lvim.builtin.which_key.mappings["P"] = { "<cmd>lua require'telescope'.extensions.project.project{}<CR>", "Projects" }
+-- lvim.builtin.which_key.mappings["t"] = {
+--   name = "+Trouble",
+--   r = { "<cmd>Trouble lsp_references<cr>", "References" },
+--   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+--   d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnosticss" },
+--   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+--   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+--   w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnosticss" },
+-- }
