78dd92b6168d0e602979c3866e7d30ef219d1fc6
Chris <chris@macbook.local>
2021-07-28 17:15:35 -0400

    more refactor

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -4,6 +4,7 @@ function lsp_config.config()
   require("lsp.kind").setup()
   require("lsp.handlers").setup()
   require("lsp.signs").setup()
+  require("ls.keybindings").setup()
 end
 
 -- My font didn't like this :/
@@ -13,30 +14,6 @@ end
 --   '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({ show_header = false, border = { { "🭽", "FloatBorder" }, { "▔", "FloatBorder" }, { "🭾", "FloatBorder" }, { "▕", "FloatBorder" }, { "🭿", "FloatBorder" }, { "▁", "FloatBorder" }, { "🭼", "FloatBorder" }, { "▏", "FloatBorder" }, } })<CR>',
 --   { noremap = true, silent = true }
 -- )
-
-function lsp_config.setup_default_bindings()
-  if lvim.lsp.default_keybinds then
-    vim.cmd "nnoremap <silent> gd <cmd>lua vim.lsp.buf.definition()<CR>"
-    vim.cmd "nnoremap <silent> gD <cmd>lua vim.lsp.buf.declaration()<CR>"
-    vim.cmd "nnoremap <silent> gr <cmd>lua vim.lsp.buf.references()<CR>"
-    vim.cmd "nnoremap <silent> gi <cmd>lua vim.lsp.buf.implementation()<CR>"
-    vim.api.nvim_set_keymap(
-      "n",
-      "gl",
-      '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({ show_header = false, border = "single" })<CR>',
-      { noremap = true, silent = true }
-    )
-
-    vim.cmd "nnoremap <silent> gp <cmd>lua require'lsp.utils'.PeekDefinition()<CR>"
-    vim.cmd "nnoremap <silent> K :lua vim.lsp.buf.hover()<CR>"
-    vim.cmd "nnoremap <silent> <C-p> :lua vim.lsp.diagnostic.goto_prev({popup_opts = {border = lvim.lsp.popup_border}})<CR>"
-    vim.cmd "nnoremap <silent> <C-n> :lua vim.lsp.diagnostic.goto_next({popup_opts = {border = lvim.lsp.popup_border}})<CR>"
-    -- vim.cmd "nnoremap <silent> <tab> <cmd>lua vim.lsp.buf.signature_help()<CR>"
-    -- scroll down hover doc or scroll in definition preview
-    -- scroll up hover doc
-    -- vim.cmd 'command! -nargs=0 LspVirtualTextToggle lua require("lsp/virtual_text").toggle()'
-  end
-end
 
 local function no_formatter_on_attach(client, bufnr)
   if lvim.lsp.on_attach_callback then
