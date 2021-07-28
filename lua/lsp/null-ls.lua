2180cfcbd3cecdb77ff26b59b2477e574f70e1c4
Luc Sinet <luc.sinet@gmail.com>
2021-07-28 18:11:05 +0200

    Improve readability/logic of lsp/null-ls.lua

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -5,24 +5,6 @@ local sources = {}
 
 local local_executables = { "prettier", "prettierd", "prettier_d_slim", "eslint_d", "eslint" }
 
-local function is_table(t)
-  return type(t) == "table"
-end
-
-local function is_string(t)
-  return type(t) == "string"
-end
-
-local function has_value(tab, val)
-  for _, value in ipairs(tab) do
-    if value == val then
-      return true
-    end
-  end
-
-  return false
-end
-
 local find_local_exe = function(exe)
   vim.cmd "let root_dir = FindRootDirectory()"
   local root_dir = vim.api.nvim_get_var "root_dir"
@@ -30,53 +12,39 @@ local find_local_exe = function(exe)
   return local_exe
 end
 
-local function setup_ls(exe, type)
-  if has_value(local_executables, exe) then
-    local smart_executable = null_ls.builtins[type][exe]
+local function setup_ls(exe, null_ls_builtins)
+  if vim.tbl_contains(local_executables, exe) then
+    local smart_executable = null_ls_builtins[exe]
     local local_executable = find_local_exe(exe)
     if vim.fn.executable(local_executable) == 1 then
       smart_executable._opts.command = local_executable
       table.insert(sources, smart_executable)
-    else
-      if vim.fn.executable(exe) == 1 then
-        table.insert(sources, smart_executable)
-      end
+    elseif vim.fn.executable(exe) == 1 then
+      table.insert(sources, smart_executable)
     end
-  else
-    if null_ls.builtins[type][exe] and vim.fn.executable(null_ls.builtins[type][exe]._opts.command) then
-      table.insert(sources, null_ls.builtins[type][exe])
-    end
+  elseif null_ls_builtins[exe] and vim.fn.executable(null_ls_builtins[exe]._opts.command) then
+    table.insert(sources, null_ls_builtins[exe])
   end
   null_ls.register { sources = sources }
 end
 
 -- TODO: for linters and formatters with spaces and '-' replace with '_'
-local function setup(filetype, type)
-  local executables = nil
-  if type == "diagnostics" then
-    executables = lvim.lang[filetype].linters
-  end
-  if type == "formatting" then
-    executables = lvim.lang[filetype].formatter.exe
-  end
-
-  if is_table(executables) then
+local function setup(null_ls_builtins, executables)
+  if type(executables) == "table" then
     for _, exe in pairs(executables) do
       if exe ~= "" then
-        setup_ls(exe, type)
+        setup_ls(exe, null_ls_builtins)
       end
     end
-  end
-  if is_string(executables) and executables ~= "" then
-    setup_ls(executables, type)
+  elseif type(executables) == "string" and executables ~= "" then
+    setup_ls(executables, null_ls_builtins)
   end
 end
 
 -- TODO: return the formatter if one was registered, then turn off the builtin formatter
 function M.setup(filetype)
-  setup(filetype, "formatting")
-  setup(filetype, "diagnostics")
-  lvim.sources = sources
+  setup(null_ls.builtins.formatting, lvim.lang[filetype].formatter.exe)
+  setup(null_ls.builtins.diagnostics, lvim.lang[filetype].linters)
   return sources
 end
 
