89c4d9f065c221568fd9ae89bc8f8b5e089a159f
Johan Melin <johan.melin@paradoxinteractive.com>
2021-07-30 15:20:27 +0200

    Remove extranous else for autopairs completion confirmation

--- lua/core/autopairs.lua
+++ lua/core/autopairs.lua
@@ -15,15 +15,10 @@ M.setup = function()
 
   vim.g.completion_confirm_key = ""
   MUtils.completion_confirm = function()
-    if vim.fn.pumvisible() ~= 0 then
-      if vim.fn.complete_info()["selected"] ~= -1 then
-        return vim.fn["compe#confirm"](npairs.esc "<cr>")
-      else
-        return npairs.esc "<cr>"
-      end
-    else
-      return npairs.autopairs_cr()
+    if vim.fn.complete_info()["selected"] ~= -1 then
+      return vim.fn["compe#confirm"](npairs.esc "<cr>")
     end
+    return npairs.esc "<cr>"
   end
 
   if package.loaded["compe"] then
