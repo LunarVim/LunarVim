9da9a3496a641c20bd0e99c96139e56f4db763df
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2021-07-30 21:35:48 +0700

    Remove unecessary function from utils

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -120,32 +120,6 @@ function utils.has_value(tab, val)
   return false
 end
 
-function utils.add_keymap(mode, opts, keymaps)
-  for _, keymap in ipairs(keymaps) do
-    vim.api.nvim_set_keymap(mode, keymap[1], keymap[2], opts)
-  end
-end
-
-function utils.add_keymap_normal_mode(opts, keymaps)
-  utils.add_keymap("n", opts, keymaps)
-end
-
-function utils.add_keymap_visual_mode(opts, keymaps)
-  utils.add_keymap("v", opts, keymaps)
-end
-
-function utils.add_keymap_visual_block_mode(opts, keymaps)
-  utils.add_keymap("x", opts, keymaps)
-end
-
-function utils.add_keymap_insert_mode(opts, keymaps)
-  utils.add_keymap("i", opts, keymaps)
-end
-
-function utils.add_keymap_term_mode(opts, keymaps)
-  utils.add_keymap("t", opts, keymaps)
-end
-
 function utils.unrequire(m)
   package.loaded[m] = nil
   _G[m] = nil
