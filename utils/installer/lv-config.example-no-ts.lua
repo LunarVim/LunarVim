a932d48ba66758de40059b8fa2e412ccbdee8b00
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2021-07-30 21:46:02 +0700

    Update config sample

--- utils/installer/lv-config.example-no-ts.lua
+++ utils/installer/lv-config.example-no-ts.lua
@@ -8,22 +8,15 @@ lvim.colorscheme = "spacegray"
 -- keymappings
 lvim.leader = "space"
 -- overwrite the key-mappings provided by LunarVim for any mode, or leave it empty to keep them
--- lvim.keys.normal_mode = {
---   Page down/up
---   {'[d', '<PageUp>'},
---   {']d', '<PageDown>'},
---
---   Navigate buffers
---   {'<Tab>', ':bnext<CR>'},
---   {'<S-Tab>', ':bprevious<CR>'},
--- }
--- if you just want to augment the existing ones then use the utility function
--- require("utils").add_keymap_insert_mode({ silent = true }, {
--- { "<C-s>", ":w<cr>" },
--- { "<C-c>", "<ESC>" },
--- })
--- you can also use the native vim way directly
--- vim.api.nvim_set_keymap("i", "<C-Space>", "compe#complete()", { noremap = true, silent = true, expr = true })
+-- -- Page down/up
+-- lvim.keys.normal_mode["[d"] = "<PageUp>"
+-- lvim.keys.normal_mode["]d"] = "<PageDown>"
+-- -- Navigate buffers
+-- lvim.keys.normal_mode["<Tab>"] = ":bnext<CR>"
+-- lvim.keys.normal_mode["<S-Tab>"] = ":bprevious<CR>"
+-- -- if you just want to augment the existing ones then use the utility function
+-- lvim.keys.insert_mode["<C-s>"] = { ":w<cr>", { silent = true } }
+-- lvim.keys.insert_mode["<C-c>"] = { "<ESC>", { silent = true } }
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
