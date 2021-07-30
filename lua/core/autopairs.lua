85104a46002d82ad05e42292fcdd94534b4d8df9
Johan Melin <johan.melin@paradoxinteractive.com>
2021-07-30 19:57:21 +0200

    Fix erroneous remove of functionality in autopairs completion

--- lua/core/autopairs.lua
+++ lua/core/autopairs.lua
@@ -15,10 +15,13 @@ M.setup = function()
 
   vim.g.completion_confirm_key = ""
   MUtils.completion_confirm = function()
-    if vim.fn.complete_info()["selected"] ~= -1 then
-      return vim.fn["compe#confirm"](npairs.esc "<cr>")
+    if vim.fn.pumvisible() ~= 0 then
+      if vim.fn.complete_info()["selected"] ~= -1 then
+        return vim.fn["compe#confirm"](npairs.esc "<cr>")
+      end
+      return npairs.esc "<cr>"
     end
-    return npairs.esc "<cr>"
+    return npairs.autopairs_cr()
   end
 
   if package.loaded["compe"] then
