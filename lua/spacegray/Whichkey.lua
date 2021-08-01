341a17daa60562f553e41c4c6792a6c917d8d9ef
Luc Sinet <Tastyep@users.noreply.github.com>
2021-08-01 12:02:19 +0200

    Fix formatting issues (#1200)

--- lua/spacegray/Whichkey.lua
+++ lua/spacegray/Whichkey.lua
@@ -1,9 +1,9 @@
 local Whichkey = {
-		WhichKey = {fg = C.purple, },
-		WhichKeySeperator = {fg = C.green, },
-		WhichKeyGroup = {fg = C.blue, },
-		WhichKeyDesc = {fg = C.light_blue, },
-		WhichKeyFloat = {bg = C.dark, },
+  WhichKey = { fg = C.purple },
+  WhichKeySeperator = { fg = C.green },
+  WhichKeyGroup = { fg = C.blue },
+  WhichKeyDesc = { fg = C.light_blue },
+  WhichKeyFloat = { bg = C.dark },
 }
 
 return Whichkey
--- lua/spacegray/config.lua
+++ lua/spacegray/config.lua
@@ -3,21 +3,21 @@ local config
 vim = vim or { g = {}, o = {} }
 
 local function opt(key, default)
-    if vim.g[key] == nil then
-        return default
-    end
-    if vim.g[key] == 0 then
-        return false
-    end
-    return vim.g[key]
+  if vim.g[key] == nil then
+    return default
+  end
+  if vim.g[key] == 0 then
+    return false
+  end
+  return vim.g[key]
 end
 
 config = {
-    transparent_background = opt("transparent_background", false),
-    italic_comments = opt("italic_keywords", true) and "italic" or "NONE",
-    italic_keywords = opt("italic_keywords", true) and "italic" or "NONE",
-    italic_functions = opt("italic_function", false) and "italic" or "NONE",
-    italic_variables = opt("italic_variables", true) and "italic" or "NONE",
+  transparent_background = opt("transparent_background", false),
+  italic_comments = opt("italic_keywords", true) and "italic" or "NONE",
+  italic_keywords = opt("italic_keywords", true) and "italic" or "NONE",
+  italic_functions = opt("italic_function", false) and "italic" or "NONE",
+  italic_variables = opt("italic_variables", true) and "italic" or "NONE",
 }
 
 return config
