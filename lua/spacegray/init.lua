40d746b576580d2e46df11acd9698bb75113258a
Diego Barranco <diegob@xps.lan>
2021-08-01 12:42:12 -0400

    removed merge message

--- lua/spacegray/init.lua
+++ lua/spacegray/init.lua
@@ -13,10 +13,7 @@ C = require "spacegray.palette"
 local async
 async = vim.loop.new_async(vim.schedule_wrap(function()
   local skeletons = {}
-<<<<<<< HEAD
-=======
 
->>>>>>> add_terminal_filetype_cmd
   for _, skeleton in ipairs(skeletons) do
     util.initialise(skeleton)
   end
