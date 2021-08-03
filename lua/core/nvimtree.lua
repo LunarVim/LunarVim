c0a653a0cf5ef1842b086149b9e79a79cc62583d
tafryn <tafryn@gmail.com>
2021-08-02 22:43:57 -0700

    Allow user's to define their own nvim-tree bindings (#1181)

--- lua/core/nvimtree.lua
+++ lua/core/nvimtree.lua
@@ -59,11 +59,13 @@ M.setup = function()
 
   local tree_cb = nvim_tree_config.nvim_tree_callback
 
-  g.nvim_tree_bindings = {
-    { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-    { key = "h", cb = tree_cb "close_node" },
-    { key = "v", cb = tree_cb "vsplit" },
-  }
+  if not g.nvim_tree_bindings then
+    g.nvim_tree_bindings = {
+      { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+      { key = "h", cb = tree_cb "close_node" },
+      { key = "v", cb = tree_cb "vsplit" },
+    }
+  end
 end
 --
 M.focus_or_close = function()
