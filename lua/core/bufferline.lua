3ccd5dbc8c56495d8424ff41cdfdb4d0d8ff39af
rebuilt <memoryman51@hotmail.com>
2021-08-06 04:04:21 +0200

    bufferline broke because the added active toggle will always be false. Set it in default_config to fix behavior

--- lua/core/bufferline.lua
+++ lua/core/bufferline.lua
@@ -1,12 +1,20 @@
-lvim.builtin.bufferline = {
-  active = true,
-  keymap = {
-    normal_mode = {
-      ["<S-l>"] = ":BufferNext<CR>",
-      ["<S-h>"] = ":BufferPrevious<CR>",
-    },
-  },
-}
+local M = {}
 
-local keymap = require "keymappings"
-keymap.append_to_defaults(lvim.builtin.bufferline.keymap)
+M.config = function()
+  lvim.builtin.bufferline = {
+    active = true,
+    keymap = {
+      normal_mode = {
+        ["<S-l>"] = ":BufferNext<CR>",
+        ["<S-h>"] = ":BufferPrevious<CR>",
+      },
+    },
+  }
+end
+
+M.setup = function()
+  local keymap = require "keymappings"
+  keymap.append_to_defaults(lvim.builtin.bufferline.keymap)
+end
+
+return M
