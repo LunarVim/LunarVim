730542a47da8e2dbb7e0a800c425f210a0325b1b
ashincoder <83629316+ashincoder@users.noreply.github.com>
2021-08-07 14:53:25 +0530

    Added space after plug icon (#1252)

--- lua/core/dashboard.lua
+++ lua/core/dashboard.lua
@@ -72,7 +72,7 @@ M.setup = function()
 
   vim.api.nvim_exec(
     [[
-    let g:dashboard_custom_footer = ['LunarVim loaded '..packages..' plugins ']
+    let g:dashboard_custom_footer = ['LunarVim loaded '..packages..' plugins  ']
 ]],
     false
   )
