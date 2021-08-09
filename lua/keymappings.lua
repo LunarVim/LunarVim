0138cd512aaac1d43b7269473e78f1f5edba8d9c
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-08-09 18:28:15 +0200

    make sure plenary is installed before using it

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -149,7 +149,9 @@ function M.config()
     lvim.keys.normal_mode["<A-Down>"] = lvim.keys.normal_mode["<C-Down>"]
     lvim.keys.normal_mode["<A-Left>"] = lvim.keys.normal_mode["<C-Left>"]
     lvim.keys.normal_mode["<A-Right>"] = lvim.keys.normal_mode["<C-Right>"]
-    Log:get_default().info "Activated mac keymappings"
+    if Log:get_default() then
+      Log:get_default().info "Activated mac keymappings"
+    end
   end
 end
 
