a9f211729183f3c330fa708782455a9395577fad
MarcSchaetz <marc@schaetz.info>
2021-08-10 22:34:09 +0200

    Refactor install.ps1
    - change function names casing to camelCase
    - use XDG_ variables for data, config and cache path
    - quote non-quoted strings
    - use windows path separator '\'
    - exit if program is not installed instead of asking to install via
    winget

--- utils/installer/install.ps1
+++ utils/installer/install.ps1
@@ -1,61 +1,42 @@
-#Set Variable to master is not set differently
+# set script variables
+$XDG_CONFIG_HOME="$HOME\.config"
+$XDG_DATA_HOME="$HOME\.local\share"
+$XDG_CACHE_HOME=$env:TEMP
+
+# Set Variable to master if not set differently
 if ($null -eq $LVBRANCH) {
     $LVBRANCH = "master"
 }
 
-#set -o nounset # error when referencing undefined variable
 $ErrorActionPreference = "Stop" # exit when command fails
 
-function mkdirondemand($path) {
+function mkdirOnDemand($path) {
 	if ((Test-Path $path) -eq $false) {
 		mkdir $path
 	}
 }
 
-function commandinstalled($cmd){
-	try { Get-Command $cmd > $null }
-	catch { return $false }
-	return $true
+function verifyInstalled($cmd){
+	try { Get-Command -Name $cmd -ErrorAction Stop | Out-Null }
+	catch { "Error! command [$cmd] is not found, please install and try again." }
 }
 
-function moveoldlvim {
+function moveOldLvim {
 	Write-Output "Not installing LunarVim"
-	Write-Output "Please move your $HOME\.local\share\lunarvim folder before installing"
+	Write-Output "Please move your $XDG_DATA_HOME\lunarvim folder before installing"
 	exit
 }
 
-function asktoinstallpip {
-	Write-Output "Please install pip3 before continuing with install"
-	exit
-}
-
-function installnode {
-	if ((commandinstalled("winget.exe")) -eq $false) {
-		Write-Output "winget is not installed, but is the only package manager currently supported."
-		Write-Output "node is not installed. Please install it manually"
-		exit
-	}
-	winget.exe install "OpenJS.NodeJSLTS"
-}
-
-function asktoinstallnode {
-	Write-Output "node not found"
-	$answer = Read-Host "Would you like to install node now (y/n)" -Confirm
-	if($answer.ToLower() -eq 'y') {
-		installnode
-	}
-}
-
-function installpynvim {
+function installPynvim {
 	Write-Output "Installing pynvim..."
 	pip3.exe install pynvim --user
 }
 
-function installpacker {
-	git clone https://github.com/wbthomason/packer.nvim ${HOME}/.local/share/lunarvim/site/pack/packer/start/packer.nvim
+function installPacker {
+	git clone "https://github.com/wbthomason/packer.nvim" "$XDG_DATA_HOME\lunarvim\site\pack\packer\start\packer.nvim"
 }
 
-function modifyhomevar($config) {
+function modifyHomeVar($config) {
 	
 	Copy-Item $config $config".save"
 	(Get-Content -Path $config) |
@@ -63,93 +44,93 @@ function modifyhomevar($config) {
 			Set-Content -Path $config
 }
 
-function cloneconfig($arg) {
+function cloneConfig($arg) {
 	Write-Output "Cloning LunarVim configuration"
-	mkdirondemand("$HOME/.local/share/lunarvim")
+	mkdirOnDemand("$XDG_DATA_HOME\lunarvim")
 	
 	if ($arg.Contains("--testing")) {
-		Copy-Item -Recurse "${pwd}" "$HOME/.local/share/lunarvim/lvim"
+		Copy-Item -Recurse "$pwd" "$XDG_DATA_HOME\lunarvim\lvim"
 	} else {
 		Write-Output "lvbranch=$LVBRANCH"
-		git clone --branch "$LVBRANCH" https://github.com/LunarVim/LunarVim.git "$HOME/.local/share/lunarvim/lvim"
+		git clone --branch "$LVBRANCH" "https://github.com/LunarVim/LunarVim.git" "$XDG_DATA_HOME\lunarvim\lvim"
 	}
 
-	mkdirondemand("$HOME/.config/lvim")
+	mkdirOnDemand("$XDG_CONFIG_HOME\lvim")
 	
-	if (($env:Path.Contains("$HOME/.local/share/lunarvim/lvim/utils/bin/")) -eq $false) {
-		$env:Path += ";$HOME/.local/share/lunarvim/lvim/utils/bin/"
+	if (($env:Path.Contains("$XDG_DATA_HOME\lunarvim\lvim\utils\bin\")) -eq $false) {
+		$env:Path += ";$XDG_DATA_HOME\lunarvim\lvim\utils\bin\"
 		[Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::User)
 	}
 	
 	# Hack because in rolling branch lv-config.lua is renamed to config.lua
 	if ($LVBRANCH -eq "rolling") {
-		Copy-Item "$HOME/.local/share/lunarvim/lvim/utils/installer/config.example-no-ts.lua" "$HOME/.config/lvim/config.lua"
+		Copy-Item "$XDG_DATA_HOME\lunarvim\lvim\utils\installer\config.example-no-ts.lua" "$XDG_CONFIG_HOME\lvim\config.lua"
 	} else {
-		Copy-Item "$HOME/.local/share/lunarvim/lvim/utils/installer/lv-config.example-no-ts.lua" "$HOME/.config/lvim/lv-config.lua"
+		Copy-Item "$XDG_DATA_HOME\lunarvim\lvim\utils\installer\lv-config.example-no-ts.lua" "$XDG_CONFIG_HOME\lvim\lv-config.lua"
 	}
 
-	modifyhomevar("$HOME/.local/share/lunarvim/lvim/lua/default-config.lua")
-	modifyhomevar("$HOME/.local/share/lunarvim/lvim/init.lua")
+	modifyHomeVar("$XDG_DATA_HOME\lunarvim\lvim\lua\default-config.lua")
+	modifyHomeVar("$XDG_DATA_HOME\lunarvim\lvim\init.lua")
 
-	nvim -u $HOME/.local/share/lunarvim/lvim/init.lua --cmd "set runtimepath+=$HOME/.local/share/lunarvim/lvim" --headless +'autocmd User PackerComplete sleep 100m | qall' +PackerInstall
-	nvim -u $HOME/.local/share/lunarvim/lvim/init.lua --cmd "set runtimepath+=$HOME/.local/share/lunarvim/lvim" --headless +'autocmd User PackerComplete sleep 100m | qall' +PackerSync
+	nvim -u $XDG_DATA_HOME\lunarvim\lvim\init.lua --cmd "set runtimepath+=$XDG_DATA_HOME\lunarvim\lvim" --headless +'autocmd User PackerComplete sleep 100m | qall' +PackerInstall
+	nvim -u $XDG_DATA_HOME\lunarvim\lvim\init.lua --cmd "set runtimepath+=$XDG_DATA_HOME\lunarvim\lvim" --headless +'autocmd User PackerComplete sleep 100m | qall' +PackerSync
 
 	Write-Output ""
 	Write-Output "Compile Complete"
 	Write-Output ""
 
-	if (Test-Path "$HOME/.local/share/lunarvim/lvim/init.lua" -PathType Any) {
+	if (Test-Path "$XDG_DATA_HOME\lunarvim\lvim\init.lua" -PathType Any) {
 		Write-Output "lv-config already present"
 	} else {
 		if($LVBRANCH -eq "rolling") {
-			Copy-Item "$HOME/.local/share/lunarvim/lvim/utils/installer/config.example.lua" "$HOME/.config/lvim/config.lua"
+			Copy-Item "$XDG_DATA_HOME\lunarvim\lvim\utils\installer\config.example.lua" "$XDG_CONFIG_HOME\lvim\config.lua"
 		} else {
-			Copy-Item "$HOME/.local/share/lunarvim/lvim/utils/installer/lv-config.example.lua" "$HOME/.config/lvim/lv-config.lua"
+			Copy-Item "$XDG_DATA_HOME\lunarvim\lvim\utils\installer\lv-config.example.lua" "$XDG_CONFIG_HOME\lvim\lv-config.lua"
 		}
 	}
 }
 
-function rewritelvimforwindows {
-	Set-Content "$HOME/.local/share/lunarvim/lvim/utils/bin/lvim.ps1" 'nvim -u "$HOME/.local/share/lunarvim/lvim/init.lua" --cmd "set runtimepath+=$HOME/.local/share/lunarvim/lvim" "$args"'
+function rewriteLvimForWindows {
+	Set-Content "$XDG_DATA_HOME\lunarvim\lvim\utils\bin\lvim.ps1" "nvim -u `"$XDG_DATA_HOME\lunarvim\lvim\init.lua`" --cmd `"set runtimepath+=$XDG_DATA_HOME\lunarvim\lvim`" `"$args`""
 }
 
 # Welcome
 Write-Output 'Installing LunarVim'
 if ($args.Contains("--override")) {
     Write-Output '!!Warning!! -> Removing all lunarvim related config because of the --overwrite flag'
-	Remove-Item -Recurse -Force "$HOME/.local/share/lunarvim" -ErrorAction Ignore
-	Remove-Item -Recurse -Force "$HOME/.cache/nvim" -ErrorAction Ignore
-	Remove-Item -Recurse -Force "$HOME/.config/lvim" -ErrorAction Ignore
+	Remove-Item -Recurse -Force "$XDG_DATA_HOME\lunarvim" -ErrorAction Ignore
+	Remove-Item -Recurse -Force "$XDG_CACHE_HOME\nvim" -ErrorAction Ignore
+	Remove-Item -Recurse -Force "$XDG_CONFIG_HOME\lvim" -ErrorAction Ignore
 }
 
 # move old lvim directory if it exists
-if (Test-Path -Path "$HOME/.local/share/lunarvim") { moveoldlvim }
+if (Test-Path -Path "$XDG_DATA_HOME\lunarvim") { moveOldLvim }
 
 # install pip
-if (commandinstalled("pip3.exe")){ Write-Output "pip installed, moving on..." } 
-else { asktoinstallpip }
+verifyInstalled("pip3.exe")
+Write-Output "pip installed, moving on..."
 
 # install node
-if (commandinstalled("node.exe")) { Write-Output "node installed, moving on..." }
-else { asktoinstallnode }
+verifyInstalled("node.exe")
+Write-Output "node installed, moving on..."
 
 # install pynvim
 if ($null -ne (pip3.exe list | Select-String "pynvim")) { Write-Output "pynvim installed, moving on..." }
-else { installpynvim }
+else { installPynvim }
 
-if ((Test-Path "$HOME/.local/share/lunarvim/site/pack/packer/start/packer.nvim" -PathType Any)) {
+if ((Test-Path "$XDG_DATA_HOME\lunarvim\site\pack\packer\start\packer.nvim" -PathType Any)) {
 	Write-Output "packer already installed"
 } else {
-	installpacker
+	installPacker
 }
 
-if ((Test-Path "$HOME/.local/share/lunarvim/lvim/init.lua" -PathType Any)) {
+if ((Test-Path "$XDG_DATA_HOME\lunarvim\lvim\init.lua" -PathType Any)) {
 	Write-Output "LunarVim already installed"
 } else {
 	# clone config down
-	cloneconfig($args)
+	cloneConfig($args)
 }
 
-rewritelvimforwindows
+rewriteLvimForWindows
 
 Write-Output "I recommend you also install and activate a font from here: https://github.com/ryanoasis/nerd-fonts"
