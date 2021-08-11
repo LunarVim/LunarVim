8f52e591e4b84016add9ff0976008db11b4ecc31
Luc Sinet <luc.sinet@gmail.com>
2021-08-11 10:07:43 +0200

    Cache providers because of galaxyline refreshing strategy

--- lua/lsp/null-ls/init.lua
+++ lua/lsp/null-ls/init.lua
@@ -1,17 +1,38 @@
 local M = {}
 
-local null_ls = require "null-ls"
 
 -- TODO: for linters and formatters with spaces and '-' replace with '_'
 function M.setup(filetype)
+  local null_ls = require "null-ls"
   local null_formatters = require "lsp.null-ls.formatters"
   local null_linters = require "lsp.null-ls.linters"
+
   local formatters = null_formatters.list_configured(lvim.lang[filetype].formatters)
   local linters = null_linters.list_configured(lvim.lang[filetype].linters)
 
-  for _, providers in ipairs { formatters, linters } do
+  local function register_providers(providers, provider_type)
+    for _, flag in ipairs { "supported", "unsupported" } do
+      require("core.log"):get_default().info("flag", flag, "providers", vim.inspect(providers[flag]))
+      for _, provider in ipairs(providers[flag]) do
+        providers_by_ft[filetype][provider_type][flag][provider.name] = provider
+      end
+    end
     null_ls.register { sources = providers.supported }
   end
+
+  providers_by_ft[filetype] = {
+    formatters = {
+      supported = {},
+      unsupported = {},
+    },
+    linters = {
+      supported = {},
+      unsupported = {},
+    },
+  }
+
+  register_providers(formatters, "formatters")
+  register_providers(linters, "linters")
 end
 
 return M
