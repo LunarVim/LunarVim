854c7a5c060fa776946699c1a53daf5b9771269e
Luc Sinet <luc.sinet@gmail.com>
2021-08-16 11:14:40 +0200

    Remove config check

--- init.lua
+++ init.lua
@@ -12,23 +12,8 @@ vim.cmd [[
 ]]
 -- vim.opt.rtp:append() instead of vim.cmd ?
 
-local function file_exists(name)
-  local f = io.open(name, "r")
-  if f ~= nil then
-    io.close(f)
-    return true
-  else
-    return false
-  end
-end
-
 local lvim_path = os.getenv "HOME" .. "/.config/lvim/"
 USER_CONFIG_PATH = lvim_path .. "config.lua"
-local config_exist = file_exists(USER_CONFIG_PATH)
-if not config_exist then
-  USER_CONFIG_PATH = lvim_path .. "lv-config.lua"
-  print "Rename ~/.config/lvim/lv-config.lua to config.lua"
-end
 
 require "default-config"
 local autocmds = require "core.autocmds"
