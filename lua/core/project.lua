30ce50efe9b0ea519a2396d0fa60895d3d2387cf
ahmedkhalf <ahmedkhalf567@gmail.com>
2021-08-16 10:51:39 +0400

    Fix lint

--- lua/core/project.lua
+++ lua/core/project.lua
@@ -35,7 +35,8 @@ function M.setup()
 
   -- Path where project.nvim will store the project history for use in
   -- telescope
-  settings["datapath"] = CACHE_PATH,
+  settings["datapath"] = CACHE_PATH
+
   require("project_nvim").setup(settings)
 
   lvim.builtin.dashboard.custom_section["b"] = {
