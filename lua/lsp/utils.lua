08c0dca3071ec2df16553dd724836307999e6aef
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-08-17 17:26:59 +0200

    [Refactor]: Remove vim-rooter dependency (#1319)

--- lua/lsp/utils.lua
+++ lua/lsp/utils.lua
@@ -4,12 +4,13 @@ function M.is_client_active(name)
   local clients = vim.lsp.get_active_clients()
   for _, client in pairs(clients) do
     if client.name == name then
-      return true
+      return true, client
     end
   end
   return false
 end
 
+-- FIXME: this should return a list instead
 function M.get_active_client_by_ft(filetype)
   if not lvim.lang[filetype] or not lvim.lang[filetype].lsp then
     return nil
