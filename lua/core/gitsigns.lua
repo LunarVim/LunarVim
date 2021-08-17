d8f4eb8ca1fdb06e4e36ffbd56561cbb52c575e6
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-08-17 15:06:32 +0200

    Fix gitsigns validation warning

--- lua/core/gitsigns.lua
+++ lua/core/gitsigns.lua
@@ -2,54 +2,56 @@ local M = {}
 M.config = function()
   lvim.builtin.gitsigns = {
     active = true,
-    signs = {
-      add = {
-        hl = "GitSignsAdd",
-        text = "▎",
-        numhl = "GitSignsAddNr",
-        linehl = "GitSignsAddLn",
+    opts = {
+      signs = {
+        add = {
+          hl = "GitSignsAdd",
+          text = "▎",
+          numhl = "GitSignsAddNr",
+          linehl = "GitSignsAddLn",
+        },
+        change = {
+          hl = "GitSignsChange",
+          text = "▎",
+          numhl = "GitSignsChangeNr",
+          linehl = "GitSignsChangeLn",
+        },
+        delete = {
+          hl = "GitSignsDelete",
+          text = "契",
+          numhl = "GitSignsDeleteNr",
+          linehl = "GitSignsDeleteLn",
+        },
+        topdelete = {
+          hl = "GitSignsDelete",
+          text = "契",
+          numhl = "GitSignsDeleteNr",
+          linehl = "GitSignsDeleteLn",
+        },
+        changedelete = {
+          hl = "GitSignsChange",
+          text = "▎",
+          numhl = "GitSignsChangeNr",
+          linehl = "GitSignsChangeLn",
+        },
       },
-      change = {
-        hl = "GitSignsChange",
-        text = "▎",
-        numhl = "GitSignsChangeNr",
-        linehl = "GitSignsChangeLn",
+      numhl = false,
+      linehl = false,
+      keymaps = {
+        -- Default keymap options
+        noremap = true,
+        buffer = true,
       },
-      delete = {
-        hl = "GitSignsDelete",
-        text = "契",
-        numhl = "GitSignsDeleteNr",
-        linehl = "GitSignsDeleteLn",
-      },
-      topdelete = {
-        hl = "GitSignsDelete",
-        text = "契",
-        numhl = "GitSignsDeleteNr",
-        linehl = "GitSignsDeleteLn",
-      },
-      changedelete = {
-        hl = "GitSignsChange",
-        text = "▎",
-        numhl = "GitSignsChangeNr",
-        linehl = "GitSignsChangeLn",
-      },
+      watch_index = { interval = 1000 },
+      sign_priority = 6,
+      update_debounce = 200,
+      status_formatter = nil, -- Use default
     },
-    numhl = false,
-    linehl = false,
-    keymaps = {
-      -- Default keymap options
-      noremap = true,
-      buffer = true,
-    },
-    watch_index = { interval = 1000 },
-    sign_priority = 6,
-    update_debounce = 200,
-    status_formatter = nil, -- Use default
   }
 end
 
 M.setup = function()
-  require("gitsigns").setup(lvim.builtin.gitsigns)
+  require("gitsigns").setup(lvim.builtin.gitsigns.opts)
 end
 
 return M
