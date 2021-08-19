6f189b5a4e0a3e6ad999b7b190fddecfd75c59ba
ahmedkhalf <ahmedkhalf567@gmail.com>
2021-08-20 00:56:21 +0400

    Fix formatting again

--- lua/core/nvimtree.lua
+++ lua/core/nvimtree.lua
@@ -96,7 +96,7 @@ M.on_open = function()
 end
 --
 M.on_close = function()
-  local buf = tonumber(vim.fn.expand("<abuf>"))
+  local buf = tonumber(vim.fn.expand "<abuf>")
   if vim.api.nvim_buf_get_option(buf, "filetype") == "NvimTree" then
     if package.loaded["bufferline.state"] then
       require("bufferline.state").set_offset(0)
