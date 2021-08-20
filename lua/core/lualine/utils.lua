9458636509be96f0689de293bf2553b1cbfc9c16
Chae SM <smchae9@gmail.com>
2021-08-19 19:35:13 -0700

    Remove unnecessary logic for theme validation

--- lua/core/lualine/utils.lua
+++ lua/core/lualine/utils.lua
@@ -4,8 +4,8 @@ function M.validate_theme()
   local theme = lvim.builtin.lualine.options.theme
 
   local lualine_loader = require "lualine.utils.loader"
-  local ok, validated_theme = pcall(lualine_loader.load_theme, theme)
-  if not ok and type(validated_theme) ~= "table" then
+  local ok = pcall(lualine_loader.load_theme, theme)
+  if not ok then
     lvim.builtin.lualine.options.theme = "auto"
   end
 end
