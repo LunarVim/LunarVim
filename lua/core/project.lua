4238e18a9b06f53ac834563c5bf9d62a16301527
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-08-20 17:20:18 +0200

    [fix]: don't overwrite user's dashboard config

--- lua/core/project.lua
+++ lua/core/project.lua
@@ -2,50 +2,43 @@ local M = {}
 --
 function M.config()
   lvim.builtin.project = {
-    --- This is on by default since it's currently the expected behavior.
     ---@usage set to false to disable project.nvim.
+    --- This is on by default since it's currently the expected behavior.
     active = true,
 
-    -- Manual mode doesn't automatically change your root directory, so you have
-    -- the option to manually do so using `:ProjectRoot` command.
+    ---@usage set to true to disable setting the current-woriking directory
+    --- Manual mode doesn't automatically change your root directory, so you have
+    --- the option to manually do so using `:ProjectRoot` command.
     manual_mode = false,
 
-    -- Methods of detecting the root directory. **"lsp"** uses the native neovim
-    -- lsp, while **"pattern"** uses vim-rooter like glob pattern matching. Here
-    -- order matters: if one is not detected, the other is used as fallback. You
-    -- can also delete or rearangne the detection methods.
+    ---@usage Methods of detecting the root directory
+    --- Allowed values: **"lsp"** uses the native neovim lsp
+    --- **"pattern"** uses vim-rooter like glob pattern matching. Here
+    --- order matters: if one is not detected, the other is used as fallback. You
+    --- can also delete or rearangne the detection methods.
     detection_methods = { "lsp", "pattern" },
 
-    -- All the patterns used to detect root dir, when **"pattern"** is in
-    -- detection_methods
+    ---@usage patterns used to detect root dir, when **"pattern"** is in detection_methods
     patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
 
-    -- Show hidden files in telescope
+    ---@ Show hidden files in telescope when searching for files in a project
     show_hidden = false,
 
-    -- When set to false, you will get a message when project.nvim changes your
-    -- directory.
+    ---@usage When set to false, you will get a message when project.nvim changes your directory.
+    -- When set to false, you will get a message when project.nvim changes your directory.
     silent_chdir = true,
+
+    ---@usage list of lsp client names to ignore when using **lsp** detection. eg: { "efm", ... }
+    ignore_lsp = {},
+
+    ---@type string
+    ---@usage path to store the project history for use in telescope
+    datapath = CACHE_PATH,
   }
 end
 --
 function M.setup()
-  local settings = lvim.builtin.project
-
-  -- Table of lsp clients to ignore by name
-  -- eg: { "efm", ... }
-  settings["ignore_lsp"] = {}
-
-  -- Path where project.nvim will store the project history for use in
-  -- telescope
-  settings["datapath"] = CACHE_PATH
-
-  require("project_nvim").setup(settings)
-
-  lvim.builtin.dashboard.custom_section["b"] = {
-    description = { "  Recent Projects    " },
-    command = "Telescope projects",
-  }
+  require("project_nvim").setup(lvim.builtin.project)
 end
 --
 return M
