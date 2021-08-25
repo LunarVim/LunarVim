28a934e932076798fa48783156d73d5a9d7f6028
Chae SM <smchae9@gmail.com>
2021-08-25 16:06:23 -0700

    Reuse gitsigns's info on branch and diff component

--- lua/core/lualine/components.lua
+++ lua/core/lualine/components.lua
@@ -1,6 +1,17 @@
 local conditions = require "core.lualine.conditions"
 local colors = require "core.lualine.colors"
 
+local function diff_source()
+  local gitsigns = vim.b.gitsigns_status_dict
+  if gitsigns then
+    return {
+      added = gitsigns.added,
+      modified = gitsigns.changed,
+      removed = gitsigns.removed,
+    }
+  end
+end
+
 return {
   mode = {
     function()
@@ -14,12 +25,10 @@ return {
     color = {},
   },
   branch = {
-    "branch",
+    "b:gitsigns_head",
     icon = " ",
-    condition = function()
-      return conditions.hide_in_width() and conditions.check_git_workspace()
-    end,
     color = { gui = "bold" },
+    condition = conditions.hide_in_width,
   },
   filename = {
     "filename",
@@ -30,6 +39,7 @@ return {
   },
   diff = {
     "diff",
+    source = diff_source,
     symbols = { added = "  ", modified = "柳", removed = " " },
     color_added = { fg = colors.green },
     color_modified = { fg = colors.yellow },
