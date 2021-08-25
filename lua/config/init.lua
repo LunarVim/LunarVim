c9bcdab8fe2a8926217716bc475cac6de0353ce2
Luc Sinet <luc.sinet@gmail.com>
2021-08-25 10:25:25 +0200

    Replace vim command with standard function loadfile

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -31,13 +31,14 @@ function M:load(config_path)
   local autocmds = require "core.autocmds"
 
   config_path = config_path or self.path
-  local ok, err = pcall(vim.cmd, "luafile " .. config_path)
-  if not ok then
+  local config, err = loadfile(config_path)
+  if err then
     print("Invalid configuration", config_path)
     print(err)
     return
   end
 
+  lvim = vim.tbl_deep_extend("force", lvim, config())
   self.path = config_path
 
   autocmds.define_augroups(lvim.autocommands)
