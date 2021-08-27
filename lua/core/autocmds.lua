df7c3cb8f0512042bd24c10e744718991ae6a9a2
Christian Chiarulli <chris.machine@pm.me>
2021-08-27 00:11:01 -0400

    q to quit quickfix when focused

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -9,6 +9,11 @@ lvim.autocommands = {
       "lua require('utils.ft').do_filetype(vim.fn.expand(\"<amatch>\"))",
     },
     {
+      "FileType",
+      "qf",
+      "nnoremap <silent> <buffer> q :q<CR>",
+    },
+    {
       "TextYankPost",
       "*",
       "lua require('vim.highlight').on_yank({higroup = 'Search', timeout = 200})",
