4e7b088e4f1e4a42ff43fe175df69f13c099d921
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-08-28 16:10:53 +0200

    add support for using package managers

--- utils/installer/install.ps1
+++ utils/installer/install.ps1
@@ -16,22 +16,22 @@ $LUNARVIM_RUNTIME_DIR = ($LUNARVIM_RUNTIME_DIR, "$XDG_DATA_HOME\lunarvim", 1 -ne
 $LUNARVIM_CONFIG_DIR = ($LUNARVIM_CONFIG_DIR, "$XDG_CONFIG_HOME\lvim", 1 -ne $null)[0]
 
 $__lvim_dirs = (
-	"$LUNARVIM_CONFIG_DIR",
-	"$LUNARVIM_RUNTIME_DIR",
-	"$NEOVIM_CACHE_DIR" # for now this is shared with neovim
+    "$LUNARVIM_CONFIG_DIR",
+    "$LUNARVIM_RUNTIME_DIR",
+    "$NEOVIM_CACHE_DIR" # for now this is shared with neovim
 )
 
 $__npm_deps = (
-	"neovim",
-	"tree-sitter-cli"
+    "neovim",
+    "tree-sitter-cli"
 )
 
 $__pip_deps = (
-	"pynvim"
+    "pynvim"
 )
 
 function main($cliargs) {
-	Write-Output "  
+    Write-Output "  
 
 		88\                                                   88\               
 		88 |                                                  \__|              
@@ -44,162 +44,205 @@ function main($cliargs) {
   
   "
   
-	__add_separator "80"
+    __add_separator "80"
   
-	# skip this in a Github workflow
-	if ( $null -eq "$GITHUB_ACTIONS" ) {
-		check_system_deps
-  
-		__add_separator "80"
+    # skip this in a Github workflow
+    if ( $null -eq "$GITHUB_ACTIONS" ) {
+        install_packer
+        setup_shim
+        exit
+    }
 
-		<# Write-Output "Would you like to check lunarvim's NodeJS dependencies?"
-		$answer = Read-Host "[y]es or [n]o (default: no) "
-		if ("$answer" -eq "y" -or "$answer" -eq "Y") {
-			install_nodejs_deps
-		} 
+    __add_separator "80"
+
+    check_system_deps
+
+    Write-Output "Would you like to check lunarvim's NodeJS dependencies?"
+    $answer = Read-Host "[y]es or [n]o (default: no) "
+    if ("$answer" -eq "y" -or "$answer" -eq "Y") {
+        install_nodejs_deps
+    } 
+
+    Write-Output "Would you like to check lunarvim's Python dependencies?"
+    $answer = Read-Host "[y]es or [n]o (default: no) "
+    if ("$answer" -eq "y" -or "$answer" -eq "Y") {
+        install_python_deps
+    } 
+
+    __add_separator "80"
+
+    Write-Output "Backing up old LunarVim configuration"
+    backup_old_config
+
+    __add_separator "80" 
   
-		Write-Output "Would you like to check lunarvim's Python dependencies?"
-		$answer = Read-Host "[y]es or [n]o (default: no) "
-		if ("$answer" -eq "y" -or "$answer" -eq "Y") {
-			install_python_deps
-		} 
-	    
-		Write-Output "Would you like to check lunarvim's Rust dependencies?"
-		$answer = Read-Host "[y]es or [n]o (default: no) "
-		if ("$answer" -eq "y" -or "$answer" -eq "Y") {
-			install_rust_deps
-		} 
-  
-		__add_separator "80"
-  
-		Write-Output "Backing up old LunarVim configuration"
-		backup_old_config
-  
-		__add_separator "80" #>
-  
-	}
-  
-	if ($cliargs.Contains("--overwrite")) {
-		Write-Output "!!Warning!! -> Removing all lunarvim related config because of the --overwrite flag"
-		$answer = Read-Host "Would you like to continue? [y]es or [n]o "
-		if ("$answer" -ne "y" -and "$answer" -ne "Y") {
-			exit 1
-		} 
+    if ($cliargs.Contains("--overwrite")) {
+        Write-Output "!!Warning!! -> Removing all lunarvim related config because of the --overwrite flag"
+        $answer = Read-Host "Would you like to continue? [y]es or [n]o "
+        if ("$answer" -ne "y" -and "$answer" -ne "Y") {
+            exit 1
+        } 
 		
-		foreach ($dir in $__lvim_dirs) {
-			if (Test-Path "$dir") {
-				Remove-Item -Force -Recurse "$dir"
-			}
-		}
-	}
+        foreach ($dir in $__lvim_dirs) {
+            if (Test-Path "$dir") {
+                Remove-Item -Force -Recurse "$dir"
+            }
+        }
+    }
   
-	if (Test-Path "$LUNARVIM_RUNTIME_DIR\site\pack\packer\start\packer.nvim") {
-		Write-Output "Packer already installed"
-	}
-	else {
-		install_packer
-	}
+    if (Test-Path "$LUNARVIM_RUNTIME_DIR\site\pack\packer\start\packer.nvim") {
+        Write-Output "Packer already installed"
+    }
+    else {
+        install_packer
+    }
   
-	__add_separator "80"
+    __add_separator "80"
   
-	if (Test-Path "$LUNARVIM_RUNTIME_DIR\lvim\init.lua" ) {
-		Write-Output "Updating LunarVim"
-		update_lvim
-	}
-	else {
-		if ($cliargs.Contains("--testing")) {
-			copy_local_lvim_repository
-		}
-		else {
-			clone_lvim
-		}
-		setup_lvim
-	}
+    if (Test-Path "$LUNARVIM_RUNTIME_DIR\lvim\init.lua" ) {
+        Write-Output "Updating LunarVim"
+        update_lvim
+    }
+    else {
+        if ($cliargs.Contains("--testing")) {
+            copy_local_lvim_repository
+        }
+        else {
+            clone_lvim
+        }
+        setup_lvim
+    }
   
-	__add_separator "80"
-  
+    __add_separator "80"
 }
 
 function print_missing_dep_msg($dep) {
-	Write-Output "[ERROR]: Unable to find dependency [$dep]"
-	Write-Output "Please install it first and re-run the installer. Try: $RECOMMEND_INSTALL $dep"
+    Write-Output "[ERROR]: Unable to find dependency [$dep]"
+    Write-Output "Please install it first and re-run the installer. Try: $RECOMMEND_INSTALL $dep"
 }
-  
 
-function check_dep($dep) {
-	try { 
-		Get-Command -Name $dep -ErrorAction Stop | Out-Null 
-	}
-	catch { 
-		print_missing_dep_msg $dep
-		exit 1
-	}
+function install_system_package($dep) {
+    if (Get-Command -Name "winget" -ErrorAction SilentlyContinue) {
+        Write-Output "[INFO]: Attempting to install dependency [$dep] with winget"
+        $install_cmd = "winget install --interactive"
+    }
+    elseif (Get-Command -Name "scoop" -ErrorAction SilentlyContinue) {
+        Write-Output "[INFO]: Attempting to install dependency [$dep] with scoop"
+        # TODO: check if it's fine to not run it with --global
+        $install_cmd = "scoop install"
+    }
+    else {
+        print_missing_dep_msg "$dep"
+        exit 1
+    }
+
+    try {
+        Invoke-Command $install_cmd $dep -ErrorAction Stop
+    }
+    catch {
+        print_missing_dep_msg "$dep"
+        exit 1
+    }
+}
+
+function check_system_dep($dep) {
+    try { 
+        Get-Command -Name $dep -ErrorAction Stop | Out-Null 
+    }
+    catch { 
+        install_system_package "$dep"
+    }
 }
 
 function check_system_deps() {
-	check_dep "git"
-	check_dep "nvim"
+    Write-Output "[INFO]: Checking dependencies.."
+    check_system_dep "git"
+    check_system_dep "nvim"
+	
+}
+
+function install_nodejs_deps() {
+    try {
+        check_system_dep "node"
+        Invoke-Command npm install -g neovim tree-sitter-cli  -ErrorAction Break
+    }
+    catch {
+        print_missing_dep_msg "$dep"
+    }
+}
+
+function install_python_deps() {
+    try {
+        check_system_dep "pip"
+        Invoke-Command python -m pip install --user pynvim -ErrorAction Break
+    }
+    catch {
+        print_missing_dep_msg "$dep"
+    }
 }
 
 function backup_old_config() {
-	foreach ($dir in $__lvim_dirs) {
-		# we create an empty folder for subsequent commands \
-		# that require an existing directory	  
-		New-Item "$dir.bak" -ItemType "directory"
-		Copy-Item -Recurse "$dir\*" "$dir.bak\."
-	}
+    foreach ($dir in $__lvim_dirs) {
+        # we create an empty folder for subsequent commands \
+        # that require an existing directory	 
+        if ( Test-Path "$dir") {
+            New-Item "$dir.bak" -ItemType "directory"
+            Copy-Item -Recurse "$dir\*" "$dir.bak\." -
+        }
+    }
 
-	Write-Output "Backup operation complete"
+    Write-Output "Backup operation complete"
 }
 
 
 function install_packer() {
-	git clone --progress --depth 1 "https://github.com/wbthomason/packer.nvim" "$LUNARVIM_RUNTIME_DIR\site\pack\packer\start\packer.nvim"
+    Invoke-Command -ErrorAction Stop -ScriptBlock { git clone --progress --depth 1 "https://github.com/wbthomason/packer.nvim" "$LUNARVIM_RUNTIME_DIR\site\pack\packer\start\packer.nvim" }
 }
   
 function copy_local_lvim_repository() {
-	Write-Output "Copy local LunarVim configuration"
-	Copy-Item -Path "$((Get-Item $PWD).Parent.Parent.FullName)" -Destination "$LUNARVIM_RUNTIME_DIR/lvim" -Recurse
+    Write-Output "Copy local LunarVim configuration"
+    Copy-Item -Path "$((Get-Item $PWD).Parent.Parent.FullName)" -Destination "$LUNARVIM_RUNTIME_DIR/lvim" -Recurse
 }
 
 function clone_lvim() {
-	Write-Output "Cloning LunarVim configuration"
-	try {
-		git clone --progress --branch "$LV_BRANCH" --depth 1 "https://github.com/${LV_REMOTE}" "$LUNARVIM_RUNTIME_DIR/lvim" 
-	}
-	catch {
-		Write-Output "Failed to clone repository. Installation failed."
-		exit 1		
-	}
+    Write-Output "Cloning LunarVim configuration"
+    try {
+        Invoke-Command -ErrorAction Stop -ScriptBlock { git clone --progress --branch "$LV_BRANCH" --depth 1 "https://github.com/$LV_REMOTE" "$LUNARVIM_RUNTIME_DIR/lvim" } 
+    }
+    catch {
+        Write-Output "Failed to clone repository. Installation failed."
+        exit 1		
+    }
 }
 
 function setup_shim() {
-	if ((Test-Path "$INSTALL_PREFIX\bin") -eq $false) {
-		New-Item "$INSTALL_PREFIX\bin" -ItemType "directory" | Out-Null
-	}
+    if ((Test-Path "$INSTALL_PREFIX\bin") -eq $false) {
+        New-Item "$INSTALL_PREFIX\bin" -ItemType "directory"
+    }
 
-	Set-Content -Path "$INSTALL_PREFIX\bin\lvim.ps1" -Value (`
-	"New-Variable -Name LUNARVIM_CONFIG_DIR -Value `"$LUNARVIM_CONFIG_DIR`"`n" +`
-	"New-Variable -Name LUNARVIM_RUNTIME_DIR -Value `"$LUNARVIM_RUNTIME_DIR`"`n`n" +`
-	"nvim -u `"$LUNARVIM_RUNTIME_DIR\lvim\init.lua`" " + '$args')
+    Set-Content -Path "$INSTALL_PREFIX\bin\lvim.ps1" ("
+		New-Variable -Name LUNARVIM_CONFIG_DIR -Value '$LUNARVIM_CONFIG_DIR'
+		New-Variable -Name LUNARVIM_RUNTIME_DIR -Value '$LUNARVIM_RUNTIME_DIR'
+		
+		nvim -u '$LUNARVIM_RUNTIME_DIR\lvim\init.lua' ", '"', "$args", '"')
 }
 
 function setup_lvim() {
-	Write-Output "Installing LunarVim shim"
+    Write-Output "Installing LunarVim shim"
   
-	setup_shim
+    setup_shim
   
-	Write-Output "Preparing Packer setup"
+    Write-Output "Preparing Packer setup"
 
-	if ((Test-Path "$LUNARVIM_CONFIG_DIR") -eq $false) {
-		New-Item "$LUNARVIM_CONFIG_DIR" -ItemType "directory"
-	}
+    if ((Test-Path "$LUNARVIM_CONFIG_DIR") -eq $false) {
+        New-Item "$LUNARVIM_CONFIG_DIR" -ItemType "directory"
+    }
 
-	Copy-Item "$LUNARVIM_RUNTIME_DIR\lvim\utils\installer\config.example-no-ts.lua" `
-		"$LUNARVIM_CONFIG_DIR\config.lua"
+    Copy-Item "$LUNARVIM_RUNTIME_DIR\lvim\utils\installer\config.example-no-ts.lua" `
+        "$LUNARVIM_CONFIG_DIR\config.lua"
   
-	nvim -u "$LUNARVIM_RUNTIME_DIR\lvim\init.lua" -c 'autocmd User PackerComplete sleep 100m | qall' -c PackerInstall
-	nvim -u "$LUNARVIM_RUNTIME_DIR\lvim\init.lua" -c 'autocmd User PackerComplete sleep 100m | qall' -c PackerSync
+    nvim -u "$LUNARVIM_RUNTIME_DIR\lvim\init.lua" -c 'autocmd User PackerComplete sleep 100m | qall' -c PackerInstall
+    nvim -u "$LUNARVIM_RUNTIME_DIR\lvim\init.lua" -c 'autocmd User PackerComplete sleep 100m | qall' -c PackerSync
   
 	Write-Output "Packer setup complete"
 	
@@ -216,27 +259,27 @@ function setup_lvim() {
 
 	__add_separator "80"
 
-	Write-Output "Thank you for installing LunarVim!!"
-	Write-Output "You can start it by running: $INSTALL_PREFIX\bin\lvim.ps1"
-	Write-Output "Do not forget to use a font with glyphs (icons) support [https://github.com/ryanoasis/nerd-fonts]"
+    Write-Output "Thank you for installing LunarVim!!"
+    Write-Output "You can start it by running: $INSTALL_PREFIX\bin\lvim.ps1"
+    Write-Output "Do not forget to use a font with glyphs (icons) support [https://github.com/ryanoasis/nerd-fonts]"
 }
 
 
 function update_lvim() {
-	try {
-		git -C "$LUNARVIM_RUNTIME_DIR/lvim" status -uno
-	}
-	catch {
-		git -C "$LUNARVIM_RUNTIME_DIR/lvim" pull --ff-only --progress -or
-		Write-Output "Unable to guarantee data integrity while updating. Please do that manually instead."
-		exit 1
-	}
-	Write-Output "Your LunarVim installation is now up to date!"
+    try {
+        Invoke-Command git -C "$LUNARVIM_RUNTIME_DIR/lvim" status -uno
+    }
+    catch {
+        git -C "$LUNARVIM_RUNTIME_DIR/lvim" pull --ff-only --progress -or
+        Write-Output "Unable to guarantee data integrity while updating. Please do that manually instead."
+        exit 1
+    }
+    Write-Output "Your LunarVim installation is now up to date!"
 }
 
 function __add_separator($div_width) {
-	"-" * $div_width
-	Write-Output ""
+    "-" * $div_width
+    Write-Output ""
 }
 
 function create_alias {
