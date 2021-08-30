0f458a04688d71d194fc104351f8cda5aff936af
Christian Chiarulli <chris.machine@pm.me>
2021-08-30 00:59:23 -0400

    onedarker colorscheme

--- lua/onedarker/Whichkey.lua
+++ lua/onedarker/Whichkey.lua
@@ -1,8 +1,8 @@
 local Whichkey = {
   WhichKey = { fg = C.purple },
   WhichKeySeperator = { fg = C.green },
-  WhichKeyGroup = { fg = C.blue },
-  WhichKeyDesc = { fg = C.light_blue },
+  WhichKeyGroup = { fg = C.cyan },
+  WhichKeyDesc = { fg = C.blue },
   WhichKeyFloat = { bg = C.dark },
 }
 
