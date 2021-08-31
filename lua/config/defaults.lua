9c3f568f5c0ef1055658d3dd89912973790d9881
Abouzar Parvan <abzcoding@users.noreply.github.com>
2021-08-31 22:18:01 +0430

    better vue support (#1438)

--- lua/config/defaults.lua
+++ lua/config/defaults.lua
@@ -1077,6 +1077,27 @@ lvim.lang = {
         cmd = {
           DATA_PATH .. "/lspinstall/vue/node_modules/.bin/vls",
         },
+        root_dir = function(fname)
+          local util = require "lspconfig/util"
+          return util.root_pattern "package.json"(fname) or util.root_pattern "vue.config.js"(fname) or vim.fn.getcwd()
+        end,
+        init_options = {
+          config = {
+            vetur = {
+              completion = {
+                autoImport = true,
+                tagCasing = "kebab",
+                useScaffoldSnippets = true,
+              },
+              useWorkspaceDependencies = true,
+              validation = {
+                script = true,
+                style = true,
+                template = true,
+              },
+            },
+          },
+        },
       },
     },
   },
