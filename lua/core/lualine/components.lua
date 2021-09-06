efa7c3c6c9f2ffa9f7e0dc231fbad1411bfacaa0
Abouzar Parvan <abzcoding@users.noreply.github.com>
2021-09-06 22:53:41 +0430

    there was a weird situation that caused lualine msg to turn boolean instead of table (#1492)

--- lua/core/lualine/components.lua
+++ lua/core/lualine/components.lua
@@ -84,7 +84,8 @@ return {
       msg = msg or "LS Inactive"
       local buf_clients = vim.lsp.buf_get_clients()
       if next(buf_clients) == nil then
-        if #msg == 0 then
+        -- TODO: clean up this if statement
+        if type(msg) == "boolean" or #msg == 0 then
           return "LS Inactive"
         end
         return msg
