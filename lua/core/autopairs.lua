03a6abbd71da0a724b0c730f9addc01076dd55c3
christianchiarulli <chrisatmachine@gmail.com>
2021-09-07 19:18:26 -0400

    some autopair rules for latex

--- lua/core/autopairs.lua
+++ lua/core/autopairs.lua
@@ -22,8 +22,33 @@ end
 M.setup = function()
   local autopairs = require "nvim-autopairs"
   local Rule = require "nvim-autopairs.rule"
+  local cond = require "nvim-autopairs.conds"
+
+  autopairs.setup {
+    check_ts = lvim.builtin.autopairs.check_ts,
+    ts_config = lvim.builtin.autopairs.ts_config,
+  }
 
   -- vim.g.completion_confirm_key = ""
+
+  autopairs.add_rule(Rule("$$", "$$", "tex"))
+  autopairs.add_rules {
+    Rule("$", "$", { "tex", "latex" }) -- don't add a pair if the next character is %
+      :with_pair(cond.not_after_regex_check "%%") -- don't add a pair if  the previous character is xxx
+      :with_pair(cond.not_before_regex_check("xxx", 3)) -- don't move right when repeat character
+      :with_move(cond.none()) -- don't delete if the next character is xx
+      :with_del(cond.not_after_regex_check "xx") -- disable  add newline when press <cr>
+      :with_cr(cond.none()),
+  }
+  autopairs.add_rules {
+    Rule("$$", "$$", "tex"):with_pair(function(opts)
+      print(vim.inspect(opts))
+      if opts.line == "aa $$" then
+        -- don't add pair on that line
+        return false
+      end
+    end),
+  }
 
   if package.loaded["cmp"] then
     require("nvim-autopairs.completion.cmp").setup {
@@ -32,11 +57,6 @@ M.setup = function()
       auto_select = true, -- automatically select the first item
     }
   end
-
-  autopairs.setup {
-    check_ts = lvim.builtin.autopairs.check_ts,
-    ts_config = lvim.builtin.autopairs.ts_config,
-  }
 
   require("nvim-treesitter.configs").setup { autopairs = { enable = true } }
 
