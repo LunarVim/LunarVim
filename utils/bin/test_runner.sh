f6c775f56307e68bc933ba496a29c2ca5b5dae31
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-09-12 12:48:11 +0200

    refactor(ci): better path handling

--- utils/bin/test_runner.sh
+++ utils/bin/test_runner.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-# TODO: allow running with a minimal_init.lua
+export LUNARVIM_CONFIG_DIR="${LUNARVIM_CONFIG_DIR:-"$HOME/.config/lvim"}"
+export LUNARVIM_RUNTIME_DIR="${LUNARVIM_RUNTIME_DIR:-"$HOME/.local/share/lunarvim"}"
+
+export LVIM_TEST_ENV=true
+
+rm -f "$LUNARVIM_CONFIG_DIR/plugin/packer_compiled.lua"
+
+lvim() {
+  # TODO: allow running with a minimal_init.lua
+  nvim -u "$LUNARVIM_RUNTIME_DIR/lvim/init.lua" --cmd "set runtimepath+=$LUNARVIM_RUNTIME_DIR/lvim" "$@"
+}
+
 if [ -n "$1" ]; then
-  nvim --headless -u ./init.lua -c "lua require('plenary.busted').run('$1')"
+  lvim --headless -c "lua require('plenary.busted').run('$1')"
 else
-  nvim --headless -u ./init.lua -c "PlenaryBustedDirectory tests/ { minimal_init = './init.lua' }"
+  lvim --headless -c "PlenaryBustedDirectory tests/ { minimal_init = './init.lua' }"
 fi
