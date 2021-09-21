ca243168da8d18856cb5a07d280a1505caf8099c
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-09-21 14:53:12 +0200

    refactor: remove global state from null-ls for now

--- lua/lsp/null-ls/init.lua
+++ lua/lsp/null-ls/init.lua
@@ -1,27 +1,10 @@
 local M = {}
+
 local Log = require "core.log"
 local formatters = require "lsp.null-ls.formatters"
 local linters = require "lsp.null-ls.linters"
 
-function M.list_supported_provider_names(filetype)
-  local names = {}
-
-  vim.list_extend(names, formatters.list_supported_names(filetype))
-  vim.list_extend(names, linters.list_supported_names(filetype))
-
-  return names
-end
-
-function M.list_unsupported_provider_names(filetype)
-  local names = {}
-
-  vim.list_extend(names, formatters.list_unsupported_names(filetype))
-  vim.list_extend(names, linters.list_unsupported_names(filetype))
-
-  return names
-end
-
-function M.setup()
+function M:setup()
   local status_ok, null_ls = pcall(require, "null-ls")
   if not status_ok then
     Log:error "Missing null-ls dependency"
--- lua/lsp/null-ls/services.lua
+++ lua/lsp/null-ls/services.lua
@@ -45,4 +45,19 @@ function M.find_command(command)
   return nil
 end
 
+function M.list_registered_providers_names(filetype)
+  local u = require "null-ls.utils"
+  local c = require "null-ls.config"
+  local registered = {}
+  for method, source in pairs(c.get()._methods) do
+    for name, filetypes in pairs(source) do
+      if u.filetype_matches(filetypes, filetype) then
+        registered[method] = registered[method] or {}
+        table.insert(registered[method], name)
+      end
+    end
+  end
+  return registered
+end
+
 return M
