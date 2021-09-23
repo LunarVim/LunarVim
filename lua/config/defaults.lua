b83877b2a23a123fe390af90a6d4ecdf726cb88a
abzcoding <abzcoding@gmail.com>
2021-09-23 17:07:16 +0330

    fix the lua goto definition bug and add better common capabilities

--- lua/config/defaults.lua
+++ lua/config/defaults.lua
@@ -644,12 +644,6 @@ lvim.lang = {
         },
         settings = {
           Lua = {
-            runtime = {
-              -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-              version = "LuaJIT",
-              -- Setup your lua path
-              path = vim.split(package.path, ";"),
-            },
             diagnostics = {
               -- Get the language server to recognize the `vim` global
               globals = { "vim", "lvim" },
@@ -662,7 +656,7 @@ lvim.lang = {
                 [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
               },
               maxPreload = 100000,
-              preloadFileSize = 1000,
+              preloadFileSize = 10000,
             },
           },
         },
