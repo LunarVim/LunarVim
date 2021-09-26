238bbf18ac4e35152be06736c75e94a81e3382e8
Abouzar Parvan <abzcoding@users.noreply.github.com>
2021-09-26 21:11:44 +0330

    fix small bugs in nvim-tree (#1629)

--- utils/installer/config.example.lua
+++ utils/installer/config.example.lua
@@ -51,7 +51,7 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.side = "left"
+lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 0
 
 -- if you don't want all the parsers change this to a table of the ones you want
