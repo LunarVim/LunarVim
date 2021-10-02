1e0d0181d55abe1e4248b3aeaded94cbf3308168
Abouzar Parvan <abzcoding@users.noreply.github.com>
2021-10-02 18:04:38 +0330

    feat: make lsp highlight color configurable (#1659)

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -9,9 +9,6 @@ local function lsp_highlight_document(client)
   if client.resolved_capabilities.document_highlight then
     vim.api.nvim_exec(
       [[
-      hi LspReferenceRead cterm=bold ctermbg=red guibg=#353d46
-      hi LspReferenceText cterm=bold ctermbg=red guibg=#353d46
-      hi LspReferenceWrite cterm=bold ctermbg=red guibg=#353d46
       augroup lsp_document_highlight
         autocmd! * <buffer>
         autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
