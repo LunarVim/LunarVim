7fec97a59044a5fb6001b9f9f9365ee24b9bcca2
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-10-10 13:26:08 +0200

    fix: use the correct path in the update-hooks

--- lua/lvim/utils/hooks.lua
+++ lua/lvim/utils/hooks.lua
@@ -1,24 +1,25 @@
 local M = {}
 
-local Log = require "core.log"
+local Log = require "lvim.core.log"
 local in_headless = #vim.api.nvim_list_uis() == 0
 
 function M.run_pre_update()
   Log:debug "Starting pre-update hook"
+  _G.__luacache.clear_cache()
 end
 
 ---Reset any startup cache files used by Packer and Impatient
 ---Tip: Useful for clearing any outdated settings
 function M.reset_cache()
   _G.__luacache.clear_cache()
-  require("plugin-loader"):cache_reset()
+  require("lvim.plugin-loader"):cache_reset()
 end
 
 function M.run_post_update()
   M.reset_cache()
   Log:debug "Starting post-update hook"
-  package.loaded["lsp.templates"] = nil
-  require("lsp.templates").generate_templates()
+  package.loaded["lvim.lsp.templates"] = nil
+  require("lvim.lsp.templates").generate_templates()
 
   if not in_headless then
     vim.schedule(function()
