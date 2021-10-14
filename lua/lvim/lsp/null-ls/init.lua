f87416c141de0909a05f1d1cad162a9774def549
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-10-14 18:58:56 +0200

    fix(null-ls): add common lsp-setup hooks (#1763)

--- lua/lvim/lsp/null-ls/init.lua
+++ lua/lvim/lsp/null-ls/init.lua
@@ -12,6 +12,12 @@ function M:setup()
   end
 
   null_ls.config()
+  local default_opts = require("lvim.lsp").get_common_opts()
+
+  if vim.tbl_isempty(lvim.lsp.null_ls.setup or {}) then
+    lvim.lsp.null_ls.setup = default_opts
+  end
+
   require("lspconfig")["null-ls"].setup(lvim.lsp.null_ls.setup)
   for filetype, config in pairs(lvim.lang) do
     if not vim.tbl_isempty(config.formatters) then
