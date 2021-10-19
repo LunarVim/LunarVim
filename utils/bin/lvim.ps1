cbbff35d47a4fd40553ed63dab00e7701c321133
Andrew Glick <17516195+Antyos@users.noreply.github.com>
2021-10-18 22:35:35 -0500

    fix(file): fix environment vars in .ps1 scripts
    Resolves #1671
    
    `$LOCALAPPDATA` and `$TEMP` are not default variables in PowerShell.
    This fix updates those variables to reference `$env:`. See below:
    - `$LOCALAPPDATA` -> `$env:LOCALAPPDATA`
    - `$TEMP` -> `$env:TEMP`

--- utils/bin/lvim.ps1
+++ utils/bin/lvim.ps1
@@ -1,6 +1,6 @@
 $env:XDG_DATA_HOME = ($env:XDG_DATA_HOME, "$env:APPDATA", 1 -ne $null)[0]
-$env:XDG_CONFIG_HOME = ($env:XDG_CONFIG_HOME, "$LOCALAPPDATA", 1 -ne $null)[0]
-$env:XDG_CACHE_HOME = ($env:XDG_CACHE_HOME, "$TEMP", 1 -ne $null)[0]
+$env:XDG_CONFIG_HOME = ($env:XDG_CONFIG_HOME, "$env:LOCALAPPDATA", 1 -ne $null)[0]
+$env:XDG_CACHE_HOME = ($env:XDG_CACHE_HOME, "$env:TEMP", 1 -ne $null)[0]
 
 $env:LUNARVIM_RUNTIME_DIR = ($env:LUNARVIM_RUNTIME_DIR, "$env:XDG_DATA_HOME\lunarvim", 1 -ne $null)[0]
 $env:LUNARVIM_CONFIG_DIR = ($env:LUNARVIM_CONFIG_DIR, "$env:XDG_CONFIG_HOME\lvim", 1 -ne $null)[0]
