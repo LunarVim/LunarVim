f3d9ef138df7f531434ce3ff37fcd379517d8640
ChristianChiarulli <chris.machine@pm.me>
2021-11-04 15:49:41 -0400

    fix: make telescope default mappings overrideable

--- lua/lvim/core/telescope.lua
+++ lua/lvim/core/telescope.lua
@@ -8,6 +8,7 @@ function M.config()
     on_config_done = nil,
   }
 
+  local actions = require "telescope.actions"
   lvim.builtin.telescope = vim.tbl_extend("force", lvim.builtin.telescope, {
     defaults = {
       prompt_prefix = " ",
@@ -32,6 +33,22 @@ function M.config()
         "--column",
         "--smart-case",
         "--hidden",
+      },
+      mappings = {
+        i = {
+          ["<C-n>"] = actions.move_selection_next,
+          ["<C-p>"] = actions.move_selection_previous,
+          ["<C-c>"] = actions.close,
+          ["<C-j>"] = actions.cycle_history_next,
+          ["<C-k>"] = actions.cycle_history_prev,
+          ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+          ["<CR>"] = actions.select_default + actions.center,
+        },
+        n = {
+          ["<C-n>"] = actions.move_selection_next,
+          ["<C-p>"] = actions.move_selection_previous,
+          ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+        },
       },
       file_ignore_patterns = {},
       path_display = { shorten = 5 },
