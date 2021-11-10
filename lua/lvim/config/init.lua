94d136114c8ef5f8e62fa9084d8b52542b66ce1f
Chase Colman <chase@colman.io>
2021-11-11 00:05:09 +0800

    fix(keymap): unset on reload, load correct order, add unset default
    fix(keymap): don't unset default keymaps on reload unless set to false
    
    fix(keymaps): prevent accessing undefined default mode
    
    fix(keymap): use unadapted mode for default check
    
    fix(keymap): apply initial lvim.keys

--- lua/lvim/config/init.lua
+++ lua/lvim/config/init.lua
@@ -26,14 +26,13 @@ function M:init()
     lvim.database = { save_location = utils.join_paths(home_dir, ".config", "lunarvim_db"), auto_execute = 1 }
   end
 
+  lvim.keys = apply_defaults(lvim.keys, require("lvim.keymappings").get_defaults())
+
   local builtins = require "lvim.core.builtins"
   builtins.config { user_config_file = user_config_file }
 
   local settings = require "lvim.config.settings"
   settings.load_options()
-
-  local default_keymaps = require("lvim.keymappings").get_defaults()
-  lvim.keys = apply_defaults(lvim.keys, default_keymaps)
 
   local autocmds = require "lvim.core.autocmds"
   lvim.autocommands = apply_defaults(lvim.autocommands, autocmds.load_augroups())
@@ -89,6 +88,9 @@ function M:load(config_path)
   autocmds.define_augroups(lvim.autocommands)
 
   vim.g.mapleader = (lvim.leader == "space" and " ") or lvim.leader
+
+  local default_keymaps = require("lvim.keymappings").get_defaults()
+  lvim.keys = apply_defaults(lvim.keys, default_keymaps)
   require("lvim.keymappings").load(lvim.keys)
 
   local settings = require "lvim.config.settings"
@@ -98,6 +100,8 @@ end
 --- Override the configuration with a user provided one
 -- @param config_path The path to the configuration overrides
 function M:reload()
+  require("lvim.keymappings").clear(lvim.keys)
+
   local lvim_modules = {}
   for module, _ in pairs(package.loaded) do
     if module:match "lvim.core" then
