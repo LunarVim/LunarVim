29fac25b60c8eace6385be6fe61b2610098a7816
Xavier Young <45989017+younger-1@users.noreply.github.com>
2021-11-16 18:14:27 +0800

    fix(onedarker): make `fg` clear for section `a` and `z`

--- lua/lualine/themes/onedarker.lua
+++ lua/lualine/themes/onedarker.lua
@@ -19,14 +19,14 @@ local colors = {
 -- LuaFormatter on
 return {
   normal = {
-    a = { fg = colors.bg, bg = colors.blue, gui = "bold" },
+    a = { fg = colors.gray2, bg = colors.blue, gui = "bold" },
     b = { fg = colors.fg, bg = colors.bg },
     c = { fg = colors.fg, bg = colors.bg },
   },
-  insert = { a = { fg = colors.fg, bg = colors.green, gui = "bold" } },
-  visual = { a = { fg = colors.fg, bg = colors.purple, gui = "bold" } },
-  command = { a = { fg = colors.fg, bg = colors.yellow, gui = "bold" } },
-  replace = { a = { fg = colors.fg, bg = colors.red1, gui = "bold" } },
+  insert = { a = { fg = colors.gray2, bg = colors.green, gui = "bold" } },
+  visual = { a = { fg = colors.gray2, bg = colors.purple, gui = "bold" } },
+  command = { a = { fg = colors.gray2, bg = colors.yellow, gui = "bold" } },
+  replace = { a = { fg = colors.gray2, bg = colors.red1, gui = "bold" } },
   inactive = {
     a = { fg = colors.gray1, bg = colors.bg, gui = "bold" },
     b = { fg = colors.gray1, bg = colors.bg },
