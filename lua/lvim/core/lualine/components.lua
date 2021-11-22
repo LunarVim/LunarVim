f3d7a94d8cb85a7c03eace8576ad7f9125147384
christianchiarulli <chrisatmachine@gmail.com>
2021-11-22 15:14:18 -0500

    feat: improved LSP grouping in lualine

--- lua/lvim/core/lualine/components.lua
+++ lua/lvim/core/lualine/components.lua
@@ -112,9 +112,9 @@ return {
       local supported_linters = linters.list_registered_providers(buf_ft)
       vim.list_extend(buf_client_names, supported_linters)
 
-      return table.concat(buf_client_names, ", ")
+      return "[" .. table.concat(buf_client_names, ", ") .. "]"
     end,
-    icon = " ",
+    -- icon = " ",
     color = { gui = "bold" },
     cond = conditions.hide_in_width,
   },
