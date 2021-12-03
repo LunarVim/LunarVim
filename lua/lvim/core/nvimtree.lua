9e3f20ea91ff74dc9548cb845d8fb5ba01330bec
xeluxee <88047141+xeluxee@users.noreply.github.com>
2021-12-03 19:30:15 +0100

    fix(nvimtree): fix wrong behavior with paths containig spaces

--- lua/lvim/core/nvimtree.lua
+++ lua/lvim/core/nvimtree.lua
@@ -170,9 +170,9 @@ end
 function M.start_telescope(telescope_mode)
   local node = require("nvim-tree.lib").get_node_at_cursor()
   local abspath = node.link_to or node.absolute_path
-  local is_folder = node.has_children and true
+  local is_folder = node.open ~= nil
   local basedir = is_folder and abspath or vim.fn.fnamemodify(abspath, ":h")
-  vim.api.nvim_command("Telescope " .. telescope_mode .. " cwd=" .. basedir)
+  vim.api.nvim_command("Telescope " .. telescope_mode .. " cwd=" .. string.gsub(basedir, " ", "\\ "))
 end
 
 return M
