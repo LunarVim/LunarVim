2e5ac35fb0474d8ce4ef77830fe7fcc08992b590
xeluxee <88047141+xeluxee@users.noreply.github.com>
2021-12-04 16:20:28 +0100

    fix(nvimtree): update telescope mapping

--- lua/lvim/core/nvimtree.lua
+++ lua/lvim/core/nvimtree.lua
@@ -172,7 +172,9 @@ function M.start_telescope(telescope_mode)
   local abspath = node.link_to or node.absolute_path
   local is_folder = node.open ~= nil
   local basedir = is_folder and abspath or vim.fn.fnamemodify(abspath, ":h")
-  vim.api.nvim_command("Telescope " .. telescope_mode .. " cwd=" .. string.gsub(basedir, " ", "\\ "))
+  require("telescope.builtin")[telescope_mode] {
+    cwd = basedir
+  }
 end
 
 return M
