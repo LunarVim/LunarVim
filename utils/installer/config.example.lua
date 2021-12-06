5095f45258cb4b9b06489ee367aec208df440c14
Walter Maldonado Jr <walter.bode.unesp@gmail.com>
2021-12-06 00:01:03 -0300

    feat: Enable octo plugin by default in config.example.lua

--- utils/installer/config.example.lua
+++ utils/installer/config.example.lua
@@ -56,6 +56,7 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
+lvim.builtin.octo.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 0
 
