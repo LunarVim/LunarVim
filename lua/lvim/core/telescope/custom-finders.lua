a3cf3b1f2a6f2fce80833753a133c9983e56d4e2
Landon Schropp <schroppl@gmail.com>
2021-12-23 02:56:19 -0600

    [Feature]: Use Telescope's git_files with fallback (#2089)

--- lua/lvim/core/telescope/custom-finders.lua
+++ lua/lvim/core/telescope/custom-finders.lua
@@ -82,4 +82,14 @@ function M.view_lunarvim_changelog()
   }):find()
 end
 
+-- Smartly opens either git_files or find_files, depending on whether the working directory is
+-- contained in a Git repo.
+function M.find_project_files()
+  local ok = pcall(builtin.git_files)
+
+  if not ok then
+    builtin.find_files()
+  end
+end
+
 return M
