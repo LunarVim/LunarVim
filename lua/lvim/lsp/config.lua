23ebcead812502988b12b7756a3b9422115746c4
Alexey Svirshchevskiy <alexey.svirshchevskiy@sitoo.com>
2022-01-26 12:27:53 +0100

    fix(lsp): avoid accessing undefined user_data

--- lua/lvim/lsp/config.lua
+++ lua/lvim/lsp/config.lua
@@ -23,7 +23,7 @@ return {
       prefix = "",
       format = function(d)
         local t = vim.deepcopy(d)
-        local code = d.code or d.user_data.lsp.code
+        local code = d.code or (d.user_data and d.user_data.lsp.code)
         if code then
           t.message = string.format("%s [%s]", t.message, code):gsub("1. ", "")
         end
