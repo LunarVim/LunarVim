c12338393ef70b338724244de1dad88538f8a36a
Abouzar Parvan <abzcoding@users.noreply.github.com>
2022-03-02 17:36:06 +0330

    chore(plugins): bump version (#2321)

--- lua/lvim/core/nvimtree.lua
+++ lua/lvim/core/nvimtree.lua
@@ -9,6 +9,7 @@ function M.config()
       disable_netrw = true,
       hijack_netrw = true,
       open_on_setup = false,
+      ignore_buffer_on_setup = false,
       ignore_ft_on_setup = {
         "startify",
         "dashboard",
