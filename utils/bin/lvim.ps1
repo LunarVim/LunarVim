7c6eab3e7c63d58d1cae60879e6238598f31bd5c
kylo252 <59826753+kylo252@users.noreply.github.com>
2022-03-09 16:21:10 +0100

    ci(windows): attempt to fix headless

--- utils/bin/lvim.ps1
+++ utils/bin/lvim.ps1
@@ -1,9 +1,13 @@
-$env:XDG_DATA_HOME = ($env:XDG_DATA_HOME, "$env:APPDATA", 1 -ne $null)[0]
-$env:XDG_CONFIG_HOME = ($env:XDG_CONFIG_HOME, "$env:LOCALAPPDATA", 1 -ne $null)[0]
-$env:XDG_CACHE_HOME = ($env:XDG_CACHE_HOME, "$env:TEMP", 1 -ne $null)[0]
+#Requires -Version 7.1
+$ErrorActionPreference = "Stop" # exit when command fails
 
-$env:LUNARVIM_RUNTIME_DIR = ($env:LUNARVIM_RUNTIME_DIR, "$env:XDG_DATA_HOME\lunarvim", 1 -ne $null)[0]
-$env:LUNARVIM_CONFIG_DIR = ($env:LUNARVIM_CONFIG_DIR, "$env:XDG_CONFIG_HOME\lvim", 1 -ne $null)[0]
-$env:LUNARVIM_CACHE_DIR = ($env:LUNARVIM_CACHE_DIR, "$env:XDG_CACHE_HOME\lvim", 1 -ne $null)[0]
+$env:XDG_DATA_HOME = $env:XDG_DATA_HOME ?? $env:APPDATA
+$env:XDG_CONFIG_HOME = $env:XDG_CONFIG_HOME ?? $env:LOCALAPPDATA
+$env:XDG_CACHE_HOME = $env:XDG_CACHE_HOME ?? $env:TEMP
+
+$env:LUNARVIM_RUNTIME_DIR = $env:LUNARVIM_RUNTIME_DIR ?? "$env:XDG_DATA_HOME\lunarvim"
+$env:LUNARVIM_CONFIG_DIR = $env:LUNARVIM_CONFIG_DIR ?? "$env:XDG_CONFIG_HOME\lvim"
+$env:LUNARVIM_CACHE_DIR = $env:LUNARVIM_CACHE_DIR ?? "$env:XDG_CACHE_HOME\lvim"
+$env:LUNARVIM_BASE_DIR = $env:LUNARVIM_BASE_DIR ?? "$env:LUNARVIM_RUNTIME_DIR\lvim"
 
 nvim -u "$env:LUNARVIM_RUNTIME_DIR\lvim\init.lua" @args
