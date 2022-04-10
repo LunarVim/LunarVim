f9d071910fd79ceb878a9606c2167389e199d3aa
Daniel Rodríguez Rivero <rdanielo@gmail.com>
2022-04-10 18:00:08 +0200

    Update plugins.lua
    just add snipmate loading, so users don't have to

--- lua/lvim/plugins.lua
+++ lua/lvim/plugins.lua
@@ -70,6 +70,8 @@ local core_plugins = {
     "L3MON4D3/LuaSnip",
     config = function()
       require("luasnip/loaders/from_vscode").lazy_load()
+      require("luasnip/loaders/from_snipmate").lazy_load()
+
     end,
   },
   {
