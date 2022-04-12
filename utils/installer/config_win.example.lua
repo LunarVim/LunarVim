e9f817572d30b94e82a959203c594b5cdabaefd3
FADHsquared <77851256+FADHsquared@users.noreply.github.com>
2022-04-12 14:45:01 +0700

    docs(windows): use alpha in config_win.example.lua
    avoid the deprecation warning upon installation by
    changing `lvim.builtin.dashboard` to lvim.builtin.alpha
    following the changes to config.example.lua

--- utils/installer/config_win.example.lua
+++ utils/installer/config_win.example.lua
@@ -70,7 +70,8 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- }
 
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.dashboard.active = true
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = false
 -- lvim.builtin.terminal.shell = "pwsh.exe -NoLogo"
