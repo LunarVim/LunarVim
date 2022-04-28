f297af004d227f964b0d595d136f3b7d0c763b89
abzcoding <abzcoding@gmail.com>
2022-04-28 17:42:58 +0430

    fix(nvimtree): add latest changes from nvimtree

--- lua/lvim/core/nvimtree.lua
+++ lua/lvim/core/nvimtree.lua
@@ -9,6 +9,8 @@ function M.config()
       disable_netrw = true,
       hijack_netrw = true,
       open_on_setup = false,
+      open_on_setup_file = false,
+      sort_by = "name",
       ignore_buffer_on_setup = false,
       ignore_ft_on_setup = {
         "startify",
@@ -21,16 +23,12 @@ function M.config()
         enable = true,
         auto_open = true,
       },
-      update_to_buf_dir = {
-        enable = true,
-        auto_open = true,
-      },
-      auto_close = false,
       open_on_tab = false,
       hijack_cursor = false,
       update_cwd = false,
       diagnostics = {
         enable = true,
+        show_on_dirs = false,
         icons = {
           hint = "",
           info = "",
@@ -57,7 +55,7 @@ function M.config()
         height = 30,
         hide_root_folder = false,
         side = "left",
-        auto_resize = false,
+        preserve_window_proportions = false,
         mappings = {
           custom_only = false,
           list = {},
@@ -66,26 +64,58 @@ function M.config()
         relativenumber = false,
         signcolumn = "yes",
       },
+      renderer = {
+        indent_markers = {
+          enable = false,
+          icons = {
+            corner = "└ ",
+            edge = "│ ",
+            none = "  ",
+          },
+        },
+        icons = {
+          webdev_colors = lvim.use_icons,
+        },
+      },
       filters = {
         dotfiles = false,
         custom = { "node_modules", "\\.cache" },
+        exclude = {},
       },
       trash = {
         cmd = "trash",
         require_confirm = true,
       },
+      log = {
+        enable = false,
+        truncate = false,
+        types = {
+          all = false,
+          config = false,
+          copy_paste = false,
+          diagnostics = false,
+          git = false,
+          profile = false,
+        },
+      },
       actions = {
+        use_system_clipboard = true,
         change_dir = {
+          enable = true,
           global = false,
+          restrict_above_cwd = false,
         },
         open_file = {
-          resize_window = true,
           quit_on_open = false,
-        },
-        window_picker = {
-          enable = false,
-          chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-          exclude = {},
+          resize_window = false,
+          window_picker = {
+            enable = true,
+            chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+            exclude = {
+              filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+              buftype = { "nofile", "terminal", "help" },
+            },
+          },
         },
       },
     },
