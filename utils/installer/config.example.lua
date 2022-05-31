73850c956a896b9afb5a41d67600fe323e31232b
abzcoding <abzcoding@gmail.com>
2022-05-31 18:43:51 +0430

    fix(nvimtree): update nvim-tree setup

--- utils/installer/config.example.lua
+++ utils/installer/config.example.lua
@@ -61,7 +61,7 @@ lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
