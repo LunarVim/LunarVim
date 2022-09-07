b57c4219a185b8b51af944205bb1adc182c05d1b
abzcoding <abzcoding@gmail.com>
2022-09-07 09:54:51 +0430

    fix(plugins): bring back original folke repos

--- lua/lvim/plugins.lua
+++ lua/lvim/plugins.lua
@@ -102,8 +102,7 @@ local core_plugins = {
     "hrsh7th/cmp-path",
   },
   {
-    -- NOTE: Temporary fix till folke comes back
-    "max397574/lua-dev.nvim",
+    "folke/lua-dev.nvim",
     module = "lua-dev",
   },
 
@@ -153,7 +152,7 @@ local core_plugins = {
 
   -- Whichkey
   {
-    "max397574/which-key.nvim",
+    "folke/which-key.nvim",
     config = function()
       require("lvim.core.which-key").setup()
     end,
