464ad8b0035cbcaf4864d14da598b77a939026d8
yangbinji <1139040653@qq.com>
2022-09-15 20:41:51 +0800

    fix(example config): fix config for treesitter (#3016)

--- utils/installer/config_win.example.lua
+++ utils/installer/config_win.example.lua
@@ -97,7 +97,7 @@ lvim.builtin.treesitter.ensure_installed = {
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
-lvim.builtin.treesitter.highlight.enabled = true
+lvim.builtin.treesitter.highlight.enable = true
 
 -- generic LSP settings
 
