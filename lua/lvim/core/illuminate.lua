917d7e4ce6b804f8186b37b77457f4ab6b03b0a6
ChristianChiarulli <chris.machine@pm.me>
2022-09-16 00:00:21 -0400

    feat(document highlight): use illuminate rather than autocommand to avoid flashing

---
+++ lua/lvim/core/illuminate.lua
@@ -0,0 +1,48 @@
+local M = {}
+
+M.config = function()
+  -- default configuration
+  require("illuminate").configure {
+    -- providers: provider used to get references in the buffer, ordered by priority
+    providers = {
+      "lsp",
+      "treesitter",
+      "regex",
+    },
+    -- delay: delay in milliseconds
+    delay = 120,
+    -- filetypes_denylist: filetypes to not illuminate, this overrides filetypes_allowlist
+    filetypes_denylist = {
+      "dirvish",
+      "fugitive",
+      "alpha",
+      "NvimTree",
+      "packer",
+      "neogitstatus",
+      "Trouble",
+      "lir",
+      "Outline",
+      "spectre_panel",
+      "toggleterm",
+      "DressingSelect",
+      "TelescopePrompt",
+    },
+    -- filetypes_allowlist: filetypes to illuminate, this is overriden by filetypes_denylist
+    filetypes_allowlist = {},
+    -- modes_denylist: modes to not illuminate, this overrides modes_allowlist
+    modes_denylist = {},
+    -- modes_allowlist: modes to illuminate, this is overriden by modes_denylist
+    modes_allowlist = {},
+    -- providers_regex_syntax_denylist: syntax to not illuminate, this overrides providers_regex_syntax_allowlist
+    -- Only applies to the 'regex' provider
+    -- Use :echom synIDattr(synIDtrans(synID(line('.'), col('.'), 1)), 'name')
+    providers_regex_syntax_denylist = {},
+    -- providers_regex_syntax_allowlist: syntax to illuminate, this is overriden by providers_regex_syntax_denylist
+    -- Only applies to the 'regex' provider
+    -- Use :echom synIDattr(synIDtrans(synID(line('.'), col('.'), 1)), 'name')
+    providers_regex_syntax_allowlist = {},
+    -- under_cursor: whether or not to illuminate under the cursor
+    under_cursor = true,
+  }
+end
+return M
--- lua/lvim/plugins.lua
+++ lua/lvim/plugins.lua
@@ -255,6 +255,8 @@ local core_plugins = {
   {
     "b0o/schemastore.nvim",
   },
+
+  { "RRethy/vim-illuminate" },
 }
 
 local default_snapshot_path = join_paths(get_lvim_base_dir(), "snapshots", "default.json")
