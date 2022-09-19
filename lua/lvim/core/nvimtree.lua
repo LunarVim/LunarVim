1f78b014b343aba1a71bfaf0416842f62564c5e5
Christian Chiarulli <chris.machine@pm.me>
2022-09-19 19:19:29 -0400

    feat: a less noisy tree

--- lua/lvim/core/nvimtree.lua
+++ lua/lvim/core/nvimtree.lua
@@ -2,6 +2,25 @@ local M = {}
 local Log = require "lvim.core.log"
 
 function M.config()
+
+  local status_ok, utils = pcall(require, "nvim-tree.utils")
+  if not status_ok then
+    return
+  end
+
+  local function notify_level(level)
+    return function(msg)
+      vim.schedule(function()
+        vim.api.nvim_echo({ { msg, "WarningMsg" } }, false, {})
+      end)
+    end
+  end
+
+  utils.notify.warn = notify_level(vim.log.levels.WARN)
+  utils.notify.error = notify_level(vim.log.levels.ERROR)
+  utils.notify.info = notify_level(vim.log.levels.INFO)
+  utils.notify.debug = notify_level(vim.log.levels.DEBUG)
+
   lvim.builtin.nvimtree = {
     active = true,
     on_config_done = nil,
