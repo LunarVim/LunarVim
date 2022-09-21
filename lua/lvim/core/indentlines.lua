d5ca5929b067f7d6f5f9f99c2280f888bad548fe
abzcoding <abzcoding@gmail.com>
2022-09-21 21:33:26 +0430

    fix(indentblankline): make sure to use the new syntax for all options

--- lua/lvim/core/indentlines.lua
+++ lua/lvim/core/indentlines.lua
@@ -1,27 +1,26 @@
 local M = {}
 
 M.config = function()
-  vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
-  vim.g.indent_blankline_filetype_exclude = {
-    "help",
-    "startify",
-    "dashboard",
-    "packer",
-    "neogitstatus",
-    "NvimTree",
-    "Trouble",
-    "text",
-  }
-  vim.g.indentLine_enabled = 1
-  vim.g.indent_blankline_char = "▏"
-  vim.g.indent_blankline_show_trailing_blankline_indent = false
-  vim.g.indent_blankline_show_first_indent_level = true
-  vim.g.indent_blankline_use_treesitter = false
-  vim.g.indent_blankline_show_current_context = true
   lvim.builtin.indentlines = {
     active = true,
     on_config_done = nil,
     options = {
+      enabled = true,
+      buftype_exclude = { "terminal", "nofile" },
+      filetype_exclude = {
+        "help",
+        "startify",
+        "dashboard",
+        "packer",
+        "neogitstatus",
+        "NvimTree",
+        "Trouble",
+        "text",
+      },
+      char = "▏",
+      show_trailing_blankline_indent = false,
+      show_first_indent_level = false,
+      use_treesitter = true,
       show_current_context = true,
     },
   }
