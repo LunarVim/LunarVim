bea690fc16e3e23e8644c61b1d2e140fb8851d1b
Christian Chiarulli <chris.machine@pm.me>
2022-09-21 21:21:00 -0400

    fix: use columns instead of winwidth

--- lua/lvim/core/lualine/conditions.lua
+++ lua/lvim/core/lualine/conditions.lua
@@ -5,7 +5,7 @@ local conditions = {
     return vim.fn.empty(vim.fn.expand "%:t") ~= 1
   end,
   hide_in_width = function()
-    return vim.fn.winwidth(0) > window_width_limit
+    return vim.o.columns > window_width_limit
   end,
   -- check_git_workspace = function()
   --   local filepath = vim.fn.expand "%:p:h"
