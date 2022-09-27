15ce6521a6ff8e2aadbb0e2ad6c80b316cabfb5f
mhmdali102 <mhmdali102@bk.ru>
2022-09-27 07:14:58 +0300

    rename files for better routing

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -65,7 +65,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'Introduction',
+            docId: 'introduction',
             position: 'left',
             label: 'Docs',
           },
