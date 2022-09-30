ca1be9702c476cf4c6d263935cee9c4185cb656f
mhmdali102 <mhmdali102@bk.ru>
2022-09-30 21:26:24 +0300

    added tokyonight theme + changed accent color to magenta

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
 import styles from './index.module.css';
 
 function HomepageHeader() {
