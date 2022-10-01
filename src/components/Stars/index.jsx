a6666131b23252b8b345655c6570f37f7559f9a7
mhmdali102 <mhmdali102@bk.ru>
2022-10-01 17:15:08 +0300

    🌠: falling stars animation done

--- src/components/Stars/index.jsx
+++ src/components/Stars/index.jsx
@@ -1,24 +1,21 @@
 import React from "react";
 import styles from "./styles.module.css";
 
-// star sizes in px
-const SIZES = [1, 1, 2, 3, 4];
-// stars count
-const COUNT = 100;
+const STAR_SIZES = [1, 1, 2, 3, 4]; // in px
+const STARS_COUNT = 75;
+const FALLING_STARS_COUNT = 5;
 
-//get random position
-function randomNumber(min, max) {
+//get random number between min and max
+const randomNumber = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
 const Stars = ({ children }) => (
   <div className={styles.starsContainer}>
-    {/* generating array with length = count */}
-    {[...Array(COUNT)].map((_, i) => {
-      /* randomize position and size */
-      const top = randomNumber(1, 100);
-      const left = randomNumber(1, 100);
-      const size = SIZES[Math.floor(Math.random() * SIZES.length)];
+    {/* generating array with length = STARS_COUNT */}
+    {[...Array(STARS_COUNT)].map((_, i) => {
+      /* random and size */
+      const size = STAR_SIZES[Math.floor(Math.random() * STAR_SIZES.length)];
 
       return (
         <div
@@ -26,8 +23,8 @@ const Stars = ({ children }) => (
           className={styles.star}
           style={{
             position: "absolute",
-            top: `${top}%`,
-            left: `${left}%`,
+            top: `${randomNumber(1, 100)}%`,
+            left: `${randomNumber(1, 100)}%`,
             height: `${size}px`,
             width: `${size}px`,
             animationDelay: `${randomNumber(2, 6)}s`,
@@ -36,6 +33,19 @@ const Stars = ({ children }) => (
         />
       );
     })}
+    {/* generating array with length = FALLING_STARS_COUNT */}
+    {[...Array(FALLING_STARS_COUNT)].map((_, i) => (
+      <div
+        key={i}
+        className={styles.fallingStar}
+        style={{
+          top: `${randomNumber(0, 30)}%`,
+          left: `${randomNumber(20, 90)}%`,
+          animationDelay: `${randomNumber(0, 10)}s`,
+          animationDuration: `${randomNumber(3, 10)}s`,
+        }}
+      />
+    ))}
     {children}
   </div>
 );
