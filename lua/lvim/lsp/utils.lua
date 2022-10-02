dc94d904da61acbf4eb0930b8845fb786f9f0502
kylo252 <59826753+kylo252@users.noreply.github.com>
2022-10-02 13:03:04 +0200

    refactor: remove deprecated function

--- lua/lvim/lsp/utils.lua
+++ lua/lvim/lsp/utils.lua
@@ -23,25 +23,14 @@ function M.get_active_clients_by_ft(filetype)
 end
 
 function M.get_client_capabilities(client_id)
-  local client
-  if not client_id then
-    local buf_clients = vim.lsp.buf_get_clients()
-    for _, buf_client in pairs(buf_clients) do
-      if buf_client.name ~= "null-ls" then
-        client = buf_client
-        break
-      end
-    end
-  else
-    client = vim.lsp.get_client_by_id(tonumber(client_id))
-  end
+  local client = vim.lsp.get_client_by_id(tonumber(client_id))
   if not client then
-    error "Unable to determine client_id"
+    Log:warn("Unable to determine client from client_id: " .. client_id)
     return
   end
 
   local enabled_caps = {}
-  for capability, status in pairs(client.server_capabilities or client.resolved_capabilities) do
+  for capability, status in pairs(client.server_capabilities) do
     if status == true then
       table.insert(enabled_caps, capability)
     end
