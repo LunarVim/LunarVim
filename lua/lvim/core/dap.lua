4a30a9ec214f3277677245e54d8489153a31aff9
Gabriele Musco <emaildigabry@gmail.com>
2022-10-03 11:37:17 +0200

    fix: (dapui) don't check for nvim version for expand_lines
    Co-authored-by: lvimuser <109605931+lvimuser@users.noreply.github.com>

--- lua/lvim/core/dap.lua
+++ lua/lvim/core/dap.lua
@@ -88,9 +88,7 @@ M.setup_ui = function()
       repl = "r",
       toggle = "t",
     },
-    -- Expand lines larger than the window
-    -- Requires >= 0.7
-    expand_lines = vim.fn.has "nvim-0.7",
+    expand_lines = true,
     -- Layouts define sections of the screen to place windows.
     -- The position can be 'left', 'right', 'top' or 'bottom'.
     -- The size specifies the height/width depending on position. It can be an Int
