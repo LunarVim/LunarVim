e5900413c3d15f46c7ed21e13c6453407d3c426a
Philipp Bokatius <contact@vonpb.de>
2022-10-06 02:53:03 +0200

    feature: pages deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,6 +15,7 @@ const config = {
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/lunarvim_icon.png",
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
