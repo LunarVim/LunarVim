8fb2fa94442ec9148ed8209abe2b3eabd2d0e1d4
2nthony <ant.hu@outlook.com>
2022-10-14 10:33:12 +0800

    fix(alpha): check alpha module

--- lua/lvim/core/alpha.lua
+++ lua/lvim/core/alpha.lua
@@ -1,5 +1,4 @@
 local M = {}
-
 function M.config()
   local lvim_dashboard = require "lvim.core.alpha.dashboard"
   local lvim_startify = require "lvim.core.alpha.startify"
@@ -65,7 +64,10 @@ local function configure_additional_autocmds()
 end
 
 function M.setup()
-  local alpha = require "alpha"
+  local status_ok, alpha = pcall(require, "alpha")
+  if not status_ok then
+    return
+  end
   local mode = lvim.builtin.alpha.mode
   local config = lvim.builtin.alpha[mode].config
 
