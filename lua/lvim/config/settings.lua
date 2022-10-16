7715900525e5cfd7b210f9de8dd2fb25fe37b512
Jakub Reroń <kubareron@gmail.com>
2022-10-16 11:52:40 +0200

    fix: disable unsupported asian characters spellchecking (#3259)

--- lua/lvim/config/settings.lua
+++ lua/lvim/config/settings.lua
@@ -57,6 +57,7 @@ M.load_default_options = function()
   }
 
   ---  SETTINGS  ---
+  vim.opt.spelllang:append "cjk" -- disable spellchecking for asian characters (VIM algorithm does not support it)
   vim.opt.shortmess:append "c" -- don't show redundant messages from ins-completion-menu
   vim.opt.shortmess:append "I" -- don't show the default intro message
   vim.opt.whichwrap:append "<,>,[,],h,l"
