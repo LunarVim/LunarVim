de03bc2d6ef5d22b907f8a4bb550136e4bd91986
Mark Huggins <mhuggins@glgroup.com>
2022-10-19 19:26:41 -0400

    feat(quickfix): remove nvim-notify
    removing nvim-notify fixes #3294

--- utils/installer/config.example.lua
+++ utils/installer/config.example.lua
@@ -64,7 +64,6 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
