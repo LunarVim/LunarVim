6b91b3ff91771efd941cdf4edca271ec3d7c7cc6
Tommy Au <75346987+smarttommyau@users.noreply.github.com>
2022-11-03 22:28:48 +0800

    Add quote to the Set-Alias
    To prevent spacing in the path causes the alias not working.

--- utils/installer/install.ps1
+++ utils/installer/install.ps1
@@ -271,7 +271,7 @@ function create_alias {
         New-Item -Path $PROFILE -ItemType "file" -Force
     }
 
-    Add-Content -Path $PROFILE -Value $("`r`nSet-Alias lvim $lvim_bin")
+    Add-Content -Path $PROFILE -Value $("`r`nSet-Alias lvim '$lvim_bin'")
 
     Write-Host 'To use the new alias in this window reload your profile with: `. $PROFILE`' -ForegroundColor Green
 }
