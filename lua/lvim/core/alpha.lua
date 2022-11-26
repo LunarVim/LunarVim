d0f110d4d0c97780baf7a6fca6aa406163d50537
LostNeophyte <lostneophyte@tuta.io>
2022-11-26 15:49:14 +0100

    refactor: add default autostart to opts

--- lua/lvim/core/alpha.lua
+++ lua/lvim/core/alpha.lua
@@ -4,8 +4,8 @@ function M.config()
   local lvim_dashboard = require "lvim.core.alpha.dashboard"
   local lvim_startify = require "lvim.core.alpha.startify"
   lvim.builtin.alpha = {
-    dashboard = { config = {}, section = lvim_dashboard.get_sections(), opts = {} },
-    startify = { config = {}, section = lvim_startify.get_sections(), opts = {} },
+    dashboard = { config = {}, section = lvim_dashboard.get_sections(), opts = { autostart = true } },
+    startify = { config = {}, section = lvim_startify.get_sections(), opts = { autostart = true } },
     active = true,
     mode = "dashboard",
   }
