6fc015565fa9b50a9e05f2048ee789cd595eb6ca
Daniel Rodríguez Rivero <rdanielo@gmail.com>
2022-12-01 11:39:53 +0100

    feat(wich-key): binding to save without formatting (#3165)
    use `<leader>bW` to save without triggering the autocmd for formatting

--- lua/lvim/core/which-key.lua
+++ lua/lvim/core/which-key.lua
@@ -93,6 +93,7 @@ M.config = function()
         f = { "<cmd>Telescope buffers<cr>", "Find" },
         b = { "<cmd>BufferLineCyclePrev<cr>", "Previous" },
         n = { "<cmd>BufferLineCycleNext<cr>", "Next" },
+        W = { "<cmd>noautocmd w<cr>", "Save without formatting (noautocmd)" },
         -- w = { "<cmd>BufferWipeout<cr>", "Wipeout" }, -- TODO: implement this for bufferline
         e = {
           "<cmd>BufferLinePickClose<cr>",
