c70a094314d890575641e9166a535b5c257951f2
opalmay <65673442+opalmay@users.noreply.github.com>
2022-12-20 17:58:43 +0200

    feat: don't prompt smart quit when buffer open in another window (#3636)

--- lua/lvim/utils/functions.lua
+++ lua/lvim/utils/functions.lua
@@ -2,8 +2,9 @@ local M = {}
 
 function M.smart_quit()
   local bufnr = vim.api.nvim_get_current_buf()
+  local buf_windows = vim.call("win_findbuf", bufnr)
   local modified = vim.api.nvim_buf_get_option(bufnr, "modified")
-  if modified then
+  if modified and #buf_windows == 1 then
     vim.ui.input({
       prompt = "You have unsaved changes. Quit anyway? (y/n) ",
     }, function(input)
