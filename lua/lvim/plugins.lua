d037bac277c8ec7c6fcfbe516976b8f622bda595
LostNeophyte <lostneophyte@tuta.io>
2022-12-23 10:29:03 +0100

    refactor(lazy): pin plugins with packer's snapshot

--- lua/lvim/plugins.lua
+++ lua/lvim/plugins.lua
@@ -291,21 +291,21 @@ local core_plugins = {
   },
 }
 
--- local default_snapshot_path = join_paths(get_lvim_base_dir(), "snapshots", "default.json")
--- local content = vim.fn.readfile(default_snapshot_path)
--- local default_sha1 = vim.fn.json_decode(content)
+local default_snapshot_path = join_paths(get_lvim_base_dir(), "snapshots", "default.json")
+local content = vim.fn.readfile(default_snapshot_path)
+local default_sha1 = vim.fn.json_decode(content)
 
--- local get_default_sha1 = function(spec)
---   local short_name, _ = require("packer.util").get_plugin_short_name(spec)
---   return default_sha1[short_name] and default_sha1[short_name].commit
--- end
+local get_default_sha1 = function(spec)
+  local short_name, _ = spec[1]:match "[^/]+$"
+  return default_sha1[short_name] and default_sha1[short_name].commit
+end
 
--- -- TODO: use lazy lockfiles, if possible
--- if not vim.env.LVIM_DEV_MODE then
---   for _, spec in ipairs(core_plugins) do
---     -- Manually lock the commit hash since Packer's snapshots are unreliable in headless mode
---     spec["commit"] = get_default_sha1(spec)
---   end
--- end
+-- TODO: use lazy lockfiles, if possible
+if not vim.env.LVIM_DEV_MODE then
+  for _, spec in ipairs(core_plugins) do
+    -- Manually lock the commit hash since Packer's snapshots are unreliable in headless mode
+    spec["commit"] = get_default_sha1(spec)
+  end
+end
 
 return core_plugins
