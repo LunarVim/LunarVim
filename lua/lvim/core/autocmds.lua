66a65d741d34043b4faabbaec070185d894373bf
LostNeophyte <lostneophyte@tuta.io>
2022-12-25 11:30:24 +0100

    don't check for windows

--- lua/lvim/core/autocmds.lua
+++ lua/lvim/core/autocmds.lua
@@ -184,11 +184,6 @@ function M.enable_reload_config_on_save()
   -- autocmds require forward slashes (even on windows)
   local pattern = user_config_file:gsub("\\", "/") .. "/*.lua"
 
-
-  if vim.loop.os_uname().version:match "Windows" then
-    -- autocmds require forward slashes even on windows
-    pattern = pattern:gsub("\\", "/")
-  end
   vim.api.nvim_create_augroup("lvim_reload_config_on_save", {})
   vim.api.nvim_create_autocmd("BufWritePost", {
     group = "lvim_reload_config_on_save",
