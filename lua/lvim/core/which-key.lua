ca1fe61870ea934ec5d26ef973d8cba686f4e70b
LostNeophyte <lostneophyte@tuta.io>
2022-12-29 15:16:34 +0100

    chore: format

--- lua/lvim/core/which-key.lua
+++ lua/lvim/core/which-key.lua
@@ -140,11 +140,11 @@ M.config = function()
         i = { "<cmd>Lazy install<cr>", "Install" },
         s = { "<cmd>Lazy sync<cr>", "Sync" },
         S = { "<cmd>Lazy clear<cr>", "Status" },
-	c = { "<cmd>Lazy clean<cr>", "Clean" },
+        c = { "<cmd>Lazy clean<cr>", "Clean" },
         u = { "<cmd>Lazy update<cr>", "Update" },
-	p = { "<cmd>Lazy profile<cr>", "Profile" },
-	l = { "<cmd>Lazy log<cr>", "Log" },
-	d = { "<cmd>Lazy debug<cr>", "Debug" },
+        p = { "<cmd>Lazy profile<cr>", "Profile" },
+        l = { "<cmd>Lazy log<cr>", "Log" },
+        d = { "<cmd>Lazy debug<cr>", "Debug" },
       },
 
       -- " Available Debug Adapters:
