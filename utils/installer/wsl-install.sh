216b27324b9cadb90977a1a4d41c3beb011eda90
Filip Hajdyła <72252240+f1lem0n@users.noreply.github.com>
2023-01-27 17:47:55 +0100

    Update wsl-install.sh

--- utils/installer/wsl-install.sh
+++ utils/installer/wsl-install.sh
@@ -7,10 +7,9 @@ sudo mv /tmp/win32yank.exe /usr/local/bin/
 # download anonymice font
 wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/AnonymousPro/complete/Anonymice%20Nerd%20Font%20Complete%20Mono.ttf $HOME/Downloads/
 
-# remove vim if installed and install neovim from source and dependencies
-apt remove vim -y
+# install neovim from source and dependencies
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.deb
-apt install -y \
+sudo apt install -y \
   ./nvim-linux64.deb \
   ripgrep
 rm nvim-linux64.deb
