52ba9abafc7be3f40cc954fb2e1852344acdd7de
opalmay <opal.mizrahi2@gmail.com>
2023-02-07 18:43:27 +0200

    refactor: use dot notation for intellisense

--- lua/lvim/core/terminal.lua
+++ lua/lvim/core/terminal.lua
@@ -2,7 +2,7 @@ local M = {}
 local Log = require "lvim.core.log"
 
 M.config = function()
-  lvim.builtin["terminal"] = {
+  lvim.builtin.terminal = {
     active = true,
     on_config_done = nil,
     -- size can be a number or function which is passed the current terminal
