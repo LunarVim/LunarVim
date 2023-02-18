0a0cf29c8aa1fafb09dac1a36e30dec642ed24a9
LostNeophyte <lostneophyte@tuta.io>
2023-02-18 08:51:26 +0100

    feat(LvimUpdate): regenerate templates after syncing plugins

--- lua/lvim/utils/hooks.lua
+++ lua/lvim/utils/hooks.lua
@@ -61,6 +61,7 @@ function M.run_post_update()
   Log:debug "Syncing core plugins"
   plugin_loader.reload { reload "lvim.plugins", lvim.plugins }
   plugin_loader.sync_core_plugins()
+  require("lvim.lsp.templates").generate_templates()
 
   if not in_headless then
     vim.schedule(function()
