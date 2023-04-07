6bd46cb6f551bd319617173bc27455ad314addcf
Jieru Mei <meijieru@gmail.com>
2023-04-06 21:55:09 -0400

    feat(lang): yaml use schemastore (#3953)

--- lua/lvim/lsp/providers/yamlls.lua
+++ lua/lvim/lsp/providers/yamlls.lua
@@ -8,21 +8,7 @@ local opts = {
         enable = true,
         url = "https://www.schemastore.org/api/json/catalog.json",
       },
-      schemas = {
-        kubernetes = {
-          "daemon.{yml,yaml}",
-          "manager.{yml,yaml}",
-          "restapi.{yml,yaml}",
-          "role.{yml,yaml}",
-          "role_binding.{yml,yaml}",
-          "*onfigma*.{yml,yaml}",
-          "*ngres*.{yml,yaml}",
-          "*ecre*.{yml,yaml}",
-          "*eployment*.{yml,yaml}",
-          "*ervic*.{yml,yaml}",
-          "kubectl-edit*.yaml",
-        },
-      },
+      schemas = require("schemastore").yaml.schemas(),
     },
   },
 }
