2f66dc3beeeb359b21e73b77cf31e758e6df18df
LostNeophyte <lostneophyte@tuta.io>
2023-05-25 09:29:08 +0200

    feat: add starter.lvim link to config.example.lua

--- utils/installer/config_win.example.lua
+++ utils/installer/config_win.example.lua
@@ -1,4 +1,5 @@
 -- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
