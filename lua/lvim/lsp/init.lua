673df24c8476f2b742c0fc1c2f0a92a5cb3794e5
Sadik Saifi <Sadiksaifi205@gmail.com>
2023-05-27 16:43:11 +0530

    feat(plugin): add rounded borders to :LspInfo window

--- lua/lvim/lsp/init.lua
+++ lua/lvim/lsp/init.lua
@@ -120,6 +120,11 @@ function M.setup()
 
   set_handler_opts_if_not_set("textDocument/hover", vim.lsp.handlers.hover, { border = "rounded" })
   set_handler_opts_if_not_set("textDocument/signatureHelp", vim.lsp.handlers.signature_help, { border = "rounded" })
+
+  -- Enable rounded borders in :LspInfo window.
+  pcall(function()
+    require("lspconfig.ui.windows").default_options.border = "rounded"
+  end)
 end
 
 return M
