a587a4503295b0f59d577d979e3048183c774b07
kylo252 <59826753+kylo252@users.noreply.github.com>
2023-08-25 11:51:57 +0200

    test: adapt recent changes in neovim and plenary (#4338)

--- tests/lvim/helpers.lua
+++ tests/lvim/helpers.lua
@@ -6,26 +6,12 @@ function M.search_file(file, args)
   local stdout, ret = Job:new({
     command = "grep",
     args = { args, file },
-    cwd = get_cache_dir(),
+    cwd = vim.loop.cwd(),
     on_stderr = function(_, data)
       table.insert(stderr, data)
     end,
   }):sync()
   return ret, stdout, stderr
-end
-
-function M.file_contains(file, query)
-  local ret, stdout, stderr = M.search_file(file, query)
-  if ret == 0 then
-    return true
-  end
-  if not vim.tbl_isempty(stderr) then
-    error(vim.inspect(stderr))
-  end
-  if not vim.tbl_isempty(stdout) then
-    error(vim.inspect(stdout))
-  end
-  return false
 end
 
 function M.log_contains(query)
