d8ffab83be68f10fc349ad9e2591206cde63f690
kylo252 <59826753+kylo252@users.noreply.github.com>
2024-05-09 09:43:12 +0200

    chore: update utils plugins (#4558)

--- lua/lvim/core/autopairs.lua
+++ lua/lvim/core/autopairs.lua
@@ -19,18 +19,26 @@ function M.config()
       java = false,
     },
     disable_filetype = { "TelescopePrompt", "spectre_panel" },
-    ignored_next_char = string.gsub([[ [%w%%%'%[%"%.] ]], "%s+", ""),
-    enable_moveright = true,
     ---@usage disable when recording or executing a macro
     disable_in_macro = false,
+    ---@usage disable  when insert after visual block mode
+    disable_in_visualblock = false,
+    disable_in_replace_mode = true,
+    ignored_next_char = string.gsub([[ [%w%%%'%[%"%.] ]], "%s+", ""),
+    enable_moveright = true,
     ---@usage add bracket pairs after quote
     enable_afterquote = true,
+    ---@usage trigger abbreviation
+    enable_abbr = false,
+    ---@usage switch for basic rule break undo sequence
+    break_undo = true,
+    map_cr = true,
     ---@usage map the <BS> key
     map_bs = true,
     ---@usage map <c-w> to delete a pair if possible
     map_c_w = false,
-    ---@usage disable when insert after visual block mode
-    disable_in_visualblock = false,
+    ---@usage Map the <C-h> key to delete a pair
+    map_c_h = false,
     ---@usage  change default fast_wrap
     fast_wrap = {
       map = "<M-e>",
